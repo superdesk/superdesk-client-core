478292313d568df32e3757760db71c43c3199a82
Syed <sjunaid@aap.com.au>
2016-02-24 11:14:20 +1100

    [sd-3361]-(fix PR Comment) - Improvement in Client Notification intervals for re-connect attempts on websocket disconnection

--- scripts/superdesk/notification/tests/notification_spec.js
+++ scripts/superdesk/notification/tests/notification_spec.js
@@ -79,16 +79,22 @@ describe('Notify Connection Service', function() {
         });
     });
 
-    it('can show disconnection message when websocket disconnected', inject(function(notifyConnectionService) {
+    it('can show disconnection message when websocket disconnected', inject(function(notifyConnectionService, notify) {
+        notify.disconnectionNotified = null;
         msg = 'Disconnected to Notification Server!';
         rootScope.$broadcast('disconnected');
+        rootScope.$digest();
         expect(notifyConnectionService.message).toEqual(msg);
+        expect(notify.disconnectionNotified).toBe(true);
     }));
 
-    it('can show success message when websocket connected', inject(function(notifyConnectionService) {
+    it('can show success message when websocket connected', inject(function(notifyConnectionService, notify) {
+        notify.disconnectionNotified = true;
         msg = 'Connected to Notification Server!';
         rootScope.$broadcast('connected');
+        rootScope.$digest();
         expect(notifyConnectionService.message).toEqual(msg);
+        expect(notify.disconnectionNotified).toBe(null);
     }));
 
 });
