8231898e2a5be6b0193465e2eb60314180834528
mugur.rus <mugur.rus@sourcefabric.org>
2016-03-01 23:52:35 +0200

    default search provider

---
+++ spec/search_provider_spec.js
@@ -0,0 +1,38 @@
+'use strict';
+var openUrl = require('./helpers/utils').open,
+    searchProvider = require('./helpers/pages').searchProvider,
+    globalSearch = require('./helpers/search');
+
+describe('search_provider', function() {
+
+    beforeEach(function(done) {
+        openUrl('/#/settings/searchProviders').then(done);
+    });
+
+    it('Add Search Provider', function() {
+        // create default search provider
+        searchProvider.addProvider('AAP Multimedia', 'AAP Multimedia', true);
+
+        // create a second search provider and set it as default
+        searchProvider.addProvider('PA Images', 'PA Images', true);
+
+        // the first provider should not be set as default
+        searchProvider.editProvider(0);
+        expect(searchProvider.checkbox.isSelected()).toBeFalsy();
+        searchProvider.checkbox.click();
+        searchProvider.saveProvider();
+
+        // the second provider should not be set as default
+        searchProvider.editProvider(1);
+        expect(searchProvider.checkbox.isSelected()).toBeFalsy();
+
+        // in search the first provider should be selected
+        openUrl('/#/search');
+        globalSearch.openFilterPanel();
+        globalSearch.openParameters();
+        expect(element.all(by.css('[value="local"]')).first().isSelected()).toBeFalsy();
+        expect(element.all(by.css('[ng-value="provider.search_provider"]')).get(0).isSelected()).toBeTruthy();
+        expect(element.all(by.css('[ng-value="provider.search_provider"]')).get(1).isSelected()).toBeFalsy();
+    });
+
+});
