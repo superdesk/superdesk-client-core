ab28a78ed1e698d966f449dc8ee8e5b8feeea453
Petr <petr.jasek@sourcefabric.org>
2016-03-10 13:22:23 +0100

    move server settings from env to settings.py

--- test-server/settings.py
+++ test-server/settings.py
@@ -1,5 +1,13 @@
 
-import os
+SUPERDESK_TESTING = True 
 
-# use default settings, just set the path
-ABS_PATH = os.path.abspath(os.path.dirname(__file__))
+SUPERDESK_URL = 'http://127.0.0.1:5000/api'
+SUPERDESK_CLIENT_URL = 'http://127.0.0.1:9090'
+
+REDIS_URL = 'redis://localhost:6379/2'
+
+MONGO_DBNAME = 'superdesk_e2e'
+ARCHIVED_DBNAME = 'superdesk_e2e_archived'
+LEGAL_ARCHIVE_DBNAME = 'superdesk_e2e_legal_archive'
+
+ELASTICSEARCH_INDEX = 'superdesk_e2e'
