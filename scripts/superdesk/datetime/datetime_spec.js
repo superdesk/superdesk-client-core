6e908b3adf559f19a09fc04230d13f42c5bf001c
Petr <petr.jasek@sourcefabric.org>
2016-03-16 13:01:25 +0100

    fix(time): make timestamps in list show non relative time
    it does only time for current day, day + time for current week,
    month + day otherwise. + is configurable.
    
    SD-4098

--- scripts/superdesk/datetime/datetime_spec.js
+++ scripts/superdesk/datetime/datetime_spec.js
@@ -1,21 +1,45 @@
 (function() {
+
     'use strict';
 
-    beforeEach(function() {
-        module('superdesk.datetime');
+    describe('superdesk.datetime module', function() {
+
+        beforeEach(module('superdesk.datetime'));
+
+        describe('reldate filter', function() {
+            it('can convert js Date into a string', inject(function($filter) {
+                var date = new Date();
+                expect($filter('reldate')(date)).toBe('a few seconds ago');
+            }));
+        });
+
+        describe('time filter', function() {
+            it('can convert date into time str', inject(function($filter) {
+                var date = new Date(2010, 10, 10, 8, 5, 35);
+                expect($filter('time')(date)).toBe('8:05');
+            }));
+        });
+
+        describe('datetime service', function() {
+            it('can short format datetime', inject(function(datetime, moment) {
+                var now = moment();
+                var day = now.isoWeekday();
+
+                expect(now.format('hh:mm')).toBe(datetime.shortFormat(now.format()));
+
+                // on monday do tuesday, otherwise day before
+                var week = day === 1 ? now.add(1, 'days') : now.subtract(1, 'days');
+                expect(week.format('dddd, hh:mm')).toBe(datetime.shortFormat(week.format()));
+
+                var next = now.add(8, 'days');
+                expect(next.format('MM/DD')).toBe(datetime.shortFormat(next.format()));
+            }));
+
+            it('can long format datetime', inject(function(datetime, moment) {
+                var now = moment();
+                expect(now.format('LLL')).toBe(datetime.longFormat(now.format()));
+            }));
+        });
     });
 
-    describe('reldate filter', function() {
-        it('can convert js Date into a string', inject(function($filter) {
-            var date = new Date();
-            expect($filter('reldate')(date)).toBe('a few seconds ago');
-        }));
-    });
-
-    describe('time filter', function() {
-        it('can convert date into time str', inject(function($filter) {
-            var date = new Date(2010, 10, 10, 8, 5, 35);
-            expect($filter('time')(date)).toBe('8:05');
-        }));
-    });
 })();
