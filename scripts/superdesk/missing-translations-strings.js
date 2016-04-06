dd0caedf8ef6437e96710ea0dd175a6620342d01
Vlad Nicoara <vlad.nicoara@sourcefabric.org>
2016-04-06 18:59:10 +0300

    SDNTB-156 Expose 'hidden' strings for traslation

---
+++ scripts/superdesk/missing-translations-strings.js
@@ -0,0 +1,7 @@
+/**
+ * Expose strings for translation that aren't visible in regular js or template files.
+ */
+gettext('publish');gettext('send');gettext('text');gettext('picture');gettext('composite');gettext('video');gettext('audio');
+gettext('content');gettext('users');gettext('sluglines');gettext('in-progress');gettext('success');gettext('error');
+gettext('retrying');gettext('failed');gettext('ingested');gettext('fetched');gettext('routed');gettext('draft');gettext('submitted');
+gettext('scheduled');gettext('published');gettext('spiked');gettext('corrected');gettext('killed');
