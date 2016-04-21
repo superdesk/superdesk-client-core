418bdc3103db8de87b3cced9cbaf104088ffe15c
Petr Jašek <jasekpetr@gmail.com>
2016-04-21 10:18:17 +0200

    feat(workspace): add config for ingest/content view (#337)
    those links can be switched off via config (and are off by default).
    
    SD-3816

--- scripts/superdesk-workspace/workspace.js
+++ scripts/superdesk-workspace/workspace.js
@@ -314,11 +314,13 @@
         };
     }
 
-    WorkspaceSidenavDirective.$inject = ['superdeskFlags', '$location', 'keyboardManager', 'gettext'];
-    function WorkspaceSidenavDirective(superdeskFlags, $location, keyboardManager, gettext) {
+    WorkspaceSidenavDirective.$inject = ['superdeskFlags', '$location', 'keyboardManager', 'gettext', 'config'];
+    function WorkspaceSidenavDirective(superdeskFlags, $location, keyboardManager, gettext, config) {
         return {
             templateUrl: 'scripts/superdesk-workspace/views/workspace-sidenav-items.html',
             link: function(scope, elem) {
+
+                scope.workspaceConfig = config.workspace || {};
 
                 /*
                  * Function for showing and hiding monitoring list
@@ -344,10 +346,12 @@
                     e.preventDefault();
                     $location.url('/workspace');
                 }, {global: true, inputDisabled: false});
+
                 keyboardManager.bind('alt+m', function (e) {
                     e.preventDefault();
                     $location.url('/workspace/monitoring');
                 }, {global: true, inputDisabled: false});
+
                 keyboardManager.bind('alt+d', function (e) {
                     e.preventDefault();
                     elem.find('.highlights-dropdown .dropdown-toggle').click();
@@ -359,18 +363,22 @@
                         elem.find('.dropdown-menu button:focus').parent('li').next().children('button').focus();
                     });
                 }, {global: true, inputDisabled: false});
+
                 keyboardManager.bind('alt+t', function (e) {
                     e.preventDefault();
                     $location.url('/workspace/tasks');
                 }, {global: true, inputDisabled: false});
+
                 keyboardManager.bind('alt+x', function (e) {
                     e.preventDefault();
                     $location.url('/workspace/spike-monitoring');
                 }, {global: true, inputDisabled: false});
+
                 keyboardManager.bind('alt+p', function (e) {
                     e.preventDefault();
                     $location.url('/workspace/personal');
                 }, {global: true, inputDisabled: false});
+
                 keyboardManager.bind('alt+f', function (e) {
                     e.preventDefault();
                     $location.url('search');
