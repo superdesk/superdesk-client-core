9eb7b02abd6a89e9ecc4c58544dd654fc5fe3af6
Naomi Aro <naomiaro@gmail.com>
2016-04-29 16:06:56 +0100

    SDPA-432 : Image won't embed in correct place

--- scripts/superdesk-archive/directives.js
+++ scripts/superdesk-archive/directives.js
@@ -16,6 +16,14 @@
             var dt = event.dataTransfer || event.originalEvent.dataTransfer;
             dt.setData('application/superdesk.item.' + item.type, angular.toJson(item));
             dt.effectAllowed = 'link';
+
+            if (item.renditions && item.renditions.thumbnail) {
+                var img = new Image();
+                var rendition = item.renditions.thumbnail;
+                img.src = rendition.href;
+                img.width = rendition.width;
+                dt.setDragImage(img, rendition.width / 2, rendition.height / 2);
+            }
         };
     }
 
