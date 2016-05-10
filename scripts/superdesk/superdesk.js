499a3a9f2e696f6b79aca12cd37f2dc4d2e5be3f
Petr Jašek <jasekpetr@gmail.com>
2016-05-10 13:05:10 +0200

    make abstract use italics via config (#434)
    * feat(config): add `defaultConfig` provider
    
    allows use to set default config value via `config` callback
    
    * feat(ui): make abstract italic via config
    
    SD-4441

--- scripts/superdesk/superdesk.js
+++ scripts/superdesk/superdesk.js
@@ -93,7 +93,39 @@
             $rootScope.config = config || {};
         }]);
 
-    angular.module('superdesk.config', []);
+    angular.module('superdesk.config', [])
+        .provider('defaultConfig', ['config', function(config) {
+            /**
+             * Set default config value for given key
+             *
+             * key can contain dots, eg. `editor.toolbar`
+             *
+             * @param {String} key
+             * @param {String} val
+             */
+            this.set = function(key, val) {
+                var dest = config;
+                var key_pieces = key.split('.');
+
+                for (var i = 0; i + 1 < key_pieces.length; i++) {
+                    var k = key_pieces[i];
+                    if (!dest.hasOwnProperty(k)) {
+                        dest[k] = {};
+                    }
+
+                    dest = dest[k];
+                }
+
+                var last_key = key_pieces[key_pieces.length - 1];
+                if (!dest.hasOwnProperty(last_key)) {
+                    dest[last_key] = val;
+                }
+            };
+
+            // used only to modify config, noting to return
+            this.$get = angular.noop;
+        }]);
+
     angular.module('superdesk.templates-cache', []);
     angular.module('superdesk', modules);
 })();
