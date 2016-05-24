abb4b9060330328c8c00f4520caefb92aade60ec
actionless <actionless.loveless@gmail.com>
2016-05-24 14:32:24 +0200

    chore(spec): add scripts to mark tests as forced (s/it/fit/)

---
+++ spec/unfit_tests.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -ue
+script_path="$(readlink -e $(dirname "${0}"))"
+
+sed_expression="sed -r"
+
+while IFS='' read -r test_name || [[ -n "${test_name}" ]]; do
+	sed_expression="${sed_expression} -e \"s/ fit\('${test_name}'/ it('${test_name}'/g\""
+done < ${script_path}/smoke_test.list
+
+for file in ${script_path}/*.js ; do
+	eval "${sed_expression} -i ${file}"
+done
