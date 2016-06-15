2ea92492ac82e3bb82eaca4dbab9edc621b74a26
Petr Jasek <jasekpetr@gmail.com>
2016-06-15 14:21:19 +0200

    fix(server): tweak travis config
    - set number of celery processes
    - avoid errors when newrelic key is not set

--- test-server/app.py
+++ test-server/app.py
@@ -1,5 +1,17 @@
 
+import os
+
+
+if os.environ.get('NEW_RELIC_LICENSE_KEY'):
+    try:
+        import newrelic.agent
+        newrelic.agent.initialize(os.path.abspath(os.path.join(os.path.dirname(__file__), 'newrelic.ini')))
+    except ImportError:
+        pass
+
+
 from superdesk.factory import get_app as superdesk_app
+
 
 def get_app(config=None):
     if config is None:
