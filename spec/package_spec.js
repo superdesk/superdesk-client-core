94e82990acaf4778f033a0ac8e0604586ef5b5e6
Syed <sjunaid@aap.com.au>
2016-06-29 12:05:14 +1000

    [SD-4841]-(package_spec) - Optimize e2e tests by combining some of the test cases

--- spec/package_spec.js
+++ spec/package_spec.js
@@ -9,56 +9,66 @@ describe('package', function() {
         monitoring.openMonitoring();
     });
 
-    it('increment package version', function() {
+    it('performs package operations', function() {
+        // increment package version.
         monitoring.actionOnItem('Edit', 3, 0);
         monitoring.actionOnItemSubmenu('Add to current', 'main', 2, 0);
         authoring.save();
         authoring.showVersions();
         expect(element.all(by.repeater('version in versions')).count()).toBe(2);
-    });
 
-    it('reorder item on package', function() {
-        monitoring.actionOnItem('Edit', 3, 0);
-        monitoring.actionOnItemSubmenu('Add to current', 'main', 2, 0);
+        authoring.showVersions(); // close version panel
+
+        // reorder item on package
         monitoring.actionOnItemSubmenu('Add to current', 'story', 3, 2);
-
         authoring.moveToGroup('MAIN', 0, 'STORY', 0);
         expect(authoring.getGroupItems('MAIN').count()).toBe(0);
         expect(authoring.getGroupItems('STORY').count()).toBe(2);
-    });
 
-    xit('add multiple items to package', function() {
-        monitoring.actionOnItem('Edit', 3, 0);
-        monitoring.selectItem(2, 0);
-        monitoring.selectItem(3, 1);
-        //TODO: operation not implemented yet, it will be implemented by SD-3308
-        //monitoring.addToCurrentMultipleItems('MAIN');
-        expect(authoring.getGroupItems('MAIN').count()).toBe(2);
-    });
+        // remove both items one by one to initialize
+        authoring.removeGroupItem('STORY', 0);
+        authoring.removeGroupItem('STORY', 0);
 
-    xit('can preview package in a package', function() {
-        monitoring.actionOnItem('Edit', 3, 0);
-        monitoring.actionOnItemSubmenu('Add to current', 'main', 3, 1);
-        authoring.save();
-        authoring.close();
-        monitoring.previewAction(3, 0);
-        //There is no preview in preview, SD-3319
-    });
-
-    it('create package from multiple items', function() {
+        // create package from multiple items
         monitoring.selectItem(2, 0);
         monitoring.selectItem(2, 1);
         monitoring.createPackageFromItems();
         expect(authoring.getGroupItems('MAIN').count()).toBe(2);
-    });
 
-    it('can add an item to an existing package only once', function() {
+        // remove both items one by one to initialize
+        authoring.removeGroupItem('MAIN', 0);
+        authoring.removeGroupItem('MAIN', 0);
+
+        // can add an item to an existing package only once
         monitoring.actionOnItem('Edit', 3, 0);
         monitoring.actionOnItemSubmenu('Add to current', 'main', 2, 0);
         monitoring.actionOnItemSubmenu('Add to current', 'story', 2, 0);
         authoring.save();
         expect(authoring.getGroupItems('MAIN').count()).toBe(1);
         expect(authoring.getGroupItems('STORY').count()).toBe(0);
+
+        // remove item and unselect to initialize
+        authoring.removeGroupItem('MAIN', 0);
+        monitoring.selectItem(2, 0);
+        monitoring.selectItem(2, 1);
+
+        authoring.save();
+
+        // create package by combining an item with open item
+        monitoring.openAction(2, 1);
+        browser.sleep(500);
+        monitoring.actionOnItem('Combine with current', 3, 0);
+        expect(authoring.getGroupItems('MAIN').count()).toBe(2);
+        // remove both items one by one to initialize
+        authoring.removeGroupItem('MAIN', 0);
+        authoring.removeGroupItem('MAIN', 0);
+
+        // add multiple items to package
+        monitoring.actionOnItem('Edit', 3, 0);
+        monitoring.selectItem(2, 0);
+        monitoring.selectItem(3, 1);
+        monitoring.addToCurrentMultipleItems();
+        expect(authoring.getGroupItems('MAIN').count()).toBe(2);
     });
 
     it('create package from published item', function() {
@@ -76,12 +86,12 @@ describe('package', function() {
         expect(authoring.getGroupItems('MAIN').count()).toBe(1);
     });
 
-    it('create package by combining an item with open item', function() {
-        monitoring.openMonitoring();
-        monitoring.openAction(2, 0);
-        browser.sleep(500);
-        monitoring.actionOnItem('Combine with current', 3, 0);
-        expect(authoring.getGroupItems('MAIN').count()).toBe(2);
+    xit('can preview package in a package', function() {
+        monitoring.actionOnItem('Edit', 3, 0);
+        monitoring.actionOnItemSubmenu('Add to current', 'main', 3, 1);
+        authoring.save();
+        authoring.close();
+        monitoring.previewAction(3, 0);
+        //There is no preview in preview, SD-3319
     });
-
 });
