972c26afa890822ee68e244f6350d0c6e98a6512
Gabriel Aszalos <gabriel.aszalos@gmail.com>
2016-07-20 14:30:07 +0100

    fixed public path issue that was stopping some images from loading

--- tasks/options/webpack-dev-server.js
+++ tasks/options/webpack-dev-server.js
@@ -1,9 +1,21 @@
-var webpackConfig = require('../../webpack.config.js');
+var o = require('../../webpack.config.js');
 
 module.exports = {
     options: {
-        webpack: webpackConfig,
-        publicPath: '/' + webpackConfig.output.publicPath,
+        webpack: {
+            cache: o.cache,
+            entry: o.entry,
+            output: {
+                path: o.output.path,
+                publicPath: 'dist',
+                filename: o.output.filename,
+                chunkFilename: o.output.chunkFilename
+            },
+            plugins: o.plugins,
+            resolve: o.resolve,
+            module: o.module
+        },
+        publicPath: '/dist',
         port: 9000,
         headers: {
             'Cache-Control': 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0'
--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,6 @@ module.exports = {
     },
     output: {
         path: path.join(__dirname, 'dist'),
-        publicPath: 'dist/',
         filename: '[name].bundle.js',
         chunkFilename: '[id].bundle.js'
     },
