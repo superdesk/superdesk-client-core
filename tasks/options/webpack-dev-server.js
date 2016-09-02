397fb5cb7790d57c6a6d96dd4b411157925e7b70
Loic Nogues <loic.nogues@sourcefabric.org>
2016-09-02 12:18:33 +0200

    single quotes instead of double quotes

--- tasks/options/webpack-dev-server.js
+++ tasks/options/webpack-dev-server.js
@@ -9,7 +9,7 @@ module.exports = function(grunt) {
             webpack: webpackConfig,
             publicPath: '/dist',
             port: 9000,
-            host: "0.0.0.0",
+            host: '0.0.0.0',
             headers: {
                 'Cache-Control': 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0'
             }
