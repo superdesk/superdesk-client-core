41fab9039d3b5a9a27ab1e962586bc0ac7003b96
Gabriel Aszalos <gabriel.aszalos@gmail.com>
2016-09-06 11:30:49 +0100

    refactor(search-providers): split 'superdesk-search-providers' module.
    This could be another way to split smaller modules (didn't create
    folders).
    
    `superdesk-search-providers` becomes:
    ```
    .
    ├── views
    │   ├── search-provider-config.html
    │   └── settings.html
    ├── constants.js
    ├── directive.js
    ├── index.js
    └── service.js
    
    1 directory, 6 files
    ```

---
+++ scripts/superdesk-search-providers/constants.js
@@ -0,0 +1,10 @@
+export const providerTypes = {
+    aapmm: 'AAP Multimedia',
+    paimg: 'PA Images',
+    // quick fix to have several scanpix instance (needed for SDNTB-237)
+    // FIXME: temporary fix, need to be refactored (SD-4448)
+    'scanpix(ntbtema)': 'ScanPix (ntbtema)',
+    'scanpix(ntbkultur)': 'ScanPix (ntbkultur)',
+    'scanpix(desk)': 'ScanPix (desk)',
+    'scanpix(npk)': 'ScanPix (npk)',
+};
