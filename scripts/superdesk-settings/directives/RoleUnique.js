9d7a97c4afcd195735be12c96ce8cbfa8bb62836
Gabriel Aszalos <gabriel.aszalos@gmail.com>
2016-09-06 11:40:06 +0100

    refactor(settings): split 'superdesk-settings' module.
    `superdesk-settings` becomes:
    ```
    .
    ├── directives
    │   ├── DateParam.js
    │   ├── RoleUnique.js
    │   ├── SettingsView.js
    │   ├── ValidError.js
    │   └── index.js
    ├── styles
    │   └── settings.less
    ├── views
    │   ├── main.html
    │   └── settings-view.html
    └── index.js
    
    3 directories, 9 files
    ```

---
+++ scripts/superdesk-settings/directives/RoleUnique.js
@@ -0,0 +1,33 @@
+RoleUnique.$inject = ['api', '$q'];
+export function RoleUnique(api, $q) {
+    return {
+        require: 'ngModel',
+        link: function (scope, element, attrs, ctrl) {
+
+            /**
+             * Test if given value is unique for seleted field
+             */
+            function testUnique(modelValue, viewValue) {
+                var value = modelValue || viewValue;
+                if (value) {
+                    var criteria = {where: {'name': value}};
+                    if (scope.editRole != null && scope.editRole._id != null) {
+                        criteria.where._id = {'$ne': scope.editRole._id};
+                    }
+                    return api.roles.query(criteria)
+                        .then(function(roles) {
+                            if (roles._items.length) {
+                                return $q.reject(roles);
+                            }
+                            return roles;
+                        });
+                }
+
+                // mark as ok
+                return $q.when();
+            }
+
+            ctrl.$asyncValidators.unique = testUnique;
+        }
+    };
+}
