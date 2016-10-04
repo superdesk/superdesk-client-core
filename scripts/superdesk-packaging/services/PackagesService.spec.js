e6db7389f091e9ee760d310d83645dd4002d223a
Petr <petr.jasek@sourcefabric.org>
2016-10-04 11:45:16 +0200

    fix(packaging): keep item type in group refs
    it's used on server later when publishing.

---
+++ scripts/superdesk-packaging/services/PackagesService.spec.js
@@ -0,0 +1,12 @@
+describe('packages service', () => {
+
+    beforeEach(window.module('superdesk.packaging'));
+
+    it('can get reference for an item', inject(packages => {
+        var item = {headline: 'foo', type: 'text', _id: 'foo:1'};
+        var ref = packages.getReferenceFor(item);
+        expect(ref.type).toBe(item.type);
+        expect(ref.residRef).toBe(item._id);
+        expect(ref.headline).toBe(item.headline);
+    }));
+});
