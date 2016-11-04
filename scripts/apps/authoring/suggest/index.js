0c6e39d46cd36969947e1bb70306f15ef38d4285
Gabriel Aszalos <gabriel.aszalos@sourcefabric.org>
2016-11-04 09:13:36 +0000

    Removed suggest sub-module from documentation

--- scripts/apps/authoring/suggest/index.js
+++ scripts/apps/authoring/suggest/index.js
@@ -3,14 +3,6 @@ import './styles.scss';
 import SuggestService from './SuggestService';
 import SuggestDirective from './SuggestDirective';
 
-/**
- * @ngdoc module
- * @module apps.authoring.suggest
- * @name apps.authoring.suggest
- * @packageName apps
- * @description The suggest module enriches the authoring component with live
- * suggest functionality.
- */
 export default angular.module('superdesk.apps.authoring.suggest', [])
     .service('suggest', SuggestService)
     .directive('sdSuggest', SuggestDirective);
