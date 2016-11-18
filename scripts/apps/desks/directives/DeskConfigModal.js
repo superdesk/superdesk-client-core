d164e3586aa6765d6bbda231b0c028568d8b8c17
pavlovicnemanja <me@pavlovicnemanja.com>
2016-11-18 15:03:35 +0100

    feat(desks): Added ability to add default language for desks

--- scripts/apps/desks/directives/DeskConfigModal.js
+++ scripts/apps/desks/directives/DeskConfigModal.js
@@ -1,5 +1,17 @@
-DeskConfigModal.$inject = ['metadata', 'content'];
-export function DeskConfigModal(metadata, content) {
+/**
+ * @ngdoc directive
+ * @module superdesk.apps.desks
+ * @name sdDeskConfigModal
+ *
+ * @requires metadata
+ * @requires content
+ * @requires api
+ *
+ * @description Generates modal for editing desks
+ */
+
+DeskConfigModal.$inject = ['metadata', 'content', 'api'];
+export function DeskConfigModal(metadata, content, api) {
     return {
         scope: {
             modalActive: '=active',
@@ -11,12 +23,29 @@ export function DeskConfigModal(metadata, content) {
         require: '^sdDeskConfig',
         templateUrl: 'scripts/apps/desks/views/desk-config-modal.html',
         link: function(scope, elem, attrs, ctrl) {
+
+            /*
+             * Initialize metadata
+             * @return {Object} metadata
+             */
             metadata.initialize().then(function() {
                 scope.metadata = metadata.values;
             });
 
+            /*
+             * Initialize content types
+             * @return {Object} profiles
+             */
             content.getTypes().then(function(profiles) {
                 scope.profiles = profiles;
+            });
+
+            /*
+             * Initialize languages
+             * @return {Object} languages
+             */
+            api.query('languages').then(function (languages) {
+                scope.languages = languages._items;
             });
         }
     };
