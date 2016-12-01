6e01a91278a5b0956c052194916b96c7c7290c56
aca-jov <aca84.jov@gmail.com>
2016-12-01 13:26:30 +0100

    Added listing routes on web site management

--- scripts/apps/web-publisher/directives/index.js
+++ scripts/apps/web-publisher/directives/index.js
@@ -1,3 +1,7 @@
+<<<<<<< 74e0478b76f5eb0aa430c205da2123848c6b8305
 export {SiteRoutesDirective} from './SiteRoutesDirective';
 export {ListArticlesDirective} from './ListArticlesDirective';
 export {CardInputFocusDirective} from './CardInputFocusDirective';
+=======
+export { SiteRoutesDirective } from './SiteRoutesDirective';
+>>>>>>> Added listing routes on web site management
--- scripts/apps/web-publisher/index.js
+++ scripts/apps/web-publisher/index.js
@@ -1,13 +1,9 @@
 import './styles/web-publisher.scss';
 
-<<<<<<< 73c4543c334408bd75671df0f12ed5599fea3d4e
 import {WebPublisherManagerController} from './controllers';
+import {WebPublisherMonitoringController} from './controllers';
 import * as services from './services';
 import * as directive from './directives';
-=======
-import { WebPublisherManagerController } from './controllers';
-import * as services from './services';
->>>>>>> Added web publisher module
 
 /**
  * @ngdoc module
@@ -17,52 +13,34 @@ import * as services from './services';
  * @description Superdesk web publisher module.
  */
 export default angular.module('superdesk.apps.web_publisher', [
-<<<<<<< 73c4543c334408bd75671df0f12ed5599fea3d4e
     'superdesk.core.activity'
 ])
 
-    .directive('sdSiteRoutes', directive.SiteRoutesDirective)
-    .directive('sdListArticles', directive.ListArticlesDirective)
-    .directive('sdCardInputFocus', directive.CardInputFocusDirective)
-    .factory('publisher', services.PublisherFactory)
-    .factory('pubapi', services.PubAPIFactory)
+.directive('sdSiteRoutes', directive.SiteRoutesDirective)
+.directive('sdListArticles', directive.ListArticlesDirective)
+.directive('sdCardInputFocus', directive.CardInputFocusDirective)
+.factory('publisher', services.PublisherFactory)
+.factory('pubapi', services.PubAPIFactory)
 
-    .config(['superdeskProvider', function(superdesk) {
-=======
-    'superdesk.core.activity',
-    'superdesk.config'
-])
-
-    .factory('publisher', services.PublisherFactory)
-    .factory('pubapi', services.PubAPIFactory)
-
-    .config(['superdeskProvider', 'config', function(superdesk, config) {
->>>>>>> Added web publisher module
-        superdesk
-            .activity('/web_publisher/monitoring', {
-                label: gettext('Web Publisher'),
-                description: gettext('Web Publisher'),
-<<<<<<< 73c4543c334408bd75671df0f12ed5599fea3d4e
-=======
-                priority: 100,
-                category: superdesk.MENU_MAIN,
-                adminTools: false,
->>>>>>> Added web publisher module
-                templateUrl: 'scripts/apps/web-publisher/views/monitoring.html',
-                sideTemplateUrl: 'scripts/apps/web-publisher/views/sidenav-items.html'
-            })
-            .activity('/web_publisher/manager', {
-                label: gettext('Web Site Management'),
-                description: gettext('Web Site Management'),
-<<<<<<< 73c4543c334408bd75671df0f12ed5599fea3d4e
-                priority: 100,
-                category: superdesk.MENU_MAIN,
-                adminTools: false,
-=======
->>>>>>> Added web publisher module
-                controller: WebPublisherManagerController,
-                controllerAs: 'webPublisher',
-                templateUrl: 'scripts/apps/web-publisher/views/manager.html',
-                sideTemplateUrl: 'scripts/apps/web-publisher/views/sidenav-items.html'
-            });
-    }]);
+.config(['superdeskProvider', function(superdesk) {
+    superdesk
+        .activity('/web_publisher/monitoring', {
+            label: gettext('Web Publisher'),
+            description: gettext('Web Publisher'),
+            controller: WebPublisherMonitoringController,
+            controllerAs: 'webPublisherMonitoring',
+            templateUrl: 'scripts/apps/web-publisher/views/monitoring/index.html',
+            sideTemplateUrl: 'scripts/apps/web-publisher/views/sidenav-items.html'
+        })
+        .activity('/web_publisher/manager', {
+            label: gettext('Web Site Management'),
+            description: gettext('Web Site Management'),
+            priority: 100,
+            category: superdesk.MENU_MAIN,
+            adminTools: false,
+            controller: WebPublisherManagerController,
+            controllerAs: 'webPublisher',
+            templateUrl: 'scripts/apps/web-publisher/views/manager.html',
+            sideTemplateUrl: 'scripts/apps/web-publisher/views/sidenav-items.html'
+        });
+}]);
