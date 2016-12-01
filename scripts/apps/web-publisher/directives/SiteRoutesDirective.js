fd12a5f4f063ae055ce09cd635d6c404a94a1276
aca-jov <aca84.jov@gmail.com>
2016-12-01 13:26:30 +0100

    Added listing routes on web site management

--- scripts/apps/web-publisher/directives/SiteRoutesDirective.js
+++ scripts/apps/web-publisher/directives/SiteRoutesDirective.js
@@ -7,14 +7,24 @@
  */
 SiteRoutesDirective.$inject = ['publisher'];
 export function SiteRoutesDirective(publisher) {
+<<<<<<< 74e0478b76f5eb0aa430c205da2123848c6b8305
     class SiteRoutes {
+=======
+
+    class SiteRoutes {
+
+>>>>>>> Added listing routes on web site management
         constructor() {
             this.scope = {site: '=site'};
             this.templateUrl = 'scripts/apps/web-publisher/views/site-routes.html';
         }
 
         link(scope) {
+<<<<<<< 74e0478b76f5eb0aa430c205da2123848c6b8305
             scope.$on('refreshRoutes', (e, data) => {
+=======
+            scope.$on('refreshRoutes', function(e, data) {
+>>>>>>> Added listing routes on web site management
                 if (data === scope.site.subdomain) {
                     this._queryItems(scope);
                 }
@@ -33,7 +43,11 @@ export function SiteRoutesDirective(publisher) {
         _queryItems(scope) {
             scope.loading = true;
             publisher.setTenant(scope.site.subdomain);
+<<<<<<< 74e0478b76f5eb0aa430c205da2123848c6b8305
             publisher.queryRoutes({type: 'collection'}).then((routes) => {
+=======
+            publisher.queryRoutes({type: 'collection'}).then(routes => {
+>>>>>>> Added listing routes on web site management
                 scope.loading = false;
                 scope.routes = routes;
             });
