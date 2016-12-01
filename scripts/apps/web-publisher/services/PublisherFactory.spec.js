85ac7dbb503897dd4f8d1718851dbe130f299d96
aca-jov <aca84.jov@gmail.com>
2016-12-01 13:26:30 +0100

    Added listing routes on web site management

--- scripts/apps/web-publisher/services/PublisherFactory.spec.js
+++ scripts/apps/web-publisher/services/PublisherFactory.spec.js
@@ -43,6 +43,54 @@ describe('publisher service', () => {
         spyOn(pubapi, 'query').and.returnValue($q.when([]));
         publisher.queryRoutes();
         $rootScope.$digest();
-        expect(pubapi.query).toHaveBeenCalledWith('content/routes');
+        expect(pubapi.query).toHaveBeenCalledWith('content/routes', undefined);
+    }));
+
+    it('can list menus', inject((publisher, pubapi, $q, $rootScope) => {
+        spyOn(pubapi, 'query').and.returnValue($q.when([]));
+        publisher.queryMenus();
+        $rootScope.$digest();
+        expect(pubapi.query).toHaveBeenCalledWith('menus', {'limit': 100});
+    }));
+
+    it('can get menu 42', inject((publisher, pubapi, $q, $rootScope) => {
+        spyOn(pubapi, 'get').and.returnValue($q.when({}));
+        publisher.getMenu(42);
+        $rootScope.$digest();
+        expect(pubapi.get).toHaveBeenCalledWith('menus', 42);
+    }));
+
+    it('can save a new menu', inject((publisher, pubapi, $q, $rootScope) => {
+        spyOn(pubapi, 'save').and.returnValue($q.when({}));
+        publisher.saveMenu({
+            name: 'Name',
+            label: 'Label'
+        });
+        $rootScope.$digest();
+        expect(pubapi.save).toHaveBeenCalledWith('menus', {menu: {
+            name: 'Name',
+            label: 'Label'
+        }}, undefined);
+    }));
+
+    it('can update an existing menu', inject((publisher, pubapi, $q, $rootScope) => {
+        spyOn(pubapi, 'save').and.returnValue($q.when({}));
+        publisher.saveMenu({
+            id: 34,
+            name: 'Name2',
+            label: 'Label2'
+        });
+        $rootScope.$digest();
+        expect(pubapi.save).toHaveBeenCalledWith('menus', {menu: {
+            name: 'Name2',
+            label: 'Label2'
+        }}, 34);
+    }));
+
+    it('can remove menu 42', inject((publisher, pubapi, $q, $rootScope) => {
+        spyOn(pubapi, 'remove').and.returnValue($q.when({}));
+        publisher.removeMenu(42);
+        $rootScope.$digest();
+        expect(pubapi.remove).toHaveBeenCalledWith('menus', 42);
     }));
 });
