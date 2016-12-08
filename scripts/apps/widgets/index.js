6410c7ebd3d90945d9ad243ef55115586bc8ca37
Petr <petr.jasek@sourcefabric.org>
2016-12-08 12:52:43 +0100

    save

--- scripts/apps/widgets/index.js
+++ scripts/apps/widgets/index.js
@@ -1,57 +1,29 @@
+var _ = require('lodash');
+
+function getText(val) {
+    try {
+        return angular.element(val).text();
+    } catch (err) {
+        return val;
+    }
+}
 
 AnsaSemanticsCtrl.$inject = ['$scope', 'api'];
 function AnsaSemanticsCtrl($scope, api) {
-    this.data = {
-        "newsDomains": [
-            "News"
-        ],
-        "places": [
-            "Sudan"
-        ],
-        "organizations": [
-            "Internal Displacement Monitoring Center"
-        ],
-        "mainGroups": [
-            "erratic rainfall",
-            "irregular rain",
-            "Sudan's temperature",
-            "hotter climate",
-            "flood-related disaster"
-        ],
-        "mainLemmas": [
-            "rain",
-            "Sudan",
-            "drought",
-            "climate change",
-            "crop",
-            "temperature",
-            "Internal Displacement Monitoring Center",
-            "climate",
-            "arable land",
-            "country",
-            "agriculture",
-            "village",
-            "cultivation",
-            "flood",
-            "disaster",
-            "people"
-        ],
-        "mainSenteces": [
-            "Sudan's temperature is expected to increase significantly.",
-            "As a result of hotter climate and erratic rainfall, much of Sudan has become progressively unsuitable for agriculture and villages.",
-            "Irregular rain has ruined crops, and the country is experiencing both droughts and floods -- making arable land unsuitable for cultivation and displacing more than 600,000 people due to flood-related disasters since 2013, according to the Internal Displacement Monitoring Center (IDMC)."
-        ],
-        "mood": -18.488033,
-        "isMOODnegative": false,
-        "isMOODneutral": true,
-        "isMOODpositive": false
+    this.refresh = () => api.save('analysis', {
+        lang: 'ENG',
+        title: $scope.item.headline,
+        text: getText($scope.item.body_html),
+        abstract: getText($scope.item.abstract)
+    }).then((result) => this.data = result.semantics);
+
+    this.remove = (term, category) => {
+        console.warn('remove', term, category);
+        this.data[category] = _.without(this.data[category], term);
     };
 
-    this.refresh = () => api.save('keywords', {
-        headline: $scope.item.headline,
-        abstract: $scope.item.abstract,
-        content: $scope.item.body_html
-    }).then((result) => this.data = result);
+    // init
+    this.refresh();
 }
 
 angular.module('superdesk.apps.widgets', [])
