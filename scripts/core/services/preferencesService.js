42e3587382ec6fdc804a63413cfdfde193b09ba6
Petr Jasek <jasekpetr@gmail.com>
2016-12-13 17:07:42 +0100

    make related items text collapsable

--- scripts/core/services/preferencesService.js
+++ scripts/core/services/preferencesService.js
@@ -215,7 +215,6 @@ export default angular.module('superdesk.core.preferences', ['superdesk.core.not
                         return result;
                     }, (response) => {
                         console.error(response);
-                        notify.error(gettext('User preferences could not be saved...'));
                         deferUpdate.reject(response);
                     })
                     .finally(() => {
