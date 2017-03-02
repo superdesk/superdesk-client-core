77cb702ae6237e3600d76c8546e20576e04cb218
Petr <petr.jasek@sourcefabric.org>
2017-03-02 11:48:48 +0100

    make only picture items draggable

--- scripts/apps/archive/directives/DraggableItem.js
+++ scripts/apps/archive/directives/DraggableItem.js
@@ -3,7 +3,7 @@ DraggableItem.$inject = ['dragitem'];
 export function DraggableItem(dragitem) {
     return {
         link: function(scope, elem) {
-            if (scope.item) {
+            if (scope.item && scope.item.type && scope.item.type === 'picture') {
                 elem.attr('draggable', true);
 
                 // set item data on event
