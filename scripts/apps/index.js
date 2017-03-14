b521bf351be5396d6559c0752db968162e84f423
aca-jov <aca84.jov@gmail.com>
2017-03-14 17:52:20 +0100

    Small fix

--- scripts/apps/index.js
+++ scripts/apps/index.js
@@ -31,22 +31,6 @@ if (withPublisher) {
     require('apps/web-publisher');
 }
 
-if (appConfig.features && appConfig.features.useTansaProofing) {
-    require('apps/tansa');
-}
-
-/* globals __SUPERDESK_CONFIG__: true */
-const appConfig = __SUPERDESK_CONFIG__;
-const withPublisher = typeof appConfig.publisher !== 'undefined';
-
-if (withPublisher) {
-    require('apps/web-publisher');
-}
-
-if (appConfig.features && appConfig.features.useTansaProofing) {
-    require('apps/tansa');
-}
-
 export default angular.module('superdesk.apps', [
     'superdesk.apps.settings',
     'superdesk.apps.dashboard',
@@ -74,7 +58,6 @@ export default angular.module('superdesk.apps', [
     'superdesk.apps.publish',
     'superdesk.apps.templates',
     'superdesk.apps.monitoring',
-    'superdesk.apps.profiling',
-    'superdesk.apps.analytics'
+    'superdesk.apps.profiling'
 ].concat(withPublisher ? 'superdesk.apps.web_publisher' : []));
 
