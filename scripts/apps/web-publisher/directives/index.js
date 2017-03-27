0d02a65af900ea7ee89a25ad88576bef794763da
aca-jov <aca84.jov@gmail.com>
2017-03-27 16:45:28 +0200

    Added incoming and published list with publish pane

--- scripts/apps/web-publisher/directives/index.js
+++ scripts/apps/web-publisher/directives/index.js
@@ -1,3 +1,4 @@
 export {SiteRoutesDirective} from './SiteRoutesDirective';
+export {PublishRoutesDirective} from './PublishRoutesDirective';
 export {ListArticlesDirective} from './ListArticlesDirective';
 export {CardInputFocusDirective} from './CardInputFocusDirective';
