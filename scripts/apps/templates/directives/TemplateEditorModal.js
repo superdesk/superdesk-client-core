38caee126cb4b4c1449af62049339a6a99589a24
Syed <sjunaid@aap.com.au>
2017-04-05 11:36:48 +1000

    [SDESK-503](fix) - Template creation -> inappropriate list gets populated when return is hit and the focus is on _slugline_ or  _takekey_

--- scripts/apps/templates/directives/TemplateEditorModal.js
+++ scripts/apps/templates/directives/TemplateEditorModal.js
@@ -1,5 +1,24 @@
 export function TemplateEditorModal() {
     return {
-        templateUrl: 'scripts/apps/templates/views/template-editor-modal.html'
+        templateUrl: 'scripts/apps/templates/views/template-editor-modal.html',
+        link: function(scope) {
+            _.defer(() => {
+                const ENTER = 13;
+
+                let modalBodyContainer = angular.element('.modal-body');
+
+                function handleKeyDown(event) {
+                    if (event.keyCode === ENTER && event.target.tagName === 'INPUT') {
+                        event.preventDefault();
+                    }
+                }
+
+                modalBodyContainer.on('keydown', handleKeyDown);
+
+                scope.$on('$destroy', () => {
+                    modalBodyContainer.off('keydown', handleKeyDown);
+                });
+            });
+        }
     };
 }
