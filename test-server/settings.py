5d76f59ec0f8fd1b92a003499e7fb84e16d8e45a
Mayur Dhamanwala <mdhamanwala@aap.com.au>
2017-05-31 19:17:52 +1000

    Fixing settings.py so that e2e test don't wipe the local databases.

--- test-server/settings.py
+++ test-server/settings.py
@@ -6,12 +6,21 @@ URL_PROTOCOL = 'http'
 URL_PREFIX = 'api'
 
 CLIENT_URL = 'http://localhost:9090'
-
 REDIS_URL = 'redis://localhost:6379/2'
 
 MONGO_DBNAME = 'superdesk_e2e'
-ARCHIVED_DBNAME = 'superdesk_e2e_archived'
-LEGAL_ARCHIVE_DBNAME = 'superdesk_e2e_legal_archive'
+MONGO_URI = 'mongodb://localhost/%s' % MONGO_DBNAME
 
-ELASTICSEARCH_INDEX = 'superdesk_e2e'
+ARCHIVED_DBNAME = 'superdesk_e2e_archived'
+ARCHIVED_URI = 'mongodb://localhost/%s' % ARCHIVED_DBNAME
+
+LEGAL_ARCHIVE_DBNAME = 'superdesk_e2e_legal_archive'
+LEGAL_ARCHIVE_URI = 'mongodb://localhost/%s' % LEGAL_ARCHIVE_DBNAME
+
+CONTENTAPI_MONGO_DBNAME = 'contentapi_e2e'
+CONTENTAPI_MONGO_URI = 'mongodb://localhost/%s' % CONTENTAPI_MONGO_DBNAME
+
+ELASTICSEARCH_INDEX = MONGO_DBNAME
 ELASTICSEARCH_BACKUPS_PATH = '/tmp/es-backups/'
+
+CONTENTAPI_ELASTICSEARCH_INDEX = CONTENTAPI_MONGO_DBNAME
