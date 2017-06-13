6a35eaaeaa8ef6370ae2ed96cfbe435ec5a0db0b
Petr <petr.jasek@sourcefabric.org>
2017-06-13 15:05:11 +0200

    fix(duplicate): can not duplicate item if default desk is not set
    now it will use current desk as fallback, to be consistent with fetch.
    
    SDESK-1352

--- scripts/apps/archive/controllers/DuplicateController.js
+++ scripts/apps/archive/controllers/DuplicateController.js
@@ -1,13 +1,18 @@
 DuplicateController.$inject = ['api', 'notify', '$rootScope', 'data', 'desks', '$location', 'workspaces', 'session'];
 export function DuplicateController(api, notify, $rootScope, data, desks, $location, workspaces, session) {
-    var currentDeskId;
+    let currentDeskId;
 
     if ($location.path() === '/search' || workspaces.isCustom()) {
         currentDeskId = session.identity.desk || data.item.task.desk;
-    } else {
-        currentDeskId = desks.getCurrentDeskId();
     }
-    api.save('duplicate', {}, {desk: currentDeskId, type: data.item._type, item_id: data.item.item_id}, data.item)
+
+    currentDeskId = currentDeskId || desks.getCurrentDeskId();
+
+    api.save('duplicate', {}, {
+        desk: currentDeskId,
+        type: data.item._type,
+        item_id: data.item.item_id
+    }, data.item)
         .then(() => {
             $rootScope.$broadcast('item:duplicate');
             notify.success(gettext('Item Duplicated'));
