a9bd49a5f266021a8996c27c2462fac3586c8873
Gabriel Aszalos <gabriel.aszalos@sourcefabric.org>
2017-06-19 15:57:04 +0200

    fix(editor3): temporarily remove embed button tooltip
    The tooltip is being removed because it is breaking the input layout.
    When it will be fixed in superdesk-ui-framework it should be added back.
    
    Change-Id: I4148ed8b8e0de4c1ac53f5b1df235aa6f9a00f98

--- scripts/core/editor3/components/embeds/EmbedButton.jsx
+++ scripts/core/editor3/components/embeds/EmbedButton.jsx
@@ -44,7 +44,7 @@ export class EmbedButton extends Component {
         const {dialogOpen} = this.state;
 
         return (
-            <div data-flow={'down'} data-sd-tooltip="Embed content" className="Editor3-styleButton">
+            <div className="Editor3-styleButton">
                 <span onClick={this.showInput}><i className="icon-code" /></span>
                 {dialogOpen ? <EmbedInput onCancel={this.hideInput} /> : null}
             </div>
