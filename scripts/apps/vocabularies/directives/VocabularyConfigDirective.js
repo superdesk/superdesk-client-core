384e59b675f5cf911ac5d29d8c9ba3e01295e94d
Petr <petr.jasek@sourcefabric.org>
2017-06-26 14:18:50 +0200

    feat(cvs): add option to create new vocabulary
    it has predefined schema with name and qcode for now.
    will store data into subject field.
    
    SDESK-1483

--- scripts/apps/vocabularies/directives/VocabularyConfigDirective.js
+++ scripts/apps/vocabularies/directives/VocabularyConfigDirective.js
@@ -1,8 +1,5 @@
 export function VocabularyConfigDirective() {
     return {
-        scope: {
-            vocabulary: '='
-        },
         controller: 'VocabularyConfig',
         templateUrl: 'scripts/apps/vocabularies/views/vocabulary-config.html'
     };
