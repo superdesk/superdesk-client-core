29bafea63a85a5c433623335bebd4c1222687dce
Petr Jasek <jasekpetr@gmail.com>
2017-07-04 11:28:08 +0200

    fix(tansa): make profile ids configurable
    SDNTB-431

--- scripts/apps/authoring/authoring/directives/AuthoringDirective.js
+++ scripts/apps/authoring/authoring/directives/AuthoringDirective.js
@@ -546,10 +546,10 @@ export function AuthoringDirective(superdesk, superdeskFlags, authoringWorkspace
 
                 switch ($scope.item.language) {
                 case 'nb-NO':
-                    window.tansa.settings.profileId = 1;
+                    window.tansa.settings.profileId = _.get($rootScope, 'config.tansa.profile.nb');
                     break;
                 case 'nn-NO':
-                    window.tansa.settings.profileId = 2;
+                    window.tansa.settings.profileId = _.get($rootScope, 'config.tansa.profile.nn');
                     break;
                 }
 
