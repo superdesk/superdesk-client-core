ad387ea7fd3aab8f2375efae5ffb301f0916ccb8
Gabriel Aszalos <gabriel.aszalos@sourcefabric.org>
2017-09-06 10:16:18 +0200

    core/editor3: characters inserted after comments are no longer part of it.
    Change-Id: I132d5a7da0083c8a1c33f0e66273e4295dbc8b12

--- scripts/core/editor3/reducers/comments/index.js
+++ scripts/core/editor3/reducers/comments/index.js
@@ -9,8 +9,10 @@ import {redrawComments, removeInlineStyles, applyInlineStyles} from './styles';
 import {getComments, addComment, replaceComments} from './store';
 import {updateComments} from './comments';
 
+// TODO(gbbr): Remove commenting from all fields except body.
 // TODO(gbbr): Make sure save works when adding only a comment, and no content.
-// TODO(gbbr): Pimp up CommentInput
+// TODO(gbbr): Pimp up CommentInput.
+// TODO(gbbr): Use react-textarea-autosize in CommentInput to allow multi-line comments.
 
 // TODO(gbbr): Weird behaviour around copy/pasting comments around. Can this be fixed?
 // TODO(gbbr): Optimize for speed (for 'insert-characters' mostly). Perhaps we can
