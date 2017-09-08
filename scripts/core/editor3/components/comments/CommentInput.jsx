d0c0e53f56b29dc6f7adb2201908c147ae4af711
Gabriel Aszalos <gabriel.aszalos@sourcefabric.org>
2017-09-08 11:17:22 +0200

    core/editor3: added multi-line commenting to CommentInput
    Change-Id: I91775ab8d2f4a057048a142418998e9e989fb7a1

--- scripts/core/editor3/components/comments/CommentInput.jsx
+++ scripts/core/editor3/components/comments/CommentInput.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {Dropdown} from 'core/ui/components';
+import Textarea from 'react-textarea-autosize';
 
 /**
  * @ngdoc React
@@ -16,7 +17,10 @@ export class CommentInput extends Component {
     constructor(props) {
         super(props);
 
+        this.state = {msg: ''};
         this.onSubmit = this.onSubmit.bind(this);
+        this.onChange = this.onChange.bind(this);
+        this.onKeyUp = this.onKeyUp.bind(this);
     }
 
     /**
@@ -26,34 +30,60 @@ export class CommentInput extends Component {
      * Consequently, it calls the `onSubmit` prop, passing it the value of the text input.
      */
     onSubmit() {
-        const {value} = this.input;
+        const {msg} = this.state;
         const {onSubmit, onCancel} = this.props;
 
-        if (value !== '') {
-            onSubmit(value);
+        if (msg !== '') {
+            onSubmit(msg);
             onCancel();
         }
     }
 
+    /**
+     * @ngdoc method
+     * @name CommentInput#onChange
+     * @description onChange is triggered when the Textarea content changes.
+     */
+    onChange({target}) {
+        this.setState({msg: target.value});
+    }
+
+    onKeyUp({nativeEvent}) {
+        const {keyCode, shiftKey} = nativeEvent;
+
+        if (keyCode === 13 && shiftKey) {
+            this.onSubmit();
+        }
+    }
+
     componentDidMount() {
-        this.input.focus();
+        $(this.input)
+            .find('textarea')
+            .focus();
     }
 
     render() {
+        const {msg} = this.state;
+
         return (
-            <Dropdown open={true}>
-                <input type="text"
-                    ref={(el) => {
-                        this.input = el;
-                    }}
-                    className="sd-line-input__input"
-                    placeholder={gettext('Type comment here')}
-                />
-                <div className="pull-right">
-                    <button className="btn btn--cancel" onClick={this.props.onCancel}>{gettext('Cancel')}</button>
-                    <button className="btn btn--primary" onClick={this.onSubmit}>{gettext('Submit')}</button>
-                </div>
-            </Dropdown>
+            <div className="comment-input">
+                <Dropdown open={true}>
+                    <Textarea
+                        ref={(el) => {
+                            this.input = el;
+                        }}
+                        className="comment-text"
+                        onChange={this.onChange}
+                        placeholder={gettext('Type your comment...')}
+                        onKeyUp={this.onKeyUp}
+                        value={msg}
+                    />
+                    <div className="pull-right">
+                        <button className="btn btn--cancel" onClick={this.props.onCancel}>{gettext('Cancel')}</button>
+                        <button className="btn btn--primary" onClick={this.onSubmit}>{gettext('Submit')}</button>
+                    </div>
+                </Dropdown>
+            </div>
         );
     }
 }
--- scripts/core/editor3/reducers/comments/index.js
+++ scripts/core/editor3/reducers/comments/index.js
@@ -9,9 +9,6 @@ import {redrawComments, removeInlineStyles, applyInlineStyles} from './styles';
 import {getComments, addComment, replaceComments} from './store';
 import {updateComments} from './comments';
 
-// TODO(gbbr): Can not make a selection when the Comment Input is visible.
-// TODO(gbbr): Pimp up CommentInput.
-// TODO(gbbr): Use react-textarea-autosize in CommentInput to allow multi-line comments.
 // TODO(gbbr): Weird behaviour around copy/pasting comments around. Can this be fixed?
 
 export {
