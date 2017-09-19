db36bc28623795e7b563dd64639d68943bed5f7c
Mark Pittaway <mark.pittaway@gmail.com>
2017-09-19 13:04:55 +1000

    chore: Use the same library versions between Superdesk-Core and Superdesk-Planning

--- scripts/apps/search/components/fields/highlights.js
+++ scripts/apps/search/components/fields/highlights.js
@@ -9,5 +9,7 @@ export function highlights(props) {
 }
 
 highlights.propTypes = {
+    // item is passed through to HighlightsInfo directly via props
+    // eslint-disable-next-line react/no-unused-prop-types
     item: PropTypes.any,
 };
