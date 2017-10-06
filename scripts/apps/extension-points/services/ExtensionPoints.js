f0bdb06d73874f2d6039aa1a847d12d32802071c
Markus Herzog <markus@markusherzog.com>
2017-10-06 14:56:15 +0200

    Update comments to reflect recent changes

--- scripts/apps/extension-points/services/ExtensionPoints.js
+++ scripts/apps/extension-points/services/ExtensionPoints.js
@@ -6,12 +6,12 @@
  * @description
  * External superdesk apps can register components that then will be hooked into
  * the core UI.
- * Inject 'extensionPointsProvider' into your module and then:
+ * Inject 'extensionPoints' into your module and then:
  *
- * extensionPointsProvider.register('MY_TYPE', MyConnectedComponent, props, an_array); // do this in config phase
+ * extensionPoints.register('MY_TYPE', MyConnectedComponent, props, data); // do this in config phase
  *
  * where MyConnectedComponent is a React component, the props is a json object that needs to contain at least the
- * redux store and an_array is an array with names
+ * redux store and data is an array with names
  * of variables that your component will receive as props from the parent scope, for example: ['item'].
  *
  * See also ExtensionPointsDirective.
