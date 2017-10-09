ee3099f6e356c28d710318f6c02bbe2e9a667d84
marwoodandrew <amarwood@aap.com.au>
2017-10-10 10:42:54 +1100

    [SDESK-1988] Add genre and desk to related item list

--- scripts/core/itemList/itemList.js
+++ scripts/core/itemList/itemList.js
@@ -1,42 +1,42 @@
 
 angular.module('superdesk.core.itemList', ['superdesk.apps.search'])
-/**
- * @ngdoc directive
- * @module superdesk.core.itemList
- * @name sdRelatedItemListWidget
- * @param {object} options
- * @param {object} itemListOptions
- * @param {object} actions
- * @param {object} loading
- * @description Creates a list of stories to appear in related items widget.
- */
-.directive('sdRelatedItemListWidget', ['notify', 'gettext', 'familyService',
-    function(notify, gettext, familyService) {
-        return {
-            scope: {
-                options: '=',
-                itemListOptions: '=',
-                actions: '=',
-                loading: '='
-            },
-            templateUrl: 'scripts/core/itemList/views/relatedItem-list-widget.html',
-            link: function(scope, element, attrs) {
-                scope.items = null;
-                scope.processedItems = null;
-                scope.selected = null;
-                var oldSearch = null;
-                var itemListListener = null;
-                var optionsListener = null;
+    /**
+     * @ngdoc directive
+     * @module superdesk.core.itemList
+     * @name sdRelatedItemListWidget
+     * @param {object} options
+     * @param {object} itemListOptions
+     * @param {object} actions
+     * @param {object} loading
+     * @description Creates a list of stories to appear in related items widget.
+     */
+    .directive('sdRelatedItemListWidget', ['notify', 'gettext', 'familyService', 'desks',
+        function(notify, gettext, familyService, desks) {
+            return {
+                scope: {
+                    options: '=',
+                    itemListOptions: '=',
+                    actions: '=',
+                    loading: '='
+                },
+                templateUrl: 'scripts/core/itemList/views/relatedItem-list-widget.html',
+                link: function(scope, element, attrs) {
+                    scope.items = null;
+                    scope.processedItems = null;
+                    scope.selected = null;
+                    var oldSearch = null;
+                    var itemListListener = null;
+                    var optionsListener = null;
 
-                /**
-                 * @ngdoc method
-                 * @name sdRelatedItemListWidget#refresh
-                 * @description Fetches and assigns relatable items
-                 */
-                scope.refresh = () => {
-                    if (scope.options.related && scope.hasKeywords()) {
-                        scope.loading = true;
-                        familyService.fetchRelatableItems(scope.itemListOptions.keyword,
+                    /**
+                     * @ngdoc method
+                     * @name sdRelatedItemListWidget#refresh
+                     * @description Fetches and assigns relatable items
+                     */
+                    scope.refresh = () => {
+                        if (scope.options.related && scope.hasKeywords()) {
+                            scope.loading = true;
+                            familyService.fetchRelatableItems(scope.itemListOptions.keyword,
                             scope.itemListOptions.sluglineMatch,
                             scope.options.item.event_id,
                             scope.itemListOptions.modificationDateAfter).then((items) => {
@@ -45,74 +45,92 @@ angular.module('superdesk.core.itemList', ['superdesk.apps.search'])
                             .finally(() => {
                                 scope.loading = false;
                             });
-                    }
-                };
+                        }
+                    };
 
-                /**
-                 * @ngdoc method
-                 * @name sdRelatedItemListWidget#canDisplayItem
-                 * @returns {Boolean}
-                 * @param {object} item
-                 * @description Checks if an item should be displayed in the list
-                 */
-                scope.canDisplayItem = (item) => {
-                    if (!scope.options.searchEnabled) {
-                        return true;
-                    }
+                    /**
+                     * @ngdoc method
+                     * @name sdRelatedItemListWidget#canDisplayItem
+                     * @returns {Boolean}
+                     * @param {object} item
+                     * @description Checks if an item should be displayed in the list
+                     */
+                    scope.canDisplayItem = (item) => {
+                        if (!scope.options.searchEnabled) {
+                            return true;
+                        }
 
-                    return scope.actions.update && scope.actions.update.condition(item) ||
+                        return scope.actions.update && scope.actions.update.condition(item) ||
                     scope.actions.addTake && scope.actions.addTake.condition(item);
-                };
+                    };
 
-                /**
-                 * @ngdoc method
-                 * @name sdRelatedItemListWidget#setProcessedItems
-                 * @description Creates or removes listeners
-                 */
-                var setProcessedItems = () => {
-                    if (scope.options.existingRelations) {
-                        scope.processedItems = scope.options.existingRelations;
-                        itemListListener && itemListListener();
-                        optionsListener && optionsListener();
-                    } else {
-                        optionsListener = scope.$watch('options.related', () => {
-                            if (scope.options.related && scope.options.item) {
-                                if (!scope.options.item.slugline) {
-                                    notify.error(gettext('Error: Slugline required.'));
+                    /**
+                     * @ngdoc method
+                     * @name sdRelatedItemListWidget#deskName
+                     * @returns {String}
+                     * @param {object} item
+                     * @description Extracts the id of the desk the item is on and returns it's name
+                     */
+                    scope.deskName = (item) => desks.deskLookup[item.task.desk].name;
+
+                    /**
+                     * @ngdoc method
+                     * @name sdRelatedItemListWidget#deskStage
+                     * @returns {String}
+                     * @param {object} item
+                     * @description Extracts the id of the stage the item is on and returns it's name
+                     */
+                    scope.deskStage = (item) => desks.stageLookup[item.task.stage].name;
+
+                    /**
+                     * @ngdoc method
+                     * @name sdRelatedItemListWidget#setProcessedItems
+                     * @description Creates or removes listeners
+                     */
+                    var setProcessedItems = () => {
+                        if (scope.options.existingRelations) {
+                            scope.processedItems = scope.options.existingRelations;
+                            itemListListener && itemListListener();
+                            optionsListener && optionsListener();
+                        } else {
+                            optionsListener = scope.$watch('options.related', () => {
+                                if (scope.options.related && scope.options.item) {
+                                    if (!scope.options.item.slugline) {
+                                        notify.error(gettext('Error: Slugline required.'));
+                                    } else {
+                                        oldSearch = scope.itemListOptions.keyword;
+                                        scope.itemListOptions.keyword = scope.options.item.slugline;
+                                        scope.refresh();
+                                    }
                                 } else {
-                                    oldSearch = scope.itemListOptions.keyword;
-                                    scope.itemListOptions.keyword = scope.options.item.slugline;
-                                    scope.refresh();
+                                    scope.itemListOptions.keyword = oldSearch || null;
                                 }
-                            } else {
-                                scope.itemListOptions.keyword = oldSearch || null;
-                            }
-                        });
-                    }
-                };
+                            });
+                        }
+                    };
 
-                /**
-                 * @ngdoc method
-                 * @name sdRelatedItemListWidget#isPublished
-                 * @returns {Boolean}
-                 * @param {object} item
-                 * @description Checks if an item is in published state
-                 */
-                scope.isPublished = (item) => _.includes(['published', 'killed', 'scheduled', 'corrected'],
+                    /**
+                     * @ngdoc method
+                     * @name sdRelatedItemListWidget#isPublished
+                     * @returns {Boolean}
+                     * @param {object} item
+                     * @description Checks if an item is in published state
+                     */
+                    scope.isPublished = (item) => _.includes(['published', 'killed', 'scheduled', 'corrected'],
                     item.state);
 
-                scope.$watch('options.existingRelations', (newVal, oldVal) => {
-                    if (newVal !== oldVal) {
-                        setProcessedItems();
-                    }
-                });
+                    scope.$watch('options.existingRelations', (newVal, oldVal) => {
+                        if (newVal !== oldVal) {
+                            setProcessedItems();
+                        }
+                    });
 
-                scope.hasKeywords = () => scope.itemListOptions.keyword &&
+                    scope.hasKeywords = () => scope.itemListOptions.keyword &&
                     scope.itemListOptions.keyword.trim().length >= 2;
 
-                scope.view = (item) => {
-                    scope.selected = item;
-                };
-            }
-        };
-    }]);
+                    scope.view = (item) => {
+                        scope.selected = item;
+                    };
+                }
+            };
+        }]);
