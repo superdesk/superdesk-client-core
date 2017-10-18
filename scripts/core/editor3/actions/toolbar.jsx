320de094a323cc6328952ca250b93761f4164d57
Gabriel Aszalos <gabriel.aszalos@sourcefabric.org>
2017-10-18 11:19:17 +0200

    core/editor3: consolidate popup code
    Cleaned up toolbar state and left only one state object indicating the
    type of popup that should be opened and the metadata that needs to be
    passed to it.
    
    Additionally created a new action which takes a highlight type and
    returns the correct configuration, augmenting it with user information.
    
    Change-Id: Ib2937ffa3af2f900c3496425b653e827c823c2b3

--- scripts/core/editor3/actions/toolbar.jsx
+++ scripts/core/editor3/actions/toolbar.jsx
@@ -56,21 +56,38 @@ export function toggleInlineStyle(inlineStyle) {
 /**
  * @ngdoc method
  * @name applyComment
- * @param {SelectionState} selection
+ * @param {SelectionState} s Selection that the comment applies to.
+ * @param {string} msg Comment body.
  * @return {String} action
  * @description Triggers the action to add a comment to the given selection.
  */
-export function applyComment(selection, msg) {
+export const applyComment = (s, data) => applyHighlight('COMMENT', s, data);
+
+/**
+ * @ngdoc method
+ * @name applyAnnotation
+ * @param {SelectionState} sel Selection that the annotation applies to.
+ * @param {string} msg Annotation body.
+ * @return {String} action
+ * @description Triggers the action to add a comment to the given selection.
+ */
+export const applyAnnotation = (s, data) => applyHighlight('ANNOTATION', s, data);
+
+// applyHighlights creates an action that applies the highlight of the given type to
+// selection and contains the given meta data.
+function applyHighlight(type, selection, data) {
     const date = new Date();
     const {display_name: author, email, picture_url: avatar} = ng.get('session').identity;
-    const type = 'ANNOTATION';
+
+    data.author = author;
+    data.email = email;
+    data.date = date;
+    data.avatar = avatar;
+    data.type = type;
 
     return {
         type: 'TOOLBAR_ADD_HIGHLIGHT',
-        payload: {
-            data: {msg, author, email, date, avatar, type},
-            selection: selection
-        }
+        payload: {data, selection}
     };
 }
 
--- scripts/core/editor3/components/toolbar/index.jsx
+++ scripts/core/editor3/components/toolbar/index.jsx
@@ -14,6 +14,20 @@ import classNames from 'classnames';
 import * as actions from '../../actions';
 
 /**
+ * @typedef PopupMeta
+ * @property {PopupTypes} type Popup type
+ * @property {Object} data Information required by this popup type (for example
+ * a selection for adding the comment or annotation too)
+ */
+
+const PopupTypes = {
+    Hidden: 'NONE',
+    Annotation: 'ANNOTATION',
+    Comment: 'COMMENT',
+    Link: 'LINK'
+};
+
+/**
  * @ngdoc React
  * @module superdesk.core.editor3
  * @name Toolbar
@@ -27,19 +41,14 @@ class ToolbarComponent extends Component {
         this.scrollContainer = $(props.scrollContainer || window);
 
         this.onScroll = this.onScroll.bind(this);
-        this.showLinkInput = this.showLinkInput.bind(this);
-        this.hideLinkInput = this.hideLinkInput.bind(this);
-        this.showCommentInput = this.showCommentInput.bind(this);
-        this.hideCommentInput = this.hideCommentInput.bind(this);
+        this.showPopup = this.showPopup.bind(this);
+        this.hidePopup = this.hidePopup.bind(this);
 
         this.state = {
-            // If non-null, it contains the link that is currently being edited
-            // (or empty for a new link) and the popup is shown.
-            editedLink: null,
-
-            // If non-null, contains a DraftJS SelectionState where a comment
-            // needs to be added (the popup is displayed).
-            selectionForComment: null,
+            // Displays the pop-up of the given type, if set to a value different than
+            // PopupTypes.Hidden. The state object value is of type PopupMeta (described
+            // at the top of the file).
+            popup: {type: PopupTypes.Hidden},
 
             // When true, the toolbar is floating at the top of the item. This
             // helps the toolbar continue to be visible when it goes out of view
@@ -73,39 +82,26 @@ class ToolbarComponent extends Component {
 
     /**
      * @ngdoc method
-     * @name Toolbar#hideLinkInput
-     * @description Hides the link input.
+     * @name Toolbar#showPopup
+     * @param {PopupTypes} type The type of pop-up to show.
+     * @description Returns a handler function which creates the popup of the given type.
+     * The function optionally expects as a parameter information to be passed onto the
+     * popup.
+     * @return {Function}
      */
-    hideLinkInput() {
-        this.setState({editedLink: null});
+    showPopup(type) {
+        return (data) => {
+            this.setState({popup: {type, data}});
+        };
     }
 
     /**
      * @ngdoc method
-     * @name Toolbar#showLinkInput
-     * @param {Event} e
-     * @param {Object} link object to edit
-     * existing link.
-     * @description Shows the link input box.
+     * @name Toolbar#hidePopup
+     * @description Hides any visible pop-up.
      */
-    showLinkInput(link) {
-        const isNewLink = !link;
-        const isCollapsed = this.props.editorState.getSelection().isCollapsed();
-
-        // only add new links if there is a selection
-        if (isNewLink && isCollapsed) {
-            return;
-        }
-
-        this.setState({editedLink: link});
-    }
-
-    showCommentInput(selection) {
-        this.setState({selectionForComment: selection});
-    }
-
-    hideCommentInput() {
-        this.setState({selectionForComment: null});
+    hidePopup() {
+        this.setState({popup: PopupTypes.Hidden});
     }
 
     componentDidMount() {
@@ -125,12 +121,13 @@ class ToolbarComponent extends Component {
             applyLink,
             editorState,
             applyComment,
+            applyAnnotation,
             addTable,
             insertImages,
             allowsHighlights,
         } = this.props;
 
-        const {editedLink, selectionForComment} = this.state;
+        const {popup} = this.state;
         const has = (opt) => editorFormat.indexOf(opt) > -1;
 
         const cx = classNames({
@@ -146,19 +143,14 @@ class ToolbarComponent extends Component {
 
                 {has('anchor') &&
                     <SelectionButton
-                        onClick={this.showLinkInput}
+                        onClick={this.showPopup(PopupTypes.Link)}
                         iconName="link"
                         tooltip={gettext('Link')}
-                    />}
-
-                {editedLink !== null &&
-                    <LinkInput
-                        editorState={editorState}
-                        onSubmit={applyLink}
-                        onCancel={this.hideLinkInput}
-                        value={editedLink} />}
-
-                {has('embed') && <EmbedButton />}
+                    />
+                }
+                {has('embed') &&
+                    <EmbedButton />
+                }
                 {has('picture') &&
                     <IconButton
                         onClick={insertImages}
@@ -173,29 +165,41 @@ class ToolbarComponent extends Component {
                         iconName="table"
                     />
                 }
-
                 {allowsHighlights && [
                     <SelectionButton
-                        onClick={this.showCommentInput}
+                        onClick={this.showPopup(PopupTypes.Comment)}
                         key="comment-button"
                         iconName="comment"
                         tooltip={gettext('Add comment')}
                     />,
                     <SelectionButton
-                        onClick={this.showCommentInput}
+                        onClick={this.showPopup(PopupTypes.Annotation)}
                         key="annotation-button"
                         iconName="pencil"
                         tooltip={gettext('Add annotation')}
                     />
                 ]}
 
-                {selectionForComment !== null &&
+                {/* TODO: Perhaps it would be cleaner to have popups in a separate component */}
+                {popup.type === PopupTypes.Link &&
+                    <LinkInput
+                        editorState={editorState}
+                        onSubmit={applyLink}
+                        onCancel={this.hidePopup}
+                        value={popup.data} />}
+
+                {popup.type === PopupTypes.Comment &&
                     <CommentInput
-                        onSubmit={(msg) => applyComment(selectionForComment, msg)}
-                        onCancel={this.hideCommentInput} />}
+                        onSubmit={(msg) => applyComment(popup.data, {msg})}
+                        onCancel={this.hidePopup} />}
+
+                {popup.type === PopupTypes.Annotation &&
+                    <CommentInput
+                        onSubmit={(msg) => applyAnnotation(popup.data, {msg})}
+                        onCancel={this.hidePopup} />}
 
                 {/* LinkToolbar must be the last node. */}
-                <LinkToolbar onEdit={this.showLinkInput} />
+                <LinkToolbar onEdit={this.showPopup(PopupTypes.Link)} />
             </div>;
     }
 }
@@ -209,6 +213,7 @@ ToolbarComponent.propTypes = {
     addTable: PropTypes.func,
     insertImages: PropTypes.func,
     applyComment: PropTypes.func,
+    applyAnnotation: PropTypes.func,
     editorState: PropTypes.object,
     editorNode: PropTypes.object,
     scrollContainer: PropTypes.string
@@ -221,6 +226,7 @@ const mapStateToProps = ({editorFormat, editorState, activeCell, allowsHighlight
 const mapDispatchToProps = (dispatch) => ({
     applyLink: (link, entity = null) => dispatch(actions.applyLink({link, entity})),
     applyComment: (sel, msg) => dispatch(actions.applyComment(sel, msg)),
+    applyAnnotation: (sel, msg) => dispatch(actions.applyAnnotation(sel, msg)),
     insertImages: () => dispatch(actions.insertImages()),
     addTable: () => dispatch(actions.addTable(1, 2))
 });
