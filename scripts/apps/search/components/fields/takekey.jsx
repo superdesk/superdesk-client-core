cde96aa6ece34441db3e9f127be37e1849fe187e
Mugur Rus <mugur.rus@sourcefabric.org>
2017-12-06 00:12:48 +0200

    change label for duplicate items in items list

--- scripts/apps/search/components/fields/takekey.jsx
+++ scripts/apps/search/components/fields/takekey.jsx
@@ -4,6 +4,10 @@ import PropTypes from 'prop-types';
 export function takekey(props) {
     const {gettextCatalog} = props.svc;
 
+    if (props.item.rewrite_of) {
+        return React.createElement('span', {className: 'takekey', key: 'takekey'},
+            gettextCatalog.getString('duplicated'));
+    }
     if (props.item.anpa_take_key) {
         return React.createElement('span', {className: 'takekey', key: 'takekey'},
             gettextCatalog.getString(props.item.anpa_take_key));
