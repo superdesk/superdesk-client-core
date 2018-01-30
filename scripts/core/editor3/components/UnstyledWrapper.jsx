ac99414e3312119c4ed35011a5ca0ec5d5646df1
Petr Jasek <jasekpetr@gmail.com>
2018-01-30 14:52:53 +0100

    feat(editor3): allow media block drag&drop
    SDESK-2006

---
+++ scripts/core/editor3/components/UnstyledWrapper.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import BaseUnstyledComponent from './BaseUnstyledComponent';
+
+/**
+ * Custom wrapper for draft unstyled blocks.
+ *
+ * It allows moving block above existing blocks, adding a dropzone
+ * above first unstyled block.
+ */
+class UnstyledWrapper extends BaseUnstyledComponent {
+    constructor(props) {
+        super(props);
+        this.dropInsertionMode = 'before';
+    }
+
+    getDropBlockKey() {
+        return this.props.children[0].key;
+    }
+
+    render() {
+        const className = 'unstyled ' + (this.state.over ? 'unstyled--over' : '');
+
+        return (
+            <div className={className} ref={(div) => this.div = div}>
+                {this.props.children}
+            </div>
+        );
+    }
+}
+
+UnstyledWrapper.propTypes = {
+    dispatch: PropTypes.func,
+    children: PropTypes.array,
+};
+
+export default UnstyledWrapper;
