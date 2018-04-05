e01c7e1263b1110b0117a4df8edc4c7b3b13b228
tomas <t.kikutis@gmail.com>
2018-04-05 08:39:00 +0300

    Fix validating unresolved suggestions before publishing

--- scripts/core/editor3/helpers/highlights.js
+++ scripts/core/editor3/helpers/highlights.js
@@ -1,4 +1,4 @@
-import {RichUtils, EditorState, convertFromRaw, Modifier} from 'draft-js';
+import {RichUtils, EditorState, Modifier} from 'draft-js';
 import {highlightsConfig} from '../highlightsConfig';
 import {editor3DataKeys, getCustomDataFromEditor, setCustomDataForEditor} from './editor3CustomData';
 import {getDraftCharacterListForSelection} from './getDraftCharacterListForSelection';
@@ -817,9 +817,7 @@ export function getSuggestionData(editorState, styleName) {
     };
 }
 
-export function fieldHasUnresolvedSuggestions(rawState) {
-    const contentState = convertFromRaw(rawState);
-    const editorState = EditorState.createWithContent(contentState);
+export function fieldHasUnresolvedSuggestions(editorState) {
     const highlights = getHighlightsState(editorState);
 
     return Object.keys(highlights.highlightsData || {})
