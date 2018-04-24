dadc3430f671ecf0d1148a32a6bf2a2edcccb765
Pablo Varela <pablovarela182@gmail.com>
2018-04-24 16:16:08 +0200

    [SDESK-2807] Register block and inline changes as one for UNDO action

--- scripts/core/editor3/helpers/removeFormat.js
+++ scripts/core/editor3/helpers/removeFormat.js
@@ -4,57 +4,49 @@ import {acceptedInlineStyles} from '../helpers/inlineStyles';
 /*
  * @ngdoc method
  * @name removeInlineStyles
- * @param {Object} editorState
- * @description removes all inline styles from selection
+ * @param {Object} content ContentState
+ * @param {Object} selection SelectionState
+ * @description Remove all inline styles from selection
+ * @return {Object} ContentState
  */
-export function removeInlineStyles(editorState) {
+export function removeInlineStyles(content, selection) {
     const contentWithoutStyles = acceptedInlineStyles.reduce(
         (newContentState, style) =>
             Modifier.removeInlineStyle(
                 newContentState,
-                editorState.getSelection(),
+                selection,
                 style
             ),
-        editorState.getCurrentContent()
+        content
     );
 
-    return EditorState.push(
-        editorState,
-        contentWithoutStyles,
-        'change-inline-style'
-    );
+    return contentWithoutStyles;
 }
 
 /*
  * @ngdoc method
  * @name removeBlockStyles
- * @param {Object} editorState
- * @param {Array} keepTypes
- * @description Set all blocks in selection to unstyled except 'keepTypes'
+ * @param {Object} content ContentState
+ * @param {Object} selection SelectionState
+ * @description Set all blocks in selection to unstyled except keepTypes
+ * @return {Object} ContentState
  */
-export function removeBlockStyles(editorState, keepTypes) {
-    const content = editorState.getCurrentContent();
-
-    const blockArray = content.getBlocksAsArray().map((block) =>
-        keepTypes.includes(block.getType())
-            ? block
-            : new ContentBlock({
-                type: 'unstyled',
-                text: block.getText(),
-                key: block.getKey(),
-                characterList: block.getCharacterList(),
-                data: block.getData(),
-            }));
-
-    // create new ContentState from Blocks array
-    const contentWithoutBlockStyles = ContentState.createFromBlockArray(
-        blockArray
+export function removeBlockStyles(content, selection, keepTypes) {
+    const blockArray = content.getBlocksAsArray().map(
+        (block) =>
+            keepTypes.includes(block.getType())
+                ? block
+                : new ContentBlock({
+                    type: 'unstyled',
+                    text: block.getText(),
+                    key: block.getKey(),
+                    characterList: block.getCharacterList(),
+                    data: block.getData(),
+                })
     );
 
-    return EditorState.push(
-        editorState,
-        contentWithoutBlockStyles,
-        'change-block-type'
+    return ContentState.createFromBlockArray(
+        blockArray
     );
 }
 
@@ -63,7 +55,13 @@ export function removeBlockStyles(editorState, keepTypes) {
  * @name removeFormatFromState
  * @param {Object} editorState
  * @description Set all blocks in selection to unstyled except atomic blocks
+ * @return {Object} EditorState
  */
 export function removeFormatFromState(editorState) {
-    return removeBlockStyles(removeInlineStyles(editorState), ['atomic']);
+    const selection = editorState.getSelection();
+    const contentWithoutInlineStyles = removeInlineStyles(editorState.getCurrentContent(), selection);
+    const contentWithoutBlockStyles = removeBlockStyles(contentWithoutInlineStyles, selection, ['atomic']);
+
+    // Push new editor state as only one change so 'UNDO' will change both at the same type
+    return EditorState.push(editorState, contentWithoutBlockStyles, 'change-block-type');
 }
