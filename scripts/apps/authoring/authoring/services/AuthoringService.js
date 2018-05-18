89fb285283232cbd243fc01659122380963d6529
tomas <t.kikutis@gmail.com>
2018-05-18 13:53:54 +0300

    Move generation of annotations to `autosave`, because `save` doesn't get called on corrections

--- scripts/apps/authoring/authoring/services/AuthoringService.js
+++ scripts/apps/authoring/authoring/services/AuthoringService.js
@@ -1,7 +1,4 @@
-import {convertFromRaw} from 'draft-js';
-import {toHTML} from 'core/editor3';
 import * as helpers from 'apps/authoring/authoring/helpers';
-import {fieldsMetaKeys, getFieldMetadata} from 'core/editor3/helpers/fieldsMeta';
 
 /**
  * @ngdoc service
@@ -22,14 +19,13 @@ import {fieldsMetaKeys, getFieldMetadata} from 'core/editor3/helpers/fieldsMeta'
  * @requires $injector
  * @requires moment
  * @requires config
- * @requires logger
  *
  * @description Authoring Service is responsible for management of the actions on a story
  */
 AuthoringService.$inject = ['$q', '$location', 'api', 'lock', 'autosave', 'confirm', 'privileges',
-    'desks', 'superdeskFlags', 'notify', 'session', '$injector', 'moment', 'config', 'logger'];
+    'desks', 'superdeskFlags', 'notify', 'session', '$injector', 'moment', 'config'];
 export function AuthoringService($q, $location, api, lock, autosave, confirm, privileges, desks, superdeskFlags,
-    notify, session, $injector, moment, config, logger) {
+    notify, session, $injector, moment, config) {
     var self = this;
 
     // TODO: have to trap desk update event for refereshing users desks.
@@ -253,36 +249,6 @@ export function AuthoringService($q, $location, api, lock, autosave, confirm, pr
     };
 
     /**
-     * Generate item annotations field
-     *
-     * @param {Object} item
-     */
-    this.generateAnnotations = (item) => {
-        const state = getFieldMetadata(item, 'body_html', fieldsMetaKeys.draftjsState);
-
-        if (state) {
-            let highlightsBlock = state.blocks[0];
-
-            if (highlightsBlock.data && highlightsBlock.data.MULTIPLE_HIGHLIGHTS &&
-                highlightsBlock.data.MULTIPLE_HIGHLIGHTS.highlightsData) {
-                let annotations = [];
-
-                _.forEach(highlightsBlock.data.MULTIPLE_HIGHLIGHTS.highlightsData, (highlight, key) => {
-                    if (key.startsWith('ANNOTATION-')) {
-                        let annotation = {};
-
-                        annotation.id = key.split('-')[1];
-                        annotation.type = highlight.data.annotationType;
-                        annotation.body = toHTML(convertFromRaw(JSON.parse(highlight.data.msg)), logger);
-                        annotations.push(annotation);
-                    }
-                });
-                item.annotations = annotations;
-            }
-        }
-    };
-
-    /**
      * Autosave the changes
      *
      * @param {Object} item
@@ -299,8 +265,6 @@ export function AuthoringService($q, $location, api, lock, autosave, confirm, pr
      * @param {Object} item
      */
     this.save = function saveAuthoring(origItem, item) {
-        this.generateAnnotations(item);
-
         var diff = helpers.extendItem({}, item);
         // Finding if all the keys are dirty for real
 
