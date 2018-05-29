d3b349c8914ad2f83d557ee33c61bd3c994b1ccd
tomas <t.kikutis@gmail.com>
2018-05-29 16:25:39 +0300

    rollback old webpack processing

--- webpack.config.js
+++ webpack.config.js
@@ -19,19 +19,8 @@ module.exports = function makeConfig(grunt) {
     // such as 'babel' or 'eslint'. This is to avoid including node_modules into
     // these loaders, but not node modules that are superdesk apps.
     const shouldExclude = function(p) {
-        // superdesk-planning is outside of the project directory, but still needs to be processed
-        if (p.includes('superdesk-planning')) {
-            return false;
-        }
-
-        if (p.startsWith(__dirname) === false) {
-            // exclude code outside of project directory
-            // useful when symlinking node_modules
-            return true;
-        }
-
         // don't exclude anything outside node_modules
-        if (p.startsWith(`${__dirname}/node_modules`) === false) {
+        if (p.indexOf('node_modules') === -1) {
             return false;
         }
 
