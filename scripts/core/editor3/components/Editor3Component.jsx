ee0ae46326c33e9b0c9feaa0201af2ed357c892a
ioan.pocol <ioan.pocol@sourcefabric.org>
2018-06-22 18:57:22 +0300

    fix(editor3): Change the way is checked the presence of editorNode

--- scripts/core/editor3/components/Editor3Component.jsx
+++ scripts/core/editor3/components/Editor3Component.jsx
@@ -428,7 +428,7 @@ export class Editor3Component extends React.Component {
 
                             const selectionRect = getVisibleSelectionRect(window);
 
-                            if (this.editorNode.dataset != null && selectionRect != null) {
+                            if (this.editorNode != null && selectionRect != null) {
                                 this.editorNode.dataset.editorSelectionRect = JSON.stringify(selectionRect);
                             }
 
