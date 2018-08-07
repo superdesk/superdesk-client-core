b7c71a71dc812898b527af7d2ad533dd79338b42
tomas <t.kikutis@gmail.com>
2018-08-07 13:49:13 +0300

    Don't apply saved selection if start/end containers don't match

--- scripts/core/global-fixes/remember-selection-after-losing-focus-from-contenteditable.js
+++ scripts/core/global-fixes/remember-selection-after-losing-focus-from-contenteditable.js
@@ -4,14 +4,19 @@
 // on every mouse and keyboard event.
 
 import {isElementInViewport} from '../helpers/dom/isElementInViewport';
+import {findParent} from '../helpers/dom/findParent';
 
 // DOM Elements are used as keys in order not to have to set attributes which might not work well with React
 // Using a WeakMap also removes the need to generate ids and automatically garbage collects.
 let savedSelections = new WeakMap();
 
 function restoreSelection(savedSelectionRanges) {
-    // `parentElement` is used because `startContainer` is a text node and doesn't have the method `scrollIntoView`
-    const elementToScrollIntoView = savedSelectionRanges[0].startContainer.parentElement;
+    // `findParent` is used because `startContainer` might be a text node and not have a method `scrollIntoView`
+    const elementToScrollIntoView = findParent(
+        savedSelectionRanges[0].startContainer,
+        (el) => typeof el.scrollIntoView === 'function',
+        true
+    );
 
     if (isElementInViewport(elementToScrollIntoView) === false) {
         elementToScrollIntoView.scrollIntoView();
@@ -35,28 +40,41 @@ function updateSelectionPosition() {
 
     const selection = window.getSelection();
 
-    const currentSelectionRanges = new Array(selection.rangeCount)
+    const currentSelectionRangesMeta = new Array(selection.rangeCount)
         .fill()
-        .map((_, i) => selection.getRangeAt(i));
+        .map((_, i) => {
+            const range = selection.getRangeAt(i);
 
-    if (currentSelectionRanges.some((range) => range.startContainer.nodeType !== 3)) {
+            return {
+                range: range,
+                originalStartContainer: range.startContainer,
+                originalEndContainer: range.endContainer,
+            };
+        });
+
+    if (currentSelectionRangesMeta.some(({range}) => range.startContainer.nodeType !== 3)) {
         // prevent saving selections on non-text nodes. Think media inside contenteditable.
         return;
     }
-
-    savedSelections.set(activeElement, currentSelectionRanges);
+    savedSelections.set(activeElement, currentSelectionRangesMeta);
 }
 
 HTMLElement.prototype.focus = (function(originalFocus) {
     return function(...args) {
         // `this` refers to DOM Element receiving focus
-        const savedSelectionRanges = savedSelections.get(this);
+        const savedSelectionRangesMeta = savedSelections.get(this);
 
-        const savedSelectionExists = Array.isArray(savedSelectionRanges) === true
-            && savedSelectionRanges.length > 0;
+        const savedSelectionExists = Array.isArray(savedSelectionRangesMeta) === true
+            && savedSelectionRangesMeta.length > 0
+            && savedSelectionRangesMeta.every(
+                ({range, originalStartContainer, originalEndContainer}) =>
+                    range.startContainer === originalStartContainer
+                    && range.endContainer === originalEndContainer
+                    && document.body.contains(range.startContainer) && document.body.contains(range.endContainer)
+            );
 
         if (this.getAttribute('contenteditable') === 'true' && savedSelectionExists) {
-            restoreSelection(savedSelectionRanges);
+            restoreSelection(savedSelectionRangesMeta.map(({range}) => range));
         } else {
             originalFocus.call(this, ...args);
         }
