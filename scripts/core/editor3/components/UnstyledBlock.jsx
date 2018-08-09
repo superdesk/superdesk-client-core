756dfc7b0e82b32fbbf75afd80dac67c1210d12e
Pablo Varela <pablovarela182@gmail.com>
2018-08-09 13:38:03 +0200

    Remove unused import

--- scripts/core/editor3/components/UnstyledBlock.jsx
+++ scripts/core/editor3/components/UnstyledBlock.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {omit} from 'lodash';
-import {connect} from 'react-redux';
 
 import BaseUnstyledComponent from './BaseUnstyledComponent';
 
