d08778b09d0a4b14ebce9d62d153c330a53d941f
tomas <t.kikutis@gmail.com>
2018-08-10 19:09:51 +0300

    Add globals

--- scripts/globals.d.ts
+++ scripts/globals.d.ts
@@ -1,2 +1,15 @@
 declare const $ : any; // jquery
 declare const gettext: any;
+declare const KV: any; // qumu widgets
+
+// angular
+declare const angular: any;
+declare const inject: any;
+
+// testing
+declare const jasmine: any;
+declare const spyOn: any;
+declare const describe: any;
+declare const beforeEach: any;
+declare const expect: any;
+declare const it: any;
