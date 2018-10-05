3751943c14f4acb73f36c16822504c9cc126d8bb
Petr Jasek <jasekpetr@gmail.com>
2018-10-05 15:13:59 +0200

    feat(upload): show all fields from picture profile in upload form
    it will also add fields defined in validator for now, it could be
    obsolete but keeping it for backward compatibility.
    
    SDANSA-197

--- scripts/apps/authoring/media/MediaMetadataEditorDirective.js
+++ scripts/apps/authoring/media/MediaMetadataEditorDirective.js
@@ -1,5 +1,12 @@
-MediaMetadataEditorDirective.$inject = ['metadata'];
-export default function MediaMetadataEditorDirective(metadata) {
+import {max, sortBy} from 'lodash';
+import {getLabelNameResolver} from 'apps/workspace/helpers/getLabelForFieldId';
+
+MediaMetadataEditorDirective.$inject = ['metadata', 'content'];
+export default function MediaMetadataEditorDirective(metadata, content) {
+    function getCV(field) {
+        return metadata.cvs.find((cv) => !cv.field_type && (cv._id === field || cv.schema_field === field));
+    }
+
     return {
         scope: {
             item: '=',
@@ -14,7 +21,43 @@ export default function MediaMetadataEditorDirective(metadata) {
         },
         template: require('./views/media-metadata-editor-directive.html'),
         link: (scope) => {
-            // pass
+            getLabelNameResolver().then((getLabelForFieldId) => {
+                const editor = content.editor({}, 'picture');
+                const schema = content.schema({}, 'picture');
+
+                // get last order
+                let nextOrder = max(Object.keys(editor).map((field) => editor[field].order || 0)) + 1;
+
+                // add missing fields from validator to editor/schema
+                Object.keys(scope.validator).forEach((field) => {
+                    if (!editor[field]) {
+                        editor[field] = {
+                            type: scope.validator[field].type || null,
+                            order: scope.validator[field].order || nextOrder++,
+                        };
+                        schema[field] = scope.validator[field];
+                    }
+                });
+
+                // set scope fields in order
+                scope.fields = sortBy(
+                    Object.keys(editor)
+                        .map((field) => Object.assign({
+                            field: field,
+                            label: getLabelForFieldId(field),
+                            cv: getCV(field),
+                        }, editor[field], schema[field])),
+                    'order'
+                );
+            });
+
+            /**
+             * Test if field should be disabled for editing
+             *
+             * @param {Object} field
+             * @return {Boolean}
+             */
+            scope.isDisabled = (field) => scope.disabled || (scope.associated && field.external);
         },
     };
 }
