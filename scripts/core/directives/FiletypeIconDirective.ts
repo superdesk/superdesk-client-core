2c102a04e1d5ebc3c6c145c8fabd3f5521a5e7c3
ioanpocol <ioan.pocol@sourcefabric.org>
2018-12-05 09:33:54 +0200

    fix(related items): Fix the refresh of the icon when related items ar… (#2693)
    * fix(related items): Fix the refresh of the icon when related items are reordered(SDESK-3644)
    
    * fix(list): Rename function

--- scripts/core/directives/FiletypeIconDirective.ts
+++ scripts/core/directives/FiletypeIconDirective.ts
@@ -11,14 +11,17 @@ export default angular.module('superdesk.core.directives.filetypeIcon', [])
     .directive('sdFiletypeIcon', ['gettextCatalog', (gettextCatalog) => ({
         scope: {item: '='},
         link: function(scope, element, attrs) {
-            var stopWatch = scope.$watch('item', (item) => {
-                if (item) {
-                    initIcon(item);
-                    stopWatch();
+            var oldClass = null;
+
+            scope.$watch('item', (item) => {
+                if (item != null) {
+                    setIcon(item);
+                } else {
+                    oldClass = null;
                 }
             });
 
-            function initIcon(item) {
+            function setIcon(item) {
                 var cls = 'filetype-icon-';
 
                 if (item.type === 'composite' && item.highlight) {
@@ -26,8 +29,14 @@ export default angular.module('superdesk.core.directives.filetypeIcon', [])
                 } else {
                     cls += item.type;
                 }
+
                 element.attr('title', `${gettextCatalog.getString('Article Type')}: ${item.type}`);
                 element.addClass(cls);
+
+                if (oldClass != null) {
+                    element.removeClass(oldClass);
+                }
+                oldClass = cls;
             }
         },
     })]);
