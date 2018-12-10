b1293268450be562dd4b4bf506f6910886c9636a
Tomas Kikutis <t.kikutis@gmail.com>
2018-12-10 15:29:19 +0200

    Do not output "undefined" if `last_name` doesn't exist

--- scripts/apps/contacts/components/fields/ContactName.tsx
+++ scripts/apps/contacts/components/fields/ContactName.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const notEmpty = (str) => typeof str === 'string' && str.length > 0;
+
 export const ContactName: React.StatelessComponent<any> = ({item}) => {
-    const displayContact = item.first_name ? `${item.first_name} ${item.last_name}` : item.organisation;
+    const displayContact = notEmpty(item.first_name) || notEmpty(item.last_name) ?
+        `${notEmpty(item.first_name) ? item.first_name : ''} ${notEmpty(item.last_name) ? item.last_name : ''}`
+        : item.organisation;
 
     return (
         <span key={`contact-name-${item._id}`} className="contact-name">
