6c0ce46b093ae42f19bfc8b78ced30b343ca4fff
Tomas Kikutis <t.kikutis@gmail.com>
2019-01-02 21:53:41 +0100

    enable hiding/unhiding side menu

---
+++ scripts/core/helpers/for-extensions.ts
@@ -0,0 +1,7 @@
+export const hideSideMenu = () => {
+    document.body.classList.add('side-menu-hidden');
+};
+
+export const unhideSideMenu = () => {
+    document.body.classList.remove('side-menu-hidden');
+};
