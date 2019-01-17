aa3c1b58bd28d42501335738b5af0c0f6d78e149
Tomas Kikutis <t.kikutis@gmail.com>
2019-01-17 09:02:24 +0100

    rename the config to reflect the changes

--- scripts/apps/monitoring/controllers/MonitoringController.ts
+++ scripts/apps/monitoring/controllers/MonitoringController.ts
@@ -126,7 +126,7 @@ export function MonitoringController($rootScope, $scope, $location, desks, confi
         self.viewColumn = viewColumn;
 
         if (self.viewColumn) {
-            self.columnsLimit = numberOfColumns ? numberOfColumns : config.features.swimlane.columnsLimit;
+            self.columnsLimit = numberOfColumns ? numberOfColumns : config.features.swimlane.defaultNumberOfColumns;
         } else {
             self.columnsLimit = null;
         }
