59938e888874ecedbbfd0b48d89136db121399aa
Ioan-Vasile Pocol <ivp@ivp-2.local>
2019-02-19 20:20:12 +0200

    fix(macro): Fix the apply of macros for editor2 (SDESK-4002)

--- scripts/apps/authoring/authoring/directives/AuthoringTopbarDirective.ts
+++ scripts/apps/authoring/authoring/directives/AuthoringTopbarDirective.ts
@@ -26,6 +26,7 @@ export function AuthoringTopbarDirective(TranslationService, privileges, authori
             scope.saveTopbar = function() {
                 scope.saveDisabled = true;
                 return scope.save(scope.item)
+                    .then((item) => angular.extend(scope.item, item))
                     .finally(() => {
                         scope.saveDisabled = false;
                     });
