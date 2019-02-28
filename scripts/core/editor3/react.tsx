e44bebaebcab9b544849e4b62a392f7317620e71
Tomas Kikutis <t.kikutis@gmail.com>
2019-02-28 07:58:17 +0100

    move props to ts

--- scripts/core/editor3/react.tsx
+++ scripts/core/editor3/react.tsx
@@ -2,19 +2,70 @@ import React from 'react';
 import createEditorStore from './store';
 import {Editor3} from './components';
 import {Provider} from 'react-redux';
-import PropTypes from 'prop-types';
 import ng from 'core/services/ng';
+import {ContentState} from 'draft-js';
 
-/* eslint-disable react/no-unused-prop-types */
+interface IProps {
+    // If set, it will be used to make sure the toolbar is always
+    // visible when scrolling. If not set, window object is used as reference.
+    // Any valid jQuery selector will do.
+    scrollContainer?: any;
+
+    // Whether this editor is the target for find & replace
+    // operations. The Find & Replace service can only have one editor as
+    // target.
+    findReplaceTarget: boolean;
+
+    // If true, allows inline highlights (commenting, annotating, etc.).
+    highlights: boolean;
+
+    // Editor format options that are enabled and should be displayed
+    // in the toolbar.
+    editorFormat: Array<string>;
+
+    // A JSON object representing the initial Content State of the Draft editor.
+    // It is transformed using `convertFromRaw` and its value is expected to be the
+    // result of `converToRaw`.
+    editorState: any;
+
+    // Function that gets called on every content change. Receives editorState.
+    onChange(contentState: ContentState): void;
+
+    // HTML value of editor. If contentState is not set, this is used.
+    html: string;
+
+    readOnly: boolean;
+    singleLine: boolean;
+    onChangeDebounce: 100;
+
+    // Spellchecker's language.
+    language?: string;
+
+    disableSpellchecker: boolean;
+
+    // If true, images may have an editable title.
+    showTitle: boolean;
+    tabindex: number;
+}
 
 /**
  * @ngdoc React
  * @name Editor
  * @description Editor as a React component.
  */
-export class Editor extends React.Component<any, any> {
-    static propTypes: any;
-    static defaultProps: any;
+export class Editor extends React.Component<IProps> {
+    static defaultProps = {
+        findReplaceTarget: false,
+        highlights: false,
+        editorFormat: [],
+        html: '',
+        readOnly: false,
+        singleLine: false,
+        onChangeDebounce: 100,
+        disableSpellchecker: true,
+        showTitle: false,
+        tabindex: 0,
+    };
 
     store: any;
 
@@ -41,64 +92,3 @@ export class Editor extends React.Component<any, any> {
         );
     }
 }
-
-Editor.defaultProps = {
-    findReplaceTarget: false,
-    highlights: false,
-    editorFormat: [],
-    html: '',
-    readOnly: false,
-    singleLine: false,
-    onChangeDebounce: 100,
-    disableSpellchecker: true,
-    showTitle: false,
-    tabindex: 0,
-};
-
-Editor.propTypes = {
-    // If set, it will be used to make sure the toolbar is always
-    // visible when scrolling. If not set, window object is used as reference.
-    // Any valid jQuery selector will do.
-    scrollContainer: PropTypes.string.isRequired,
-
-    // Whether this editor is the target for find & replace
-    // operations. The Find & Replace service can only have one editor as
-    // target.
-    findReplaceTarget: PropTypes.bool,
-
-    // If true, allows inline highlights (commenting, annotating, etc.).
-    highlights: PropTypes.bool,
-
-    // Editor format options that are enabled and should be displayed
-    // in the toolbar.
-    editorFormat: PropTypes.array,
-
-    // A JSON object representing the initial Content State of the Draft editor.
-    // It is transformed using `convertFromRaw` and its value is expected to be the
-    // result of `converToRaw`.
-    editorState: PropTypes.object,
-
-    // HTML value of editor. If contentState is not set, this is used.
-    html: PropTypes.string,
-
-    // If true, editor is read-only.
-    readOnly: PropTypes.bool,
-
-    // Function that gets called on every content change. Receives editorState.
-    onChange: PropTypes.func.isRequired,
-
-    // Spellchecker's language.
-    language: PropTypes.string,
-
-    // If true, disables the spellchecker.
-    disableSpellchecker: PropTypes.bool,
-
-    // If true, editor is single-line.
-    singleLine: PropTypes.bool,
-
-    // If true, images may have an editable title.
-    showTitle: PropTypes.bool,
-
-    // Tab index
-    tabindex: PropTypes.number,
-};
