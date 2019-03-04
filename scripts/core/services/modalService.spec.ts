5177437badd17f28a6b8af3eb20515f5df9be610
Petr Jasek <jasekpetr@gmail.com>
2019-03-04 14:07:42 +0100

    refactor e2e test to unittest

---
+++ scripts/core/services/modalService.spec.ts
@@ -0,0 +1,18 @@
+describe('modal service', () => {
+
+    beforeEach(window.module('superdesk.core.services.modal'));
+
+    beforeEach(inject(($templateCache) => {
+        $templateCache.put('scripts/core/views/confirmation-modal.html', require('./../views/confirmation-modal.html'));
+    }));
+
+    it('does not translate modal header/body', inject((modal, gettextCatalog, $rootScope) => {
+        spyOn(gettextCatalog, 'getString').and.returnValue('translated');
+
+        modal.confirm('body', 'header', 'ok', 'cancel', 'foo');
+        $rootScope.$digest();
+
+        expect(angular.element(document.body).find('.modal__header').text().trim()).toBe('header');
+        expect(angular.element(document.body).find('.modal__body').text().trim()).toBe('body');
+    }));
+});
