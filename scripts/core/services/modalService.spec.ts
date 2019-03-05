6792ec3c75149f9fce5632de40ba30a1f3ed70ca
Petr Jasek <jasekpetr@gmail.com>
2019-03-05 15:32:15 +0100

    add comment

--- scripts/core/services/modalService.spec.ts
+++ scripts/core/services/modalService.spec.ts
@@ -5,6 +5,9 @@ describe('modal service', () => {
         $templateCache.put('scripts/core/views/confirmation-modal.html', require('./../views/confirmation-modal.html'));
     }));
 
+    // modal should never try to translate its contents,
+    // it breaks rendering of such strings. everything should be translated before
+    // calling modal service
     it('does not translate modal header/body', inject((modal, gettextCatalog, $rootScope) => {
         spyOn(gettextCatalog, 'getString').and.returnValue('translated');
 
