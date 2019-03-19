39beede3cb39a1ffdb4c283fdfab2bf6a504f864
Petr Jašek <jasekpetr@gmail.com>
2019-03-19 10:49:20 +0100

    fix(fields): fix missing related content items as associations (#2876)
    * fix(fields): fix missing related content items as associations
    
    those are stored as ids only, so it must be fetched to get the type
    
    SDBELGA-73

--- scripts/apps/search/components/fields/associatedItems.tsx
+++ scripts/apps/search/components/fields/associatedItems.tsx
@@ -1,34 +1,15 @@
 import React from 'react';
-import {IArticle} from 'superdesk-interfaces/Article';
+import PropTypes from 'prop-types';
+import {isEmpty} from 'lodash';
+import {AssociatedItemsList} from './AssociatedItemsList';
 
-const TYPES_TO_ICONS = {
-    picture: 'icon-photo',
-    graphic: 'icon-graphic',
-    video: 'icon-video',
-    audio: 'icon-audio',
+export const associatedItems: React.StatelessComponent<any> = ({item, svc}) => (
+    isEmpty(item.associations) ? null : <AssociatedItemsList key="associatedItems" item={item} svc={svc} />
+);
+
+associatedItems.propTypes = {
+    item: PropTypes.object.isRequired,
+    svc: PropTypes.shape({
+        content: PropTypes.object.isRequired,
+    }).isRequired,
 };
-
-/**
- * Render icon with count for each item type present in associations.
- */
-export function associatedItems({item}: {item: IArticle}) {
-    const associations = Object.values(item.associations || {});
-    const icons = Object.keys(TYPES_TO_ICONS).map((type) => ({
-        type: type,
-        icon: TYPES_TO_ICONS[type],
-        count: associations.filter((assoc: IArticle) => assoc != null && assoc.type === type).length,
-    }))
-        .filter((data) => data.count > 0)
-        .map((associatedType) => (
-            <span key={associatedType.type} className="sd-text-icon sd-text-icon--aligned-r">
-                <i className={`${associatedType.icon} sd-opacity--40`} />
-                {associatedType.count}
-            </span>
-        ));
-
-    return icons.length ? (
-        <span style={{marginLeft: 'auto'}}> {/* push it right */}
-            {icons}
-        </span>
-    ) : null;
-}
