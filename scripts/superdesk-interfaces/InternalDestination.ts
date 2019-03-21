09ce8167e40b91fb005fbd944712cee10ab19d76
Tomas Kikutis <t.kikutis@gmail.com>
2019-03-21 19:34:59 +0100

    update interfaces

---
+++ scripts/superdesk-interfaces/InternalDestination.ts
@@ -0,0 +1,10 @@
+import {IDefaultApiFields} from "types/RestApi";
+
+export interface IInternalDestination extends IDefaultApiFields {
+    name: string;
+    is_active: boolean;
+    filter?: string;
+    desk: string;
+    stage?: string;
+    macro?: string;
+}
