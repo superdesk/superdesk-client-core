85d5b480aa43eb9da1de58d20f1c8ed03b2aab79
Tomas Kikutis <t.kikutis@gmail.com>
2019-03-21 19:39:09 +0100

    add typescript typings

--- scripts/core/ui/components/Form/Checkbox.tsx
+++ scripts/core/ui/components/Form/Checkbox.tsx
@@ -1,93 +1,90 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
-/**
- * @ngdoc react
- * @name Checkbox
- * @description Component to show checkbox input in styles including radiobutton
- */
-export const Checkbox: React.StatelessComponent<any> = ({
-    field,
-    value,
-    checkedValue,
-    label,
-    labelPosition,
-    readOnly,
-    onChange,
-    type,
-}) => {
-    const isRadio = type === 'radio';
-    const onClick = readOnly ?
-        null :
-        (event) => {
-            event.stopPropagation();
-            onChange(field, isRadio ? checkedValue : !value);
-        };
+interface IProps {
+    field?: string;
+    label?: string;
+    labelPosition: 'left' | 'right' | 'inside';
+    value: boolean | string;
+    checkedValue: string;
+    onChange(field: IProps['field'] | undefined, value: IProps['value']): void;
+    readOnly: boolean;
+    type: 'radio' | 'checkbox';
+}
 
-    const className = classNames(
-        'sd-checkbox',
-        {
-            'sd-checkbox--disabled': readOnly,
-            'sd-checkbox--button-style': labelPosition === 'inside',
-            'sd-checkbox--radio': isRadio,
-            checked: isRadio ? value === checkedValue : value,
-        },
-    );
+// Component to show checkbox input in styles including radiobutton
+export class Checkbox extends React.Component<IProps> {
+    static defaultProps = {
+        value: false,
+        checkedValue: '',
+        readOnly: false,
+        labelPosition: 'right',
+        type: 'checkbox',
+    };
 
-    let checkbox;
+    render() {
 
-    if (labelPosition === 'inside') {
-        checkbox = (
-            <a className="sd-check__wrapper" onClick={onClick}>
-                <span className={className}>
+        const {
+            field,
+            value,
+            checkedValue,
+            label,
+            labelPosition,
+            readOnly,
+            onChange,
+            type,
+        } = this.props;
+
+        const isRadio = type === 'radio';
+        const onClick = readOnly ?
+            null :
+            (event) => {
+                event.stopPropagation();
+                onChange(field, isRadio ? checkedValue : !value);
+            };
+
+        const className = classNames(
+            'sd-checkbox',
+            {
+                'sd-checkbox--disabled': readOnly,
+                'sd-checkbox--button-style': labelPosition === 'inside',
+                'sd-checkbox--radio': isRadio,
+                checked: isRadio ? value === checkedValue : value,
+            },
+        );
+
+        let checkbox;
+
+        if (labelPosition === 'inside') {
+            checkbox = (
+                <a className="sd-check__wrapper" onClick={onClick}>
+                    <span className={className}>
+                        <label className={readOnly ? 'sd-label--disabled' : ''}>
+                            {label}
+                        </label>
+                    </span>
+                </a>
+            );
+        } else if (labelPosition === 'left') {
+            checkbox = (
+                <a className="sd-check__wrapper" onClick={onClick}>
                     <label className={readOnly ? 'sd-label--disabled' : ''}>
                         {label}
                     </label>
-                </span>
-            </a>
-        );
-    } else if (labelPosition === 'left') {
-        checkbox = (
-            <a className="sd-check__wrapper" onClick={onClick}>
-                <label className={readOnly ? 'sd-label--disabled' : ''}>
-                    {label}
-                </label>
-                <span className={className}/>
-            </a>
-        );
-    } else {
-        checkbox = (
-            <a className="sd-check__wrapper" onClick={onClick}>
-                <span className={className}/>
-                <label className={readOnly ? 'sd-label--disabled' : ''}>
-                    {label}
-                </label>
-            </a>
-        );
+                    <span className={className}/>
+                </a>
+            );
+        } else {
+            checkbox = (
+                <a className="sd-check__wrapper" onClick={onClick}>
+                    <span className={className}/>
+                    <label className={readOnly ? 'sd-label--disabled' : ''}>
+                        {label}
+                    </label>
+                </a>
+            );
+        }
+
+        return checkbox;
     }
-
-    return checkbox;
-};
-
-Checkbox.propTypes = {
-    field: PropTypes.string,
-    label: PropTypes.string,
-    labelPosition: PropTypes.oneOf(['left', 'right', 'inside']),
-    value: PropTypes.oneOfType([
-        PropTypes.bool,
-        PropTypes.string,
-    ]),
-    checkedValue: PropTypes.string,
-    onChange: PropTypes.func.isRequired,
-    readOnly: PropTypes.bool,
-    type: PropTypes.oneOf(['radio', 'checkbox']),
-};
-
-Checkbox.defaultProps = {
-    value: false,
-    checkedValue: '',
-    readOnly: false,
-    labelPosition: 'right',
-    type: 'checkbox',
-};
+}
