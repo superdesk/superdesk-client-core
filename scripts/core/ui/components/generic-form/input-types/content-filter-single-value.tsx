9b53acf86fdc1cc6679494511c13ba41ee41b1c0
Tomas Kikutis <t.kikutis@gmail.com>
2019-03-22 17:33:07 +0100

    make conent_filter_single_value use generic async select

--- scripts/core/ui/components/generic-form/input-types/content-filter-single-value.tsx
+++ scripts/core/ui/components/generic-form/input-types/content-filter-single-value.tsx
@@ -1,77 +1,12 @@
-import React from 'react';
-import classNames from 'classnames';
-import {connectServices} from 'core/helpers/ReactRenderAsync';
-import {IInputType} from '../interfaces/input-types';
+import {getSelectSingleValue} from './select_single_value';
 import {IRestApiResponse} from 'types/RestApi';
 import {IContentFilter} from 'superdesk-interfaces/ContentFilter';
+import ng from 'core/services/ng';
 
-interface IProps extends IInputType<string> {
-    api?: any;
-}
-
-interface IState {
-    contentFilters: Array<IContentFilter>;
-}
-
-export class ContentFilterSingleValueComponent extends React.Component<IProps, IState> {
-    initalValue: string;
-
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            contentFilters: [],
-        };
-
-        this.initalValue = props.value;
-    }
-    componentDidMount() {
-        this.props.api('content_filters').query({max_results: 200}).then((res: IRestApiResponse<IContentFilter>) => {
-            this.setState({
-                contentFilters: res._items,
-            });
-        });
-    }
-    render() {
-        if (this.state.contentFilters == null) {
-            return null;
-        }
-
-        if (this.props.previewOuput) {
-            let contentFilter = this.state.contentFilters.find((item) => item._id === this.props.value);
-
-            return contentFilter == null ? <div>{this.props.value}</div> : <div>{contentFilter.name}</div>;
-        }
-
-        return (
-            <div className={classNames('sd-line-input', {'sd-line-input--invalid': this.props.issues.length > 0})}>
-                <label className="sd-line-input__label">{this.props.formField.label}</label>
-                <select
-                    disabled={this.props.disabled}
-                    value={this.props.value}
-                    className="sd-line-input__select"
-                    onChange={(event) => {
-                        this.props.onChange(event.target.value === '' ? this.initalValue : event.target.value);
-                    }}
-                >
-                    <option value="" />
-                    {
-                        this.state.contentFilters.map(({_id, name}, i) => (
-                            <option key={i} value={_id}>{name}</option>
-                        ))
-                    }
-                </select>
-                {
-                    this.props.issues.map((str, i) => (
-                        <div key={i} className="sd-line-input__message">{str}</div>
-                    ))
-                }
-            </div>
-        );
-    }
-}
-
-export const ContentFilterSingleValue = connectServices<IProps>(
-    ContentFilterSingleValueComponent,
-    ['api'],
+export const ContentFilterSingleValue = getSelectSingleValue(
+    () =>
+        ng.getService('api')
+            .then((api) => api('content_filters').query({max_results: 200}))
+            .then((contentFilters: IRestApiResponse<IContentFilter>) =>
+                contentFilters._items.map(({_id, name}) => ({id: _id, label: name}))),
 );
