6dbefef55efe9f7415f016b0f2008623a335499a
Tomas Kikutis <t.kikutis@gmail.com>
2019-04-01 10:12:30 +0200

    class to function

--- scripts/core/editor3/html/to-html/HTMLGenerator.ts
+++ scripts/core/editor3/html/to-html/HTMLGenerator.ts
@@ -4,94 +4,85 @@ import {getAnnotationsFromContentState} from 'core/editor3/helpers/editor3Custom
 import {stateToHTML} from 'draft-js-export-html';
 import {trimStartExact, trimEndExact} from 'core/helpers/utils';
 
-export class HTMLGenerator {
-    contentState: ContentState;
-    disabled: Array<any>;
+export const editor3StateToHtml = (
+    contentState: ContentState,
+    disabled: Array<string> = [], // A set of disabled elements (ie. ['table'] will ignore
+) => {
+    const annotationsByStyleName = getAnnotationsFromContentState(contentState)
+        .reduce((accumulator, item) => {
+            return {...accumulator, [item.styleName]: item};
+        }, {});
 
-    constructor(contentState, disabled = []) {
-        this.contentState = contentState;
+    let options = {
+        inlineStyles: {
+            BOLD: {element: 'b'},
+            ITALIC: {element: 'i'},
+            STRIKETHROUGH: {element: 's'},
+            SUBSCRIPT: {element: 'sub'},
+            SUPERSCRIPT: {element: 'sup'},
+        },
+        entityStyleFn: (entity: Draft.EntityInstance) => {
+            if (entity.getType() === 'LINK') {
+                const data = entity.getData();
 
-        // A set of disabled elements (ie. ['table'] will ignore
-        this.disabled = disabled || [];
-    }
+                if (data.url) {
+                    return {
+                        element: 'a',
+                        attributes: {href: data.url},
+                    };
+                } else {
+                    const link = data.link;
 
-    html() {
-        const annotationsByStyleName = getAnnotationsFromContentState(this.contentState)
-            .reduce((accumulator, item) => {
-                return {...accumulator, [item.styleName]: item};
-            }, {});
-
-        let options = {
-            inlineStyles: {
-                BOLD: {element: 'b'},
-                ITALIC: {element: 'i'},
-                STRIKETHROUGH: {element: 's'},
-                SUBSCRIPT: {element: 'sub'},
-                SUPERSCRIPT: {element: 'sup'},
-            },
-            entityStyleFn: (entity: Draft.EntityInstance) => {
-                if (entity.getType() === 'LINK') {
-                    const data = entity.getData();
-
-                    if (data.url) {
+                    if (link.attachment != null) {
                         return {
                             element: 'a',
-                            attributes: {href: data.url},
+                            attributes: {'data-attachment': link.attachment},
+                        };
+                    } else if (link.target != null) {
+                        return {
+                            element: 'a',
+                            attributes: {href: link.target, target: link.target},
                         };
                     } else {
-                        const link = data.link;
-
-                        if (link.attachment != null) {
-                            return {
-                                element: 'a',
-                                attributes: {'data-attachment': link.attachment},
-                            };
-                        } else if (link.target != null) {
-                            return {
-                                element: 'a',
-                                attributes: {href: link.target, target: link.target},
-                            };
-                        } else {
-                            return {
-                                element: 'a',
-                                attributes: {href: link.href},
-                            };
-                        }
+                        return {
+                            element: 'a',
+                            attributes: {href: link.href},
+                        };
                     }
                 }
-            },
+            }
+        },
 
-            blockRenderers: {
-                atomic: (block: ContentBlock) => new AtomicBlockParser(this.contentState, this.disabled).parse(block),
-            },
+        blockRenderers: {
+            atomic: (block: ContentBlock) => new AtomicBlockParser(contentState, disabled).parse(block),
+        },
 
-            inlineStyleFn: (styles: DraftInlineStyle) => {
-                let annotationStyleName = styles.find((styleName: string) => annotationsByStyleName[styleName] != null);
+        inlineStyleFn: (styles: DraftInlineStyle) => {
+            let annotationStyleName = styles.find((styleName: string) => annotationsByStyleName[styleName] != null);
 
-                if (annotationStyleName != null) {
-                    return {
-                        element: 'span',
-                        attributes: {
-                            'annotation-id': annotationsByStyleName[annotationStyleName].id,
-                        },
-                    };
-                }
-            },
-        };
+            if (annotationStyleName != null) {
+                return {
+                    element: 'span',
+                    attributes: {
+                        'annotation-id': annotationsByStyleName[annotationStyleName].id,
+                    },
+                };
+            }
+        },
+    };
 
-        /*
-            If an atomic block is the only content in the editor, it automatically gets line breaks around it.
-            trimStartExact & trimEndExact gets rid of those line breaks.
-            .trim() at the end removes whitespace.
-        */
-        var res = trimStartExact(
-            trimEndExact(
-                stateToHTML(this.contentState, options),
-                '<p><br></p>',
-            ),
+    /*
+        If an atomic block is the only content in the editor, it automatically gets line breaks around it.
+        trimStartExact & trimEndExact gets rid of those line breaks.
+        .trim() at the end removes whitespace.
+    */
+    var res = trimStartExact(
+        trimEndExact(
+            stateToHTML(contentState, options),
             '<p><br></p>',
-        ).trim();
+        ),
+        '<p><br></p>',
+    ).trim();
 
-        return res;
-    }
-}
+    return res;
+};
