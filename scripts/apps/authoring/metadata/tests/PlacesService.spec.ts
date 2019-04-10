8c0d05b934d1b2e016d1459ec88aec8a5614f742
Petr Jasek <jasekpetr@gmail.com>
2019-04-10 11:54:54 +0200

    fix dateline place search when geonames is not configured
    SDESK-4169

---
+++ scripts/apps/authoring/metadata/tests/PlacesService.spec.ts
@@ -0,0 +1,19 @@
+
+describe('places service', () => {
+    beforeEach(window.module('superdesk.apps.authoring.metadata'));
+
+    it('can search for dateline using cities if geonames are unavailable', (done) => {
+        inject((places, metadata) => {
+            spyOn(metadata, 'fetchCities').and.returnValue(Promise.resolve([
+                {city: 'Prague'},
+                {city: 'Brno'},
+            ]));
+
+            places.searchDateline('br', 'en').then((cities) => {
+                expect(cities.length).toBe(1);
+                expect(cities[0].city).toBe('Brno');
+                done();
+            });
+        });
+    });
+});
