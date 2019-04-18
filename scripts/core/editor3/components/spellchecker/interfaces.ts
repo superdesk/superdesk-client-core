d7f6a466a6217723684fedebbfc8b35294b142cd
Tomas Kikutis <t.kikutis@gmail.com>
2019-04-18 15:11:59 +0200

    improve interface

--- scripts/core/editor3/components/spellchecker/interfaces.ts
+++ scripts/core/editor3/components/spellchecker/interfaces.ts
@@ -7,7 +7,8 @@ export interface ISpellcheckWarning {
 
     // list of text fragments suggested to replace offending text fragment.
     // Can consist of multiple words. Can NOT span multiple paragraphs.
-    suggestions: Array<string>;
+    // Can be omited if `ISpellchecker['getSuggestions']` method is defined.
+    suggestions?: Array<string>;
 }
 
 export interface ISpellcheckerAction {
@@ -20,7 +21,8 @@ export interface ISpellchecker {
     check(text: string): Promise<Array<ISpellcheckWarning>>;
 
     // text - formatting-free text, must be single-line
-    getSuggestions(text: string): Promise<Array<string>>;
+    // can be ommited if suggestions are provided in `ISpellcheckWarning`s returned from the `check` method.
+    getSuggestions?(text: string): Promise<Array<string>>;
 
     actions: {[key: string]: ISpellcheckerAction};
 }
