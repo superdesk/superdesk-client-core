eefcd4b498be9b41a6aba558c007eb6bbaab28b7
Tomas Kikutis <t.kikutis@gmail.com>
2019-06-13 15:12:11 +0200

    get suggestions for substring

--- scripts/core/editor3/components/spellchecker/default-spellcheckers.tsx
+++ scripts/core/editor3/components/spellchecker/default-spellcheckers.tsx
@@ -1,5 +1,6 @@
 import ng from 'core/services/ng';
 import {ISpellchecker, ISpellcheckerAction, ISpellcheckWarning} from './interfaces';
+import {logger} from 'core/services/logger';
 
 const actions: {[key: string]: ISpellcheckerAction} = {
     addToDictionary: {
@@ -83,6 +84,32 @@ export function getSpellchecker(language: string): ISpellchecker {
                     };
 
                     xhr.send(JSON.stringify({spellchecker: 'grammalecte', text: str}));
+                });
+            }),
+            getSuggestions: (str) => ng.getServices(['config', 'session']).then((services: Array<any>) => {
+                const [config, session] = services;
+
+                return new Promise((resolve, reject) => {
+                    const xhr = new XMLHttpRequest();
+
+                    xhr.open('POST', config.server.url + '/spellchecker', true);
+
+                    xhr.setRequestHeader('Content-Type', 'application/json');
+                    xhr.setRequestHeader('Authorization', session.token);
+
+                    xhr.onload = function() {
+                        if (this.status >= 200 && this.status <= 300) {
+                            try {
+                                resolve(JSON.parse(this.responseText).suggestions);
+                            } catch (e) {
+                                reject('invalid JSON received from server');
+                            }
+                        } else {
+                            reject('server error');
+                        }
+                    };
+
+                    xhr.send(JSON.stringify({spellchecker: 'grammalecte', text: str, suggestions: true}));
                 });
             }),
             actions: {},
