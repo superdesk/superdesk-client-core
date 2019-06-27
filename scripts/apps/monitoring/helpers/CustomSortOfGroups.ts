7ba102726b37b094ed4f48296aaf1cde08da1485
Pablo P Varela <pablovarela182@gmail.com>
2019-06-27 12:53:33 +0200

    Add default option to custom sort of groups (#2994)
    * Add default option to custom sort of groups
    
    And some css fixes to match the UI proposal
    
    * Fix irregular whitespace
    
    * Make default config an object and rename sort fields

--- scripts/apps/monitoring/helpers/CustomSortOfGroups.ts
+++ scripts/apps/monitoring/helpers/CustomSortOfGroups.ts
@@ -3,6 +3,17 @@ import {StageGroup} from '../directives/MonitoringGroup';
 
 export type GroupSortOptions = Array<string>;
 
+type OrderType = 'desc' | 'asc';
+
+export type GroupSortConfig = {
+    default?: { field: string, order: OrderType },
+    allowed_fields_to_sort: GroupSortOptions
+};
+
+function isOrderType(o: string): o is OrderType {
+    return ['desc', 'asc'].includes(o);
+}
+
 export function matchGroupToOrderConfig(group: StageGroup) {
     if (group._id.endsWith(':output')) {
         return 'monitoring.output.sort';
@@ -13,13 +24,18 @@ export function matchGroupToOrderConfig(group: StageGroup) {
     return 'monitoring.stage.sort';
 }
 
-export default function getCustomSortForGroup(config: any, group: StageGroup): GroupSortOptions {
+export default function getCustomSortForGroup(config: any, group: StageGroup): GroupSortConfig | null {
     if (!group || !group._id || !group.type) {
-        return [];
+        return null;
     }
 
     const configForGroup = matchGroupToOrderConfig(group);
-    const customConfig = get(config, configForGroup, []);
+    const customConfig: GroupSortConfig = get(config, configForGroup, null);
+
+    if (customConfig && customConfig.default && !isOrderType(customConfig.default.order)) {
+        console.warn(`Default sort order is not a valid string '${customConfig.default.order}'. Use 'asc' or 'desc'`);
+        customConfig.default.order = 'asc';
+    }
 
     return customConfig;
 }
