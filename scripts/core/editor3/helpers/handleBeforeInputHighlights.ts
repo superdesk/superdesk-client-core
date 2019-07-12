71da7115cc2f6d4ee98129cab51ee9d098a2dfa9
pablopunk <pablovarela182@gmail.com>
2019-07-12 12:42:07 +0200

    Add docs

--- scripts/core/editor3/helpers/handleBeforeInputHighlights.ts
+++ scripts/core/editor3/helpers/handleBeforeInputHighlights.ts
@@ -1,4 +1,10 @@
-import {EditorState, Modifier, CharacterMetadata, DraftHandleValue} from 'draft-js';
+import {
+    EditorState,
+    Modifier,
+    CharacterMetadata,
+    SelectionState,
+    DraftHandleValue
+} from 'draft-js';
 import {List, OrderedSet} from 'immutable';
 import {uniq} from 'lodash';
 
@@ -15,28 +21,40 @@ function isSelectionAtEndOfBlock(editorState: EditorState): boolean {
 }
 
 /**
- * @ngdoc method
- * @name addCommentsForServer
- * @param {EditorState} editorState
- * @param {SelectionState} collapsedSelection
- * @return {List<CharacterMetadata>}
+ * Returns the characters before and after the cursor.
  */
-function getRelevantCharactersForCollapsedSelection(editorState, collapsedSelection) {
-    let characters: any = List();
-    const selectionAtTheStartOfTheLine = editorState.getSelection().getStartOffset() === 0;
+function getRelevantCharactersForCollapsedSelection(
+    editorState: EditorState,
+    collapsedSelection: SelectionState,
+): List<CharacterMetadata> {
+    let characters = List<CharacterMetadata>();
+    const selectionAtTheStartOfTheLine =
+        editorState.getSelection().getStartOffset() === 0;
     const selectionAtTheEndOfTheBlock = isSelectionAtEndOfBlock(editorState);
     const resizeLeft = selectionAtTheStartOfTheLine ? 2 : 1; // SDESK-2861
-    const resizedSelection = resizeDraftSelection(resizeLeft, 1, collapsedSelection, editorState, false);
-    const resizeLeftFailed = collapsedSelection.getStartOffset() === resizedSelection.getStartOffset();
-    const resizeRightFailed = collapsedSelection.getEndOffset() === resizedSelection.getEndOffset();
+    const resizeRight = selectionAtTheEndOfTheBlock ? 2 : 1; // SDESK-4335
+    const resizedSelection = resizeDraftSelection(
+        resizeLeft,
+        resizeRight,
+        collapsedSelection,
+        editorState,
+        false,
+    );
+    const resizeLeftFailed =
+        collapsedSelection.getStartOffset() ===
+        resizedSelection.getStartOffset();
+    const resizeRightFailed =
+        collapsedSelection.getEndOffset() === resizedSelection.getEndOffset();
 
     if (resizeLeftFailed === true) {
         characters = characters.push(CharacterMetadata.create());
     }
 
-    characters = characters.concat(getDraftCharacterListForSelection(editorState, resizedSelection));
+    characters = characters.merge(
+        getDraftCharacterListForSelection(editorState, resizedSelection),
+    );
 
-    if (resizeRightFailed === true || selectionAtTheEndOfTheBlock) {
+    if (resizeRightFailed === true) {
         characters = characters.push(CharacterMetadata.create());
     }
 
@@ -48,35 +66,53 @@ function getRelevantCharactersForCollapsedSelection(editorState, collapsedSelect
  * @name handleBeforeInputHighlights
  * @description prevents inheriting of highlight styles
  */
-export function handleBeforeInputHighlights(onChange, chars: string, editorState: EditorState): DraftHandleValue {
+export function handleBeforeInputHighlights(
+    onChange,
+    chars: string,
+    editorState: EditorState,
+): DraftHandleValue {
     // see handleBeforeInputHighlights.spec.gif
 
     const selection = editorState.getSelection();
     const characterList = selection.isCollapsed()
         ? getRelevantCharactersForCollapsedSelection(editorState, selection)
         : getDraftCharacterListForSelection(editorState, selection);
-    const characterStyles = characterList.map((character) => character.getStyle()).toJS();
-    const allHighlightStyles = uniq(characterStyles.reduce((a, b) => a.concat(b)))
-        .filter(styleNameBelongsToHighlight);
+    const characterStyles = characterList
+        .map((character) => character.getStyle())
+        .toJS();
+    const allHighlightStyles = uniq(
+        characterStyles.reduce((a, b) => a.concat(b)),
+    ).filter(styleNameBelongsToHighlight);
 
     if (allHighlightStyles.length < 1) {
         return 'not-handled';
     }
 
-    const commonHighlightStyles = allHighlightStyles.filter(
-        (styleName) => characterStyles.every((stylesAtPosition) => stylesAtPosition.includes(styleName)),
+    const commonHighlightStyles = allHighlightStyles.filter((styleName) =>
+        characterStyles.every((stylesAtPosition) =>
+            stylesAtPosition.includes(styleName),
+        ),
     );
 
-    const nextInlineStyles = OrderedSet<string>(editorState.getCurrentInlineStyle()
-        .filter((styleName) => styleNameBelongsToHighlight(styleName) === false)
-        .concat(commonHighlightStyles));
+    const nextInlineStyles = OrderedSet<string>(
+        editorState
+            .getCurrentInlineStyle()
+            .filter(
+                (styleName) => styleNameBelongsToHighlight(styleName) === false,
+            )
+            .concat(commonHighlightStyles),
+    );
     const nextContentState = Modifier.replaceText(
         editorState.getCurrentContent(),
         editorState.getSelection(),
         chars,
         nextInlineStyles,
     );
-    const nextEditorState = EditorState.push(editorState, nextContentState, 'insert-characters');
+    const nextEditorState = EditorState.push(
+        editorState,
+        nextContentState,
+        'insert-characters',
+    );
 
     onChange(nextEditorState);
 
