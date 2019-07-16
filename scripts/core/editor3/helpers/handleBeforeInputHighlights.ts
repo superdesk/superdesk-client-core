9236de137c6b7fd0d494fabe02f4cfeb56e5c3c9
Pablo P Varela <pablovarela182@gmail.com>
2019-07-16 15:23:01 +0200

    Avoid extending inline styles when pressing space at end of blo… (#3013)
    * Avoid extending inline styles when pressing space at end of block
    
    This includes all styles (annotations, comments, italic...)
    
    * Fix lint
    
    * Make resize checks consistent with all number of blocks
    
    * Add docs
    
    * Update comment

--- scripts/core/editor3/helpers/handleBeforeInputHighlights.ts
+++ scripts/core/editor3/helpers/handleBeforeInputHighlights.ts
@@ -1,32 +1,60 @@
-import {EditorState, Modifier, CharacterMetadata} from 'draft-js';
-import {List} from 'immutable';
+import {
+    EditorState,
+    Modifier,
+    CharacterMetadata,
+    SelectionState,
+    DraftHandleValue
+} from 'draft-js';
+import {List, OrderedSet} from 'immutable';
 import {uniq} from 'lodash';
 
 import {getDraftCharacterListForSelection} from './getDraftCharacterListForSelection';
 import {resizeDraftSelection} from './resizeDraftSelection';
 import {styleNameBelongsToHighlight} from './highlights';
 
-/**
- * @ngdoc method
- * @name addCommentsForServer
- * @param {EditorState} editorState
- * @param {SelectionState} collapsedSelection
- * @return {List<CharacterMetadata>}
- */
-function getRelevantCharactersForCollapsedSelection(editorState, collapsedSelection) {
-    let characters: any = List();
-    const selectionAtTheStartOfTheLine = editorState.getSelection().getStartOffset() === 0;
-    const resizeLeft = selectionAtTheStartOfTheLine ? 2 : 1; // SDESK-2861
+function isSelectionAtEndOfBlock(editorState: EditorState): boolean {
+    const selection = editorState.getSelection();
+    const endBlockKey = selection.getEndKey();
+    const block = editorState.getCurrentContent().getBlockForKey(endBlockKey);
 
-    const resizedSelection = resizeDraftSelection(resizeLeft, 1, collapsedSelection, editorState, false);
-    const resizeLeftFailed = collapsedSelection.getStartOffset() === resizedSelection.getStartOffset();
-    const resizeRightFailed = collapsedSelection.getEndOffset() === resizedSelection.getEndOffset();
+    return block.getLength() === selection.getEndOffset();
+}
+
+/**
+ * Returns the characters before and after the cursor.
+ *   If it fails resizing the selection to the right, it will push a character with an empty style on the left.
+ *   If it fails resizing the selection to the left, it will also push a character with an empty style on the right.
+ */
+function getRelevantCharactersForCollapsedSelection(
+    editorState: EditorState,
+    collapsedSelection: SelectionState,
+): List<CharacterMetadata> {
+    let characters = List<CharacterMetadata>();
+    const selectionAtTheStartOfTheLine =
+        editorState.getSelection().getStartOffset() === 0;
+    const selectionAtTheEndOfTheBlock = isSelectionAtEndOfBlock(editorState);
+    const resizeLeft = selectionAtTheStartOfTheLine ? 2 : 1; // SDESK-2861
+    const resizeRight = selectionAtTheEndOfTheBlock ? 2 : 1; // SDESK-4335
+    const resizedSelection = resizeDraftSelection(
+        resizeLeft,
+        resizeRight,
+        collapsedSelection,
+        editorState,
+        false,
+    );
+    const resizeLeftFailed =
+        collapsedSelection.getStartOffset() ===
+        resizedSelection.getStartOffset();
+    const resizeRightFailed =
+        collapsedSelection.getEndOffset() === resizedSelection.getEndOffset();
 
     if (resizeLeftFailed === true) {
         characters = characters.push(CharacterMetadata.create());
     }
 
-    characters = characters.concat(getDraftCharacterListForSelection(editorState, resizedSelection));
+    characters = characters.merge(
+        getDraftCharacterListForSelection(editorState, resizedSelection),
+    );
 
     if (resizeRightFailed === true) {
         characters = characters.push(CharacterMetadata.create());
@@ -40,39 +68,53 @@ function getRelevantCharactersForCollapsedSelection(editorState, collapsedSelect
  * @name handleBeforeInputHighlights
  * @description prevents inheriting of highlight styles
  */
-export function handleBeforeInputHighlights(onChange, chars, editorState) {
+export function handleBeforeInputHighlights(
+    onChange,
+    chars: string,
+    editorState: EditorState,
+): DraftHandleValue {
     // see handleBeforeInputHighlights.spec.gif
 
     const selection = editorState.getSelection();
-
     const characterList = selection.isCollapsed()
         ? getRelevantCharactersForCollapsedSelection(editorState, selection)
         : getDraftCharacterListForSelection(editorState, selection);
-
-    const characterStyles = characterList.map((character) => character.getStyle()).toJS();
-
-    const allHighlightStyles = uniq(characterStyles.reduce((a, b) => a.concat(b)))
-        .filter(styleNameBelongsToHighlight);
+    const characterStyles = characterList
+        .map((character) => character.getStyle())
+        .toJS();
+    const allHighlightStyles = uniq(
+        characterStyles.reduce((a, b) => a.concat(b)),
+    ).filter(styleNameBelongsToHighlight);
 
     if (allHighlightStyles.length < 1) {
         return 'not-handled';
     }
 
-    const commonHighlightStyles = allHighlightStyles.filter(
-        (styleName) => characterStyles.every((stylesAtPosition) => stylesAtPosition.includes(styleName)),
+    const commonHighlightStyles = allHighlightStyles.filter((styleName) =>
+        characterStyles.every((stylesAtPosition) =>
+            stylesAtPosition.includes(styleName),
+        ),
     );
 
-    const nextInlineStyles = editorState.getCurrentInlineStyle()
-        .filter((styleName) => styleNameBelongsToHighlight(styleName) === false)
-        .concat(commonHighlightStyles);
-
-    const nextContentstate = Modifier.replaceText(
+    const nextInlineStyles = OrderedSet<string>(
+        editorState
+            .getCurrentInlineStyle()
+            .filter(
+                (styleName) => styleNameBelongsToHighlight(styleName) === false,
+            )
+            .concat(commonHighlightStyles),
+    );
+    const nextContentState = Modifier.replaceText(
         editorState.getCurrentContent(),
         editorState.getSelection(),
         chars,
         nextInlineStyles,
     );
-    const nextEditorState = EditorState.push(editorState, nextContentstate, 'insert-characters');
+    const nextEditorState = EditorState.push(
+        editorState,
+        nextContentState,
+        'insert-characters',
+    );
 
     onChange(nextEditorState);
 
