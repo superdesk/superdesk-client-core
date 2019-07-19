c54fff53a21d69f514bc79e512820ad2759f4a05
Tomas Kikutis <t.kikutis@gmail.com>
2019-07-19 11:45:14 +0200

    merge types

--- scripts/core/superdesk-api.d.ts
+++ scripts/core/superdesk-api.d.ts
@@ -100,11 +100,58 @@ declare module 'superdesk-api' {
 
     // ENTITIES
 
-    // this is a subset of the main IArticle interface found in the core
-    // a subset is used in order expose things gradually as needed
+    export interface IAuthor {
+        role: string;
+        parent: string;
+    }
+
     export interface IArticle extends IBaseRestApiResponse {
         _id: string;
+        _current_version: number;
+        guid: string;
+        translated_from: string;
+        translation_id: string;
+        usageterms: any;
+        keywords: any;
+        language: any;
         slugline: string;
+        genre: any;
+        anpa_take_key: any;
+        place: any;
+        priority: any;
+        urgency: any;
+        anpa_category: any;
+        subject: any;
+        company_codes: Array<any>;
+        ednote: string;
+        authors: Array<IAuthor>;
+        headline: string;
+        sms: string;
+        abstract: string;
+        byline: string;
+        dateline: string;
+        body_html: string;
+        footer: string;
+        firstcreated: any;
+        versioncreated: any;
+        body_footer: string;
+        sign_off: string;
+        feature_media: any;
+        media_description: string;
+        associations: { string: IArticle };
+        type: 'text' | 'picture' | 'video' | 'audio' | 'preformatted' | 'graphic' | 'composite';
+        firstpublished?: string;
+        linked_in_packages: any;
+        gone: any;
+        lock_action: any;
+        lock_user: any;
+        lock_session: any;
+        rewritten_by?: string;
+
+        highlights?: Array<string>;
+
+        // storage for custom fields created by users
+        extra?: {[key: string]: any};
 
         task: {
             desk: IDesk['_id'];
@@ -112,14 +159,18 @@ declare module 'superdesk-api' {
             user: IUser['_id'];
         };
 
+        // might be only used for client-side state
+        created: any;
+        archived: any;
+
         // remove when SDESK-4343 is done.
-        selected: boolean;
+        selected: any;
 
         // planning extension
         assignment_id?: string;
 
         // markForUser extension
-        marked_for_user?: string | null;
+        marked_for_user?: string;
     }
 
     export interface IUserRole extends IBaseRestApiResponse {
@@ -637,80 +688,5 @@ declare module 'superdesk-api' {
     }
 
 
-
-    // SUPERDESK ENTITIES
-
-    export interface IAuthor {
-        role: string;
-        parent: string;
-    }
-
-    export interface IArticle extends IBaseRestApiResponse {
-        _id: string;
-        _current_version: number;
-        guid: string;
-        translated_from: string;
-        translation_id: string;
-        usageterms: any;
-        keywords: any;
-        language: any;
-        slugline: any;
-        genre: any;
-        anpa_take_key: any;
-        place: any;
-        priority: any;
-        urgency: any;
-        anpa_category: any;
-        subject: any;
-        company_codes: Array<any>;
-        ednote: string;
-        authors: Array<IAuthor>;
-        headline: string;
-        sms: string;
-        abstract: string;
-        byline: string;
-        dateline: string;
-        body_html: string;
-        footer: string;
-        firstcreated: any;
-        versioncreated: any;
-        body_footer: string;
-        sign_off: string;
-        feature_media: any;
-        media_description: string;
-        associations: { string: IArticle };
-        type: 'text' | 'picture' | 'video' | 'audio' | 'preformatted' | 'graphic' | 'composite';
-        firstpublished?: string;
-        linked_in_packages: any;
-        gone: any;
-        lock_action: any;
-        lock_user: any;
-        lock_session: any;
-        rewritten_by?: string;
-
-        highlights?: Array<string>;
-
-        // storage for custom fields created by users
-        extra?: {[key: string]: any};
-
-        task: {
-            desk: IDesk['_id'];
-            stage: IStage['_id'];
-            user: IUser['_id'];
-        };
-
-        // might be only used for client-side state
-        created: any;
-        archived: any;
-
-        // remove when SDESK-4343 is done.
-        selected: any;
-
-        // planning extension
-        assignment_id?: string;
-
-        // markForUser extension
-        marked_for_user?: string;
-    }
 
 }
