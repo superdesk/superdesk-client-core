f4022bc847ff0dcbf4a1a434a0d66ba20419f6b0
Nemanja Pavlovic <me@pavlovicnemanja.com>
2019-07-25 18:08:02 +0200

    feat(metadata): Fixed issue with coping and pasting metadata (#3035)
    * feat(metadata): Fixed issue with coping and pasting metadata
    
    * fix(lint): Fixed lint issues

--- scripts/apps/authoring/media/MediaCopyMetadataDirective.ts
+++ scripts/apps/authoring/media/MediaCopyMetadataDirective.ts
@@ -1,23 +1,29 @@
+import {keys, pick, merge} from 'lodash';
+
 MediaCopyMetadataDirective.$inject = [];
 export default function MediaCopyMetadataDirective() {
     return {
         scope: {
             metadata: '=',
+            validator: '=',
             onChange: '&',
         },
         template: require('./views/media-copy-metadata-directive.html'),
         link: (scope) => {
             const METADATA_ITEMS = 'metadata:items';
+            const FIELD_KEYS = keys(scope.validator);
 
             scope.metadataFromStorage = !!localStorage.getItem(METADATA_ITEMS);
 
             scope.copyMetadata = (metadata) => {
                 scope.metadataFromStorage = true;
-                localStorage.setItem(METADATA_ITEMS, JSON.stringify(metadata));
+                localStorage.setItem(METADATA_ITEMS, JSON.stringify(pick(metadata, FIELD_KEYS)));
             };
 
             scope.pasteMetadata = () => {
-                scope.metadata = JSON.parse(localStorage.getItem(METADATA_ITEMS));
+                const metadataFromStorage = JSON.parse(localStorage.getItem(METADATA_ITEMS));
+
+                scope.metadata = merge(scope.metadata, metadataFromStorage);
                 scope.onChange();
             };
 
