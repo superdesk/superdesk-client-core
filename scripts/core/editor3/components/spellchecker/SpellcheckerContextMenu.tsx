c090bc5592ee8e09f53d69617c39035c63961cfa
pablopunk <pablovarela182@gmail.com>
2019-07-29 11:20:46 +0200

    Show message for style suggestions in the spellchecker

--- scripts/core/editor3/components/spellchecker/SpellcheckerContextMenu.tsx
+++ scripts/core/editor3/components/spellchecker/SpellcheckerContextMenu.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import * as actions from '../../actions';
-import {StickElementsWithTracking} from 'core/helpers/dom/stickElementsWithTracking';
-import {ISpellcheckWarning, ISpellchecker} from './interfaces';
-import {reloadSpellcheckerWarnings} from '../../actions';
+import { StickElementsWithTracking } from 'core/helpers/dom/stickElementsWithTracking';
+import {
+    ISpellcheckWarning,
+    ISpellchecker,
+    ISpellcheckerSuggestion
+} from './interfaces';
+import { reloadSpellcheckerWarnings } from '../../actions';
 
 interface IProps {
     warning: ISpellcheckWarning;
@@ -17,78 +21,115 @@ export class SpellcheckerContextMenuComponent extends React.Component<IProps> {
     dropdownElement: any;
 
     componentDidMount() {
-        this.stickyElementTracker = new StickElementsWithTracking(this.props.targetElement, this.dropdownElement);
+        this.stickyElementTracker = new StickElementsWithTracking(
+            this.props.targetElement,
+            this.dropdownElement,
+        );
     }
     componentWillUnmount() {
         this.stickyElementTracker.destroy();
     }
 
+    onSuggestionClick(suggestion: ISpellcheckerSuggestion) {
+        this.props.dispatch(
+            actions.replaceWord({
+                word: {
+                    text: this.props.warning.text,
+                    offset: this.props.warning.startOffset,
+                },
+                newWord: suggestion.text,
+            }),
+        );
+    }
+
     render() {
-        const {suggestions} = this.props.warning;
-        const {spellchecker} = this.props;
+        const { suggestions, message } = this.props.warning;
+        const { spellchecker } = this.props;
+
+        // If the message exists, use it as the button text
+        // instead of the suggestion
+        const messageExists = Boolean(message);
 
         return (
-            <div className={'dropdown open suggestions-dropdown'}
-                ref={(el) => this.dropdownElement = el}
-                style={{zIndex: 999, border: 'solid transparent', borderWidth: '6px 0'}}
-                data-test-id="spellchecker-menu"
+            <div
+                className={'dropdown open suggestions-dropdown'}
+                ref={(el) => (this.dropdownElement = el)}
+                style={{
+                    zIndex: 999,
+                    border: 'solid transparent',
+                    borderWidth: '6px 0',
+                }}
+                data-test-id='spellchecker-menu'
             >
-                <ul className={'dropdown__menu'} style={{position: 'static'}}>
-                    <div className="form-label" style={{margin: '0 16px'}}>{gettext('Suggestions')}</div>
-                    {
-                        suggestions.length === 0
-                            ? <li><button>{gettext('SORRY, NO SUGGESTIONS.')}</button></li>
-                            : suggestions.map((suggestion, index) =>
+                <ul className={'dropdown__menu'} style={{ position: 'static' }}>
+                    <div className='form-label' style={{ margin: '0 16px' }}>
+                        {gettext('Suggestions')}
+                    </div>
+                    {messageExists && (
+                        <li>
+                            <button onMouseDown={() => this.onSuggestionClick(suggestions[0])}>{message}</button>
+                        </li>
+                    )}
+                    {!messageExists &&
+                        (suggestions.length === 0 ? (
+                            <li>
+                                <button>
+                                    {gettext('SORRY, NO SUGGESTIONS.')}
+                                </button>
+                            </li>
+                        ) : (
+                            suggestions.map((suggestion, index) => (
                                 <li key={index}>
                                     <button
-                                        onMouseDown={() => {
-                                            this.props.dispatch(actions.replaceWord({
-                                                word: {
-                                                    text: this.props.warning.text,
-                                                    offset: this.props.warning.startOffset,
-                                                },
-                                                newWord: suggestion.text,
-                                            }));
-                                        }}
-                                        data-test-id="spellchecker-menu--suggestion"
+                                        onMouseDown={() =>
+                                            this.onSuggestionClick(suggestion)
+                                        }
+                                        data-test-id='spellchecker-menu--suggestion'
                                     >
                                         {suggestion.text}
                                     </button>
-                                </li>,
-                            )
-                    }
-                    {
-                        Object.keys(spellchecker.actions).length < 1 ? null : (
-                            <div>
-                                <li className="divider"/>
-                                <div className="form-label" style={{margin: '0 16px'}}>{gettext('Actions')}</div>
-                                {
-                                    Object.keys(spellchecker.actions).map((key, i) => {
-                                        const action = spellchecker.actions[key];
-
-                                        return (
-                                            <li key={i}>
-                                                <button
-                                                    onMouseDown={() => action.perform(this.props.warning).then(
-                                                        () => {
-                                                            this.props.dispatch(reloadSpellcheckerWarnings());
-                                                        },
-                                                    )}
-                                                    data-test-id="spellchecker-menu--action"
-                                                >
-                                                    {action.label}
-                                                </button>
-                                            </li>
-                                        );
-                                    })
-                                }
+                                </li>
+                            ))
+                        ))}
+                    {Object.keys(spellchecker.actions).length < 1 ? null : (
+                        <div>
+                            <li className='divider' />
+                            <div
+                                className='form-label'
+                                style={{ margin: '0 16px' }}
+                            >
+                                {gettext('Actions')}
                             </div>
-                        )
-                    }
+                            {Object.keys(spellchecker.actions).map((key, i) => {
+                                const action = spellchecker.actions[key];
+
+                                return (
+                                    <li key={i}>
+                                        <button
+                                            onMouseDown={() =>
+                                                action
+                                                    .perform(this.props.warning)
+                                                    .then(() => {
+                                                        this.props.dispatch(
+                                                            reloadSpellcheckerWarnings(),
+                                                        );
+                                                    })
+                                            }
+                                            data-test-id='spellchecker-menu--action'
+                                        >
+                                            {action.label}
+                                        </button>
+                                    </li>
+                                );
+                            })}
+                        </div>
+                    )}
                 </ul>
             </div>
         );
     }
 }
 
-export const SpellcheckerContextMenu = connect()(SpellcheckerContextMenuComponent);
+export const SpellcheckerContextMenu = connect()(
+    SpellcheckerContextMenuComponent,
+);
