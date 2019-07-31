41cb8f3f041ebfb837144014bc24904de1cb1a14
fritzSF <vladimir.stefanovic@sourcefabric.org>
2019-07-31 12:17:22 +0200

    [SDESK-4533] Articles with long headlines should show limited in the list display (#3045)

--- spec/helpers/monitoring.ts
+++ spec/helpers/monitoring.ts
@@ -795,7 +795,7 @@ class Monitoring {
             var highlightList = element(by.className('highlights-list-menu'));
 
             waitFor(highlightList);
-            highlightList.all(by.className('btn--mini')).first().click();
+            highlightList.all(by.className('btn--small')).first().click();
         };
 
         /**
@@ -810,7 +810,7 @@ class Monitoring {
             var deskList = element(by.className('highlights-list-menu'));
 
             waitFor(deskList);
-            deskList.all(by.className('btn--mini')).first().click();
+            deskList.all(by.className('btn--small')).first().click();
         };
 
         /**
