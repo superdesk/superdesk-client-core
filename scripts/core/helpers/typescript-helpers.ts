61928ea4e728c35f40e46d742780d110e11edc1d
Tomas Kikutis <t.kikutis@gmail.com>
2019-08-08 17:12:21 +0200

    fix lint

--- scripts/core/helpers/typescript-helpers.ts
+++ scripts/core/helpers/typescript-helpers.ts
@@ -2,6 +2,7 @@ export function nameof<T>(name: keyof T) {
     return name;
 }
 
+// eslint-disable-next-line space-infix-ops
 export type Writeable<T> = { -readonly [P in keyof T]-?: T[P] };
 
 export function assertNever(x: never): never {
