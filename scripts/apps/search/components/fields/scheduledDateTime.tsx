5ba20a1bb05316961bee6094bb27c61be5f163a8
GyanP <gyanig72@gmail.com>
2019-08-14 12:27:27 +0530

    use pure component

--- scripts/apps/search/components/fields/scheduledDateTime.tsx
+++ scripts/apps/search/components/fields/scheduledDateTime.tsx
@@ -2,21 +2,25 @@ import React from 'react';
 import {get} from 'lodash';
 import {IPropsItemListInfo} from '../ListItemInfo';
 
-export const scheduledDateTime: React.StatelessComponent<Pick<IPropsItemListInfo, 'item' | 'svc'>> = (props) => {
-    const datetime = props.svc.datetime;
+class ScheduledDateTime extends React.PureComponent<Pick<IPropsItemListInfo, 'item' | 'svc'>> {
+    render() {
+        const datetime = this.props.svc.datetime;
 
-    if (props.item.state != null && props.item.state === 'scheduled') {
-        const scheduled = get(props.item, 'archive_item.schedule_settings.utc_publish_schedule');
+        if (this.props.item.state != null && this.props.item.state === 'scheduled') {
+            const scheduled = get(this.props.item, 'archive_item.schedule_settings.utc_publish_schedule');
 
-        return (
-            <span
-                key="scheduledDateTime"
-                style={{color: '#da7200', marginRight: 4}}
-            >
-                { datetime.scheduledFormat(scheduled) }
-            </span>
-        );
-    } else {
-        return null;
+            return (
+                <span
+                    key="scheduledDateTime"
+                    style={{color: '#da7200', marginRight: 4}}
+                >
+                    { datetime.scheduledFormat(scheduled) }
+                </span>
+            );
+        } else {
+            return null;
+        }
     }
-};
+}
+
+export const scheduledDateTime = ScheduledDateTime;
