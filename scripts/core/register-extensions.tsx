659d174eb99a5d9c8f36b80270484e5048f31219
pablopunk <pablovarela182@gmail.com>
2019-08-22 10:22:09 +0200

    Missing comma

--- scripts/core/register-extensions.tsx
+++ scripts/core/register-extensions.tsx
@@ -24,7 +24,7 @@ export function registerExtensions(
                 lock,
                 session,
                 authoringWorkspace,
-                config
+                config,
             );
 
             return extensionObject.extension.activate(superdeskApi).then((activationResult) => {
