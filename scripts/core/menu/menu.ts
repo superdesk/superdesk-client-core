5aebc6e6630401e4bfceec8537ec05e961e4bf93
Tomas Kikutis <t.kikutis@gmail.com>
2019-09-06 22:35:48 +0200

    Ensure that menu items are displayed as highlighted for child paths.

--- scripts/core/menu/menu.ts
+++ scripts/core/menu/menu.ts
@@ -1,5 +1,6 @@
 import {reactToAngular1} from 'superdesk-ui-framework';
 import {GlobalMenuHorizontal} from './GlobalMenuHorizontal';
+import {trimStartExact} from 'core/helpers/utils';
 
 SuperdeskFlagsService.$inject = ['config'];
 function SuperdeskFlagsService(config) {
@@ -80,7 +81,19 @@ angular.module('superdesk.core.menu', [
         'config',
         'deployConfig',
         'lodash',
-        function($route, superdesk, betaService, userNotifications, asset, privileges, config, deployConfig, _) {
+        'workspaceMenu',
+        function(
+            $route,
+            superdesk,
+            betaService,
+            userNotifications,
+            asset,
+            privileges,
+            config,
+            deployConfig,
+            _,
+            workspaceMenu,
+        ) {
             return {
                 require: '^sdSuperdeskView',
                 templateUrl: asset.templateUrl('core/menu/views/menu.html'),
@@ -90,6 +103,71 @@ angular.module('superdesk.core.menu', [
                     scope.menu = [];
                     scope.isTestEnvironment = config.isTestEnvironment;
                     scope.environmentName = config.environmentName;
+
+                    // menu items and groups - start
+                    let group = null;
+
+                    scope.items = [];
+
+                    workspaceMenu
+                        .filter((item) => !item.if || scope.$eval(item.if))
+                        .forEach((item) => {
+                            const itemGroup = item.group || group;
+
+                            if (itemGroup !== group) {
+                                scope.items.push({hr: 1});
+                                group = itemGroup;
+                            }
+
+                            scope.items.push(item);
+                        });
+                    // menu items and groups - end
+
+                    /*
+                        Marking item as active even if current path doesn't match exactly to item href path
+                        For example, if current path is /a/b/c and there is only menu item for /a/b,
+                        /a/b should be set active.
+                    */
+                    function getActiveMenuItemPath(currentPath: string) {
+                        // get a map between full urls and their parts
+                        // {'/a/b/c': ['a', 'b', 'c']}
+                        const hrefs: {[fullPath: string]: Array<string>} = {};
+
+                        for (const item of scope.items) {
+                            if (typeof item.href === 'string' && item.href.length > 0) {
+                                hrefs[item.href] = trimStartExact(item.href, '/').split('/');
+                            }
+                        }
+
+                        // get all parts of current route path
+                        const currentUrlSplit = trimStartExact(currentPath, '/').split('/');
+
+                        // Try matching all `x` parts of the current route path.
+                        // If no match is found, try matching for n-1 path parts, then for n-2 parts and so on
+                        // Example:  ['a', 'b', 'c'] ['a', 'b'] it will first try to see if all 3 array elements match,
+                        // then first 2 and lastly it will only check if the first one matches.
+                        for (let x = currentUrlSplit.length; x > 0; x--) {
+
+                            // iterate hrefs and try matching `x` parts for every href
+                            for (const key of Object.keys(hrefs)) {
+                                let matches = true;
+
+                                // test if all parts match for current href
+                                for (var i = x; i > 0; i--) {
+                                    if (currentUrlSplit[i - 1] !== hrefs[key][i - 1]) {
+                                        matches = false;
+                                        break;
+                                    }
+                                }
+
+                                if (matches) {
+                                    return key;
+                                }
+                            }
+                        }
+
+                        return null;
+                    }
 
                     deployConfig.get('feedback_url').then((url) => {
                         scope.feedback_url = url;
@@ -129,6 +207,8 @@ angular.module('superdesk.core.menu', [
                             activity.isActive = route && route.href &&
                                     route.href.substr(0, activity.href.length) === activity.href;
                         });
+
+                        scope.activeMenuItemPath = getActiveMenuItemPath(route.href);
                     }
 
                     scope.$on('$locationChangeStart', () => {
