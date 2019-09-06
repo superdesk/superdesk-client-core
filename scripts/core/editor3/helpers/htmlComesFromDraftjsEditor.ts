94297b572230196e7b93b7933effe129a8a15cfe
pablopunk <pablovarela182@gmail.com>
2019-09-06 15:32:16 +0200

    Lint

--- scripts/core/editor3/helpers/htmlComesFromDraftjsEditor.ts
+++ scripts/core/editor3/helpers/htmlComesFromDraftjsEditor.ts
@@ -1,1 +1,2 @@
-export const htmlComesFromDraftjsEditor = (html: string) => new DOMParser().parseFromString(html, 'text/html').querySelector('[data-offset-key]') != null;
+export const htmlComesFromDraftjsEditor = (html: string) =>
+    new DOMParser().parseFromString(html, 'text/html').querySelector('[data-offset-key]') != null;
