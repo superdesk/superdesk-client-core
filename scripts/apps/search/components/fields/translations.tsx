24e67b5d032d19a3aebdfbc5266d40bf1be5fcf5
Petr Jasek <jasekpetr@gmail.com>
2019-09-23 11:33:11 +0200

    use fragment instead of an array

--- scripts/apps/search/components/fields/translations.tsx
+++ scripts/apps/search/components/fields/translations.tsx
@@ -26,50 +26,44 @@ export class Translations extends React.PureComponent<IProps, IState> {
     }
 
     render() {
-        const output = [];
+        return (
+            <React.Fragment>
+                {this.props.item.translated_from != null && (
+                    <button key="translated" className="label label--hollow"
+                        onClick={(event) => {
+                            this.renderOriginalArticle(event.target);
+                        }}>
+                        {gettext('translation')}
+                    </button>
+                )}
 
-        if (this.props.item.translated_from != null) {
-            output.push(
-                <button key="translated" className="label label--hollow"
-                    onClick={(event) => {
-                        this.renderOriginalArticle(event.target);
-                    }}>
-                    {gettext('translation')}
-                </button>,
-            );
-        }
+                {this.props.item.translations != null && this.props.item.translations.length > 0 && (
+                    <button key="translations" className="text-link"
+                        onClick={(event) => {
+                            this.renderTranslations(event.target);
+                        }}>
+                        {'('}<b>{this.props.item.translations.length}</b>{')'}
+                        {' '}
+                        {gettextPlural(this.props.item.translations.length, 'translation', 'translations')}
+                    </button>
+                )}
 
-        if (this.props.item.translations != null && this.props.item.translations.length > 0) {
-            output.push(
-                <button key="translations" className="text-link"
-                    onClick={(event) => {
-                        this.renderTranslations(event.target);
-                    }}>
-                    {'('}<b>{this.props.item.translations.length}</b>{')'}
-                    {' '}
-                    {gettextPlural(this.props.item.translations.length, 'translation', 'translations')}
-                </button>,
-            );
-        }
-
-        if (this.state.popup != null) {
-            output.push(
-                <ItemListPopup
-                    key="popup"
-                    target={this.state.popup.target}
-                    label={this.state.popup.label}
-                    close={this.closePopup}>
-                    <TranslationsList
-                        ids={this.state.popup.ids}
-                        onClick={(item) => {
-                            ng.get('$rootScope').$broadcast('broadcast:preview', {item});
-                        }}
-                    />
-                </ItemListPopup>,
-            );
-        }
-
-        return output.length > 0 ? output : null;
+                {this.state.popup != null && (
+                    <ItemListPopup
+                        key="popup"
+                        target={this.state.popup.target}
+                        label={this.state.popup.label}
+                        close={this.closePopup}>
+                        <TranslationsList
+                            ids={this.state.popup.ids}
+                            onClick={(item) => {
+                                ng.get('$rootScope').$broadcast('broadcast:preview', {item});
+                            }}
+                        />
+                    </ItemListPopup>
+                )}
+            </React.Fragment>
+        );
     }
 
     renderOriginalArticle(elem: EventTarget) {
