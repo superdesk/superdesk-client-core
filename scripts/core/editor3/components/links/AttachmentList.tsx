1c0d43705b2dbe44f5c8c670a1b583ca4fe8f35e
pablopunk <pablovarela182@gmail.com>
2019-09-26 15:11:10 +0200

    Refactor attachment list component

--- scripts/core/editor3/components/links/AttachmentList.tsx
+++ scripts/core/editor3/components/links/AttachmentList.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-multi-comp */
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
@@ -6,34 +7,22 @@ import ng from 'core/services/ng';
 import {FileiconFilter, FilesizeFilter} from 'core/ui/ui';
 import {gettext} from 'core/utils';
 import {IAttachment} from 'apps/authoring/attachments';
+import {IArticle} from 'superdesk-api';
 
-export class AttachmentList extends React.Component<any, any> {
-    static propTypes: any;
-    static defaultProps: any;
+interface IAttachmentItemProps {
+    attachment: IAttachment;
+    selected: boolean;
+    onClick: (attachment: IAttachment) => void;
+}
 
-    constructor(props) {
-        super(props);
-        this.state = {};
-        this.renderAttachment = this.renderAttachment.bind(this);
-    }
-
-    componentDidMount() {
-        ng.get('attachments').byItem(this.props.item)
-            .then((attachments) => {
-                this.setState({attachments: attachments});
-            });
-    }
-
-    renderAttachment(attachment: IAttachment) {
-        if (attachment.internal === true) {
-            return null;
-        }
-
+class AttachmentItem extends React.Component<IAttachmentItemProps> {
+    render() {
+        const {attachment} = this.props;
         const fileicon = 'big-icon--' + FileiconFilter()(attachment.mimetype);
         const filesize = FilesizeFilter()(attachment.media.length);
 
         const className = classNames('sd-list-item', {
-            'sd-list-item--active': attachment._id === this.props.selected,
+            'sd-list-item--active': this.props.selected,
         });
 
         return (
@@ -48,13 +37,36 @@ export class AttachmentList extends React.Component<any, any> {
                     <div className="sd-list-item__row">
                         <h5>{attachment.filename} ({filesize})</h5>
                     </div>
-                    <div className="sd-list-item__row">
-                        <div className="description">{attachment.description}</div>
-                        <div className="description">{attachment.description}</div>
+                    <div className="sd-list-item__row description">
+                        {attachment.description}
                     </div>
                 </div>
             </div>
         );
+    }
+}
+
+interface IProps {
+    item: IArticle;
+    selected: string;
+    onClick: (attachment: IAttachment) => void;
+}
+
+interface IState {
+    attachments?: Array<IAttachment>;
+}
+
+export class AttachmentList extends React.Component<IProps, IState> {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    componentDidMount() {
+        ng.get('attachments').byItem(this.props.item)
+            .then((attachments) => {
+                this.setState({attachments: attachments});
+            });
     }
 
     render() {
@@ -62,9 +74,15 @@ export class AttachmentList extends React.Component<any, any> {
             return <div />; // wait for attachments
         }
 
-        const attachments = this.state.attachments
-            .map(this.renderAttachment)
-            .filter(Boolean);
+        const publicAttachments = this.state.attachments.filter(({internal}) => !internal);
+
+        const attachments = publicAttachments
+            .map((attachement) =>
+                <AttachmentItem
+                    key={`attachment-${attachement._id}`}
+                    attachment={attachement}
+                    selected={attachement._id === this.props.selected}
+                    onClick={this.props.onClick} />);
 
         if (attachments.length) {
             return <div>{attachments}</div>;
@@ -72,14 +90,8 @@ export class AttachmentList extends React.Component<any, any> {
 
         return (
             <p style={{padding: 20, margin: 0}}>
-                {gettext('There are no attachments yet. Upload some first using Attachments widget.')}
+                {gettext('There are no public attachments yet. Upload some first using Attachments widget.')}
             </p>
         );
     }
 }
-
-AttachmentList.propTypes = {
-    item: PropTypes.object.isRequired,
-    selected: PropTypes.string,
-    onClick: PropTypes.func,
-};
