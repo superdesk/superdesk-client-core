2b4b4c89a9d5c278c93ef678e3eed63fcf322349
Loi Tran <loi.tran@idsolutions.com.vn>
2019-10-10 14:38:17 +0700

    Add upload function for preview thumbnail

--- scripts/extensions/videoEditor/src/VideoPreviewThumbnail/VideoPreviewThumbnail.tsx
+++ scripts/extensions/videoEditor/src/VideoPreviewThumbnail/VideoPreviewThumbnail.tsx
@@ -13,6 +13,7 @@ interface IState {
 export class VideoPreviewThumbnail extends React.Component<IProps, IState> {
     static contextType = VideoEditorContext;
     private ref: React.RefObject<HTMLCanvasElement>;
+    private size: number;
 
     constructor(props: IProps) {
         super(props);
@@ -21,13 +22,27 @@ export class VideoPreviewThumbnail extends React.Component<IProps, IState> {
             show: false,
         };
         this.ref = React.createRef();
+        this.size = 160; // max size of element canvas
     }
 
     handleClick = () => {
         this.setState({ dirty: true });
-        const ctx = this.ref.current!.getContext('2d');
-        console.log(this.props);
-        ctx!.drawImage(this.props.videoRef.current!, 0, 0, this.ref.current!.width, this.ref.current!.height);
+        const video = this.props.videoRef.current;
+        if (!video) return;
+        this.drawCanvas(video, video.videoWidth, video.videoHeight);
+    };
+
+    handleChange = (files: FileList | null) => {
+        const reader = new FileReader();
+        reader.onload = () => {
+            const image = new Image();
+            image.onload = () => {
+                this.drawCanvas(image, image.width, image.height);
+            };
+            image.src = reader.result as string;
+        };
+
+        reader.readAsDataURL(files![0]);
     };
 
     handleSave = () => {};
@@ -38,11 +53,28 @@ export class VideoPreviewThumbnail extends React.Component<IProps, IState> {
         ctx!.clearRect(0, 0, this.ref.current!.width, this.ref.current!.height);
     };
 
+    drawCanvas = (element: any, width: number, height: number) => {
+        const ctx = this.ref.current!.getContext('2d');
+        const ratio = width / height;
+        let [drawWidth, drawHeight] = [this.size, this.size];
+        if (ratio > 1) {
+            drawHeight = this.size / ratio;
+        } else {
+            drawWidth = this.size * ratio;
+        }
+        this.ref.current!.width = drawWidth;
+        this.ref.current!.height = drawHeight;
+        ctx!.drawImage(element, 0, 0, drawWidth, drawHeight);
+        this.setState({ dirty: true });
+    };
+
     render() {
         const { getClass } = this.context.superdesk.utilities.CSS;
         return (
             <div className="sd-photo-preview__thumbnail-edit">
-                <canvas ref={this.ref} className={getClass('thumbnail-edit__preview')}></canvas>
+                <div className={getClass('thumbnail-edit__preview')}>
+                    <canvas ref={this.ref} className={getClass('thumbnail-edit__preview-canvas')}></canvas>
+                </div>
                 <div className={getClass('thumbnail-edit__container')}>
                     {!this.state.dirty ? (
                         <>
@@ -58,6 +90,7 @@ export class VideoPreviewThumbnail extends React.Component<IProps, IState> {
                                         type="file"
                                         style={{ display: 'none' }}
                                         accept=".png,.jpg,.jpeg,.webp"
+                                        onChange={e => this.handleChange(e.target.files)}
                                     />
                                     <i className="icon-upload icon--white"></i>
                                 </label>
