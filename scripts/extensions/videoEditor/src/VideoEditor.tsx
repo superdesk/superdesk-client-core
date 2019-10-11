747d3a7841c56d25608edf1ca342be5d9e4dc9a5
Loi Tran <loi.tran@idsolutions.com.vn>
2019-10-11 10:33:27 +0700

    Fix error display save button on turning off crop

--- scripts/extensions/videoEditor/src/VideoEditor.tsx
+++ scripts/extensions/videoEditor/src/VideoEditor.tsx
@@ -110,9 +110,10 @@ export class VideoEditor extends React.Component<IProps, IState> {
 
         this.setState({ cropEnabled: !this.state.cropEnabled }, () => {
             if (this.state.cropEnabled === false) {
-                this.setState({ crop: this.initState.crop });
+                this.setState({ crop: this.initState.crop }, this.checkIsDirty);
+            } else {
+                this.checkIsDirty();
             }
-            this.checkIsDirty();
         });
     };
 
