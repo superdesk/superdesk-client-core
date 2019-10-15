dc4cf701258f76b1e785b836733a66814bada09e
Tomas Kikutis <t.kikutis@gmail.com>
2019-10-15 15:39:12 +0200

    Increase the limit of displayed marked items (#3157)

--- scripts/core/superdesk-api.d.ts
+++ scripts/core/superdesk-api.d.ts
@@ -414,7 +414,7 @@ declare module 'superdesk-api' {
         endpoint: string;
         page: {
             from: number;
-            size?: number;
+            size: number;
         };
         sort: Array<{[field: string]: 'asc' | 'desc'}>;
 
