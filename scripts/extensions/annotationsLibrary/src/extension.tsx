ce74c12f2afdaa299b82da32db6c03cb327437c6
Pablo P Varela <pablovarela182@gmail.com>
2019-11-08 15:29:50 +0100

    Don't duplicate annotation from library when reusing it (#3200)
    SDESK-4800

--- scripts/extensions/annotationsLibrary/src/extension.tsx
+++ scripts/extensions/annotationsLibrary/src/extension.tsx
@@ -6,28 +6,48 @@ import {IKnowledgeBaseItem} from './interfaces';
 
 const RESOURCE = 'concept_items';
 
+function annotationExistsInKnowledgeBase(superdesk: ISuperdesk, annotationText: string) {
+    const {dataApi} = superdesk;
+    const {nameField} = getFields(superdesk);
+    const {generateFilterForServer} = superdesk.forms;
+
+    return dataApi.query<IKnowledgeBaseItem>(
+        RESOURCE,
+        1,
+        {field: 'name', direction: 'ascending'},
+        {name: generateFilterForServer(nameField.type, annotationText)},
+    ).then((res: IRestApiResponse<IKnowledgeBaseItem>) => res._items.length > 0);
+}
+
 function annotationFromLibraryTabSelectedByDefault(
     superdesk: ISuperdesk,
     annotationText: string,
     mode: 'create' | 'edit',
 ) {
-    const {dataApi} = superdesk;
     const {assertNever} = superdesk.helpers;
-    const {nameField} = getFields(superdesk);
-    const {generateFilterForServer} = superdesk.forms;
 
     if (mode === 'edit') {
         return Promise.resolve(false);
     } else if (mode === 'create') {
-        return dataApi.query<IKnowledgeBaseItem>(
-            RESOURCE,
-            1,
-            {field: 'name', direction: 'ascending'},
-            {name: generateFilterForServer(nameField.type, annotationText)},
-        ).then((res: IRestApiResponse<IKnowledgeBaseItem>) => res._items.length > 0);
+        return annotationExistsInKnowledgeBase(superdesk, annotationText);
     } else {
         return assertNever(mode);
     }
+}
+
+function onAnnotationCreate(superdesk: ISuperdesk, language: string, annotationText: string, definitionHtml: string) {
+    annotationExistsInKnowledgeBase(superdesk, annotationText)
+        .then((exists) => {
+            // Don't create a knowledge base item for that annotation if it already exists
+            if (!exists) {
+                superdesk.dataApi.create(RESOURCE, {
+                    language: language,
+                    name: annotationText,
+                    definition_html: definitionHtml,
+                    cpnat_type: 'cpnat:abstract',
+                });
+            }
+        });
 }
 
 var extension: IExtension = {
@@ -44,12 +64,7 @@ var extension: IExtension = {
                             selectedByDefault: (annotationText: string, mode: 'create' | 'edit') =>
                                 annotationFromLibraryTabSelectedByDefault(superdesk, annotationText, mode),
                             onAnnotationCreate: (language: string, annotationText: string, definitionHtml: string) => {
-                                superdesk.dataApi.create(RESOURCE, {
-                                    language: language,
-                                    name: annotationText,
-                                    definition_html: definitionHtml,
-                                    cpnat_type: 'cpnat:abstract',
-                                });
+                                return onAnnotationCreate(superdesk, language, annotationText, definitionHtml);
                             },
                         },
                     ],
