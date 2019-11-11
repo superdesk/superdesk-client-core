ad3142435bc8437139f00215ba96bd507a37e00e
Tomas Kikutis <t.kikutis@gmail.com>
2019-11-11 18:11:44 +0100

    display the entire datetime only if the switch is enabled

--- scripts/extensions/datetimeField/src/extension.tsx
+++ scripts/extensions/datetimeField/src/extension.tsx
@@ -17,101 +17,113 @@ class DateTimeField extends React.PureComponent<IEditorComponentProps> {
         const day = date == null ? '' : date.toISOString().slice(0, 10);
         const hour = date == null ? '' : date.toISOString().slice(11, 16);
 
-        return (
-            <div>
-                <input
-                    type="checkbox"
-                    checked={selected}
-                    onChange={(event) => {
-                        if (event.target.value === 'on' && selected !== true) {
-                            this.props.setValue(new Date().toISOString());
-                        } else {
-                            this.props.setValue(null);
-                        }
-                    }}
-                />
-
-                <input
-                    type="date"
-                    value={day}
-                    onChange={(event) => {
-                        if (event.target.value === '') {
-                            this.props.setValue(null);
-                            return;
-                        }
-
-                        const value = event.target.value.split('-');
-                        const nextDate = date ?? moment();
-
-                        nextDate.set({
-                            year: parseInt(value[0], 10),
-                            month: parseInt(value[1], 10) - 1,
-                            date: parseInt(value[2], 10),
-                        });
-
-                        this.props.setValue(nextDate.toISOString());
-                    }}
-                    style={{width: 130}}
-                />
-
-                @
-
-                <input
-                    type="time"
-                    value={hour}
-                    onChange={(event) => {
-                        if (event.target.value === '') {
-                            return;
-                        }
-
-                        const value = event.target.value.split(':');
-                        const nextDate = date ?? moment();
-
-                        nextDate.set({
-                            hour: parseInt(value[0], 10) + 1,
-                            minute: parseInt(value[1], 10),
-                        });
-                        this.props.setValue(nextDate.toISOString());
-                    }}
-                    style={{width: 130}}
-                />
-
-                <button
-                    disabled={date == null}
-                    onClick={() => {
-                        if (date != null) {
-                            date.add('minutes', 10);
-                            this.props.setValue(date.toISOString());
-                        }
-                    }}
-                >
-                    + 10 min
-                </button>
-                <button
-                    disabled={date == null}
-                    onClick={() => {
-                        if (date != null) {
-                            date.add('minutes', 30);
-                            this.props.setValue(date.toISOString());
-                        }
-                    }}
-                >
-                    + 30 min
-                </button>
-                <button
-                    disabled={date == null}
-                    onClick={() => {
-                        if (date != null) {
-                            date.add('minutes', 60);
-                            this.props.setValue(date.toISOString());
-                        }
-                    }}
-                >
-                    + 1 hour
-                </button>
-
-            </div>
+        const checkbox = (
+            <input
+                type="checkbox"
+                checked={selected}
+                onChange={(event) => {
+                    if (event.target.value === 'on' && selected !== true) {
+                        this.props.setValue(new Date().toISOString());
+                    } else {
+                        this.props.setValue(null);
+                    }
+                }}
+            />
         );
+
+        if (!selected) {
+            return (
+                <div>
+                    {checkbox}
+                </div>
+            );
+        } else {
+            return (
+                <div>
+                    {checkbox}
+
+                    <input
+                        type="date"
+                        value={day}
+                        onChange={(event) => {
+                            if (event.target.value === '') {
+                                this.props.setValue(null);
+                                return;
+                            }
+
+                            const value = event.target.value.split('-');
+                            const nextDate = date ?? moment();
+
+                            nextDate.set({
+                                year: parseInt(value[0], 10),
+                                month: parseInt(value[1], 10) - 1,
+                                date: parseInt(value[2], 10),
+                            });
+
+                            this.props.setValue(nextDate.toISOString());
+                        }}
+                        style={{width: 130}}
+                    />
+
+                    @
+
+                    <input
+                        type="time"
+                        value={hour}
+                        onChange={(event) => {
+                            if (event.target.value === '') {
+                                return;
+                            }
+
+                            const value = event.target.value.split(':');
+                            const nextDate = date ?? moment();
+
+                            nextDate.set({
+                                hour: parseInt(value[0], 10) + 1,
+                                minute: parseInt(value[1], 10),
+                            });
+                            this.props.setValue(nextDate.toISOString());
+                        }}
+                        style={{width: 130}}
+                    />
+
+                    <button
+                        disabled={date == null}
+                        onClick={() => {
+                            if (date != null) {
+                                date.add('minutes', 10);
+                                this.props.setValue(date.toISOString());
+                            }
+                        }}
+                    >
+                        + 10 min
+                    </button>
+                    <button
+                        disabled={date == null}
+                        onClick={() => {
+                            if (date != null) {
+                                date.add('minutes', 30);
+                                this.props.setValue(date.toISOString());
+                            }
+                        }}
+                    >
+                        + 30 min
+                    </button>
+                    <button
+                        disabled={date == null}
+                        onClick={() => {
+                            if (date != null) {
+                                date.add('minutes', 60);
+                                this.props.setValue(date.toISOString());
+                            }
+                        }}
+                    >
+                        + 1 hour
+                    </button>
+
+                </div>
+            );
+        }
     }
 }
 
