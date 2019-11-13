b64c5b28172779191906d3de6377207a57a2dd2e
Loi Tran <loi.tran@idsolutions.com.vn>
2019-11-13 10:23:09 +0700

    Use video tag for Timeline thumbnail items

--- scripts/extensions/videoEditor/src/VideoTimeline/ListThumbnails.tsx
+++ scripts/extensions/videoEditor/src/VideoTimeline/ListThumbnails.tsx
@@ -19,9 +19,9 @@ export class ListThumbnails extends React.PureComponent<IProps> {
             <div className={`${getClass('frames')} ${getClass('frames--thumbs')}`}>
                 <div className={getClass('frames__inner')}>
                     {this.props.thumbnails.map((item: IThumbnail, index: number) => (
-                        <img
+                        <video
                             className={`${getClass('frames__video')} ${item.url && getClass('frames__video--loaded')}`}
-                            src={item.url}
+                            poster={item.url}
                             width={item.width}
                             height={item.height}
                             key={index}
