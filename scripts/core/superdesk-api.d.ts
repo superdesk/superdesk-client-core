d0810b2c115bd75d65bed84eda8c3b3fddb59b52
Petr Jasek <jasekpetr@gmail.com>
2019-11-19 15:10:22 +0100

    implement mark user and send action
    SDBELGA-218

--- scripts/core/superdesk-api.d.ts
+++ scripts/core/superdesk-api.d.ts
@@ -795,6 +795,9 @@ declare module 'superdesk-api' {
                 getIptcSubjects(): Promise<Array<ISubject>>;
                 getVocabulary(id: string): Promise<Array<ISubject>>;
             };
+            desk: {
+                getDeskStages(deskId: string): Promise<Array<IStage>>;
+            };
         };
         helpers: {
             assertNever(x: never): never;
