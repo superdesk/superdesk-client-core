61b306d513f72ea6b536f7ea316dc7390f56887d
Loi Tran <loi.tran@idsolutions.com.vn>
2019-11-21 10:52:15 +0700

    Use promise instead of async/await
    Lock keycloak version to exact 6.0.1

--- scripts/core/auth/keycloak-directive.ts
+++ scripts/core/auth/keycloak-directive.ts
@@ -1,4 +1,4 @@
-import Keycloak from 'keycloak-js'
+import Keycloak from 'keycloak-js';
 
 /*
  * Keycloak modal is watching session identity, redirect to login screen
@@ -11,34 +11,35 @@ angular.module('superdesk.core.auth.keycloak', []).directive('sdKeycloakModal', 
     function(session, auth, deployConfig) {
         return {
             link: function(scope, $element) {
-                const init = async () => {
-                    const methods = await deployConfig.all({ oidc: 'oidc_auth' })
-                    const isOIDCEnabled = await methods.oidc
-                    if (!isOIDCEnabled) {
-                        return;
-                    }
-
-                    deployConfig.all({
-                        keycloak: 'keycloak_config',
-                    }).then(configs => {
-                        scope.keycloak = Keycloak(configs.keycloak)
-                        scope.keycloak.configs = configs.keycloak
-                    }).then(() => {
-                        scope.keycloak.init({
-                            onLoad: 'login-required',
-                        }).success(() => {
-                            auth.loginOIDC(scope.keycloak.token);
-                        })
-                    })
-                }
-                init()
+                const init = function() {
+                    deployConfig.all({ oidc: 'oidc_auth' }).then((methods) => {
+                        return methods.oidc;
+                    }).then((enable) => {
+                        if (!enable) {
+                            return;
+                        }
+                        deployConfig.all({
+                            keycloak: 'keycloak_config',
+                        }).then((configs) => {
+                            scope.keycloak = Keycloak(configs.keycloak);
+                            scope.keycloak.configs = configs.keycloak;
+                        }).then(() => {
+                            scope.keycloak.init({
+                                onLoad: 'login-required',
+                            }).success(() => {
+                                auth.loginOIDC(scope.keycloak.token);
+                            });
+                        });
+                    });
+                };
+                init();
 
                 scope.$watch(() => session.identity, () => {
                     if (session.identity == null && (scope.keycloak || {}).authenticated) {
-                        scope.keycloak.logout()
+                        scope.keycloak.logout();
                     }
-                })
-            }
-        }
-    }
-])
+                });
+            },
+        };
+    },
+]);
