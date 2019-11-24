ed4d16c03029c0b711b40e37b2703d3afdf5a6e5
Tomas Kikutis <t.kikutis@gmail.com>
2019-11-24 15:01:12 +0100

    code style

--- scripts/extensions/videoEditor/src/get-video-editor-modal.tsx
+++ scripts/extensions/videoEditor/src/get-video-editor-modal.tsx
@@ -4,7 +4,7 @@ import {VideoEditor} from './VideoEditor';
 import {VideoEditorProvider} from './VideoEditorContext';
 
 export function getVideoEditModal(superdesk: ISuperdesk, article: IArticle) {
-    return class VideoEditModal extends React.PureComponent<{ closeModal(): void }> {
+    return class VideoEditModal extends React.PureComponent<{closeModal(): void}> {
         render() {
             return (
                 <VideoEditorProvider value={{superdesk: superdesk}}>
