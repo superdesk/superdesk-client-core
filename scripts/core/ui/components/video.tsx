8c6054bbee2faafe684e9410244e3f2aef1c8412
Petr Jasek <jasekpetr@gmail.com>
2019-11-26 15:09:23 +0100

    fix bug when video in editor was not playable at some point
    the actuall fix is to use key on video element because otherwise
    it does not force reload.
    
    SDESK-4817

--- scripts/core/ui/components/video.tsx
+++ scripts/core/ui/components/video.tsx
@@ -3,6 +3,8 @@ import {IArticle} from 'superdesk-api';
 
 interface IProps {
     item: IArticle;
+    width?: string;
+    height?: string;
 }
 
 export class VideoComponent extends React.PureComponent<IProps> {
@@ -22,7 +24,7 @@ export class VideoComponent extends React.PureComponent<IProps> {
             .filter((rendition) => rendition.mimetype != null && rendition.mimetype.startsWith('video'));
 
         return (
-            <video controls preload="metadata" poster={poster}>
+            <video controls preload="metadata" poster={poster} width={this.props.width} height={this.props.height}>
                 {videoRenditions.map((rendition) => (
                     <source key={rendition.href} src={rendition.href} type={rendition.mimetype} />
                 ))}
