7d0a53a7f042270cd134e5ae2b5d660d844c8ddd
Petr Jašek <jasekpetr@gmail.com>
2019-11-28 12:09:43 +0100

    handle exif parsing errors on image upload (#3229)
    SDANSA-352

--- scripts/core/superdesk-api.d.ts
+++ scripts/core/superdesk-api.d.ts
@@ -909,7 +909,7 @@ declare module 'superdesk-api' {
 
     // IPTC picture metadata
 
-    interface IPTCMetadata {
+    export interface IPTCMetadata {
         // envelope
         Destination: string;
         ServiceIdentifier: string;
