33c19e49c23d152f266ea1ea9d9aabb4492c8f33
Petr Jašek <jasekpetr@gmail.com>
2019-12-02 16:12:38 +0100

    implement autocomplete for slugline (#3223)
    * implement autocomplete for slugline
    
    SDCP-81

--- scripts/core/superdesk-api.d.ts
+++ scripts/core/superdesk-api.d.ts
@@ -433,8 +433,8 @@ declare module 'superdesk-api' {
     }
 
     // Eve properties
-    export interface IRestApiResponse<T extends IBaseRestApiResponse> {
-        _items: Array<T>;
+    export interface IRestApiResponse<T> {
+        _items: Array<T & IBaseRestApiResponse>;
         _links: {
             parent: IRestApiLink;
             self: IRestApiLink;
@@ -910,6 +910,7 @@ declare module 'superdesk-api' {
         saml_auth: any;
         google_auth: any;
         saml_label: any;
+        archive_autocomplete: boolean;
 
 
         // FROM CLIENT
