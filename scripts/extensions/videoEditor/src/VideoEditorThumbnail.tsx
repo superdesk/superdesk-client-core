a2d14bd2b5e69315a4273f11dc8bf292d79383e2
Loi Tran <loi.tran@idsolutions.com.vn>
2019-12-05 17:45:20 +0700

    Remove using url args to avoid thumbnail caching

--- scripts/extensions/videoEditor/src/VideoEditorThumbnail.tsx
+++ scripts/extensions/videoEditor/src/VideoEditorThumbnail.tsx
@@ -46,10 +46,10 @@ export class VideoEditorThumbnail extends React.Component<IProps, IState> {
     }
 
     componentDidMount() {
-        let thumbnail = this.props.article.renditions?.thumbnail?.href;
+        const thumbnail = this.props.article.renditions?.thumbnail?.href;
 
         if (thumbnail) {
-            this.setThumbnail(thumbnail + `?t=${Math.random()}`);
+            this.setThumbnail(thumbnail);
         }
     }
 
@@ -187,7 +187,7 @@ export class VideoEditorThumbnail extends React.Component<IProps, IState> {
                     });
                     this.clearCanvas();
                     this.props.onSave(res);
-                    this.setThumbnail(res.renditions?.thumbnail?.href + `?t=${Math.random()}`);
+                    this.setThumbnail(res.renditions?.thumbnail?.href ?? '');
                 });
         }
     }
@@ -206,7 +206,7 @@ export class VideoEditorThumbnail extends React.Component<IProps, IState> {
 
         this.interval = window.setInterval(() => {
             dataApi
-                .findOne('video_edit', this.props.article._id + `?t=${Math.random()}`) // avoid caching response
+                .findOne('video_edit', this.props.article._id)
                 .then((response: IArticle) => {
                     if (response.project?.processing?.thumbnail_preview === false) {
                         clearInterval(this.interval);
