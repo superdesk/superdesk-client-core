5779d407728af802776659e2c1a5c353760572dc
Petr Jašek <jasekpetr@gmail.com>
2019-12-10 12:58:50 +0100

    avoid opening preview on select with shift (#3246)
    * avoid opening preview on select with shift
    
    SDANSA-356

--- scripts/apps/search/components/ItemList.tsx
+++ scripts/apps/search/components/ItemList.tsx
@@ -68,7 +68,7 @@ export class ItemList extends React.Component<any, IState> {
         this.unbindActionKeyShortcuts = this.unbindActionKeyShortcuts.bind(this);
     }
 
-    multiSelect(items, selected, event?) {
+    multiSelect(items, selected) {
         const {search, multi} = this.props.svc;
         const {scope} = this.props;
 
@@ -83,7 +83,6 @@ export class ItemList extends React.Component<any, IState> {
             });
         });
 
-        this.select(_.last(items), event);
         this.setState({itemsById: itemsById});
     }
 
