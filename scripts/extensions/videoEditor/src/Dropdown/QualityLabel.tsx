e9261c437b2660a6633dd15fea1f8499ce2fc86f
Loi Tran <loi.tran@idsolutions.com.vn>
2019-12-10 16:28:54 +0700

    Replace memo with PureComponent

--- scripts/extensions/videoEditor/src/Dropdown/QualityLabel.tsx
+++ scripts/extensions/videoEditor/src/Dropdown/QualityLabel.tsx
@@ -1,21 +1,21 @@
 import * as React from 'react';
 import {IDropdownLabel} from '../interfaces';
 
-function Label(props: IDropdownLabel) {
-    const value = String(props.selectedItem || 'Same');
+export class QualityLabel extends React.PureComponent<IDropdownLabel> {
+    render() {
+        const value = String(this.props.selectedItem || 'Same');
 
-    return (
-        <button
-            className="dropdown__toggle dark-ui dropdown-toggle"
-            aria-haspopup="true"
-            aria-expanded="false"
-            onClick={props.onClick}
-            sd-tooltip={props.title}
-        >
-            {props.getText!(value)}
-            <span className="dropdown__caret dropdown__caret--white" />
-        </button>
-    );
+        return (
+            <button
+                className="dropdown__toggle dark-ui dropdown-toggle"
+                aria-haspopup="true"
+                aria-expanded="false"
+                onClick={this.props.onClick}
+                sd-tooltip={this.props.title}
+            >
+                {this.props.getText!(value)}
+                <span className="dropdown__caret dropdown__caret--white" />
+            </button>
+        );
+    }
 }
-
-export const QualityLabel = React.memo(Label);
