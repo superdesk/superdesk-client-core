4a296b0b3e125863afcf7ba690ffeeb0852a8360
Loi Tran <loi.tran@idsolutions.com.vn>
2020-01-08 10:02:59 +0700

    Remove unused code

--- scripts/core/auth/keycloak-service.ts
+++ scripts/core/auth/keycloak-service.ts
@@ -14,14 +14,13 @@ angular.module('superdesk.core.auth.keycloak', []).service('keycloak', [
 
         this.keycloakAuth = () => {
             keycloak = Keycloak(appConfig.keycloak_config);
-
-            keycloak.configs = appConfig.keycloak_config;
             keycloak
                 .init({onLoad: 'login-required'})
                 .success(() => {
                     auth.loginOIDC(keycloak.token);
                 });
         };
+
         $rootScope.$watch(
             () => session.identity,
             () => {
