5f0a43c7b14d2d40ed37933c7b8a291314e78122
Tomas Kikutis <t.kikutis@gmail.com>
2020-01-29 17:31:17 +0100

    add types, remove unused code

--- scripts/apps/users/components/UserAvatar.tsx
+++ scripts/apps/users/components/UserAvatar.tsx
@@ -1,30 +1,37 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
 
-/*
- * @ngdoc React
- * @module superdesk.apps.users
- * @name UserAvatar
- * @param {string} displayName The user's display name.
- * @param {string=} pictureUrl URL of the user's avatar, if provided.
- * @description Displays the user avatar. If `pictureUrl` is provided it shows the picture
- * at that URL, otherwise it shows the initial of `displayName`.
- */
-export const UserAvatar: React.StatelessComponent<any> = ({displayName, pictureUrl, title, ...rest}) =>
-    <div className="user-avatar" {...rest} title={title || displayName} data-test-id="user-avatar">
-        <figure className={classNames(
-            'avatar avatar--no-margin',
-            {'no-bg': pictureUrl},
-            {initials: !pictureUrl},
-        )}>
-            {pictureUrl && <img src={pictureUrl} />}
-            {displayName && <span>{displayName[0].toUpperCase()}</span>}
-        </figure>
-    </div>;
+interface IProps {
+    displayName: string;
+    title?: string;
+    pictureUrl?: string;
+    status?: {online: boolean};
+}
 
-UserAvatar.propTypes = {
-    displayName: PropTypes.string.isRequired,
-    title: PropTypes.string,
-    pictureUrl: PropTypes.string,
-};
+export class UserAvatar extends React.PureComponent<IProps> {
+    render() {
+        const {displayName, pictureUrl, title, status} = this.props;
+
+        return (
+            <div className="user-avatar" title={title || displayName} data-test-id="user-avatar">
+                <figure className={[
+                    'avatar',
+                    'avatar--no-margin',
+                    pictureUrl ? 'no-bg' : 'initials',
+                ].join(' ')
+                }>
+                    {pictureUrl && <img src={pictureUrl} />}
+                    {displayName && <span>{displayName[0].toUpperCase()}</span>}
+                </figure>
+                {
+                    status == null ? null : (
+                        <div className={
+                            status.online
+                                ? 'status-indicator--online'
+                                : 'status-indicator--offline'
+                        } />
+                    )
+                }
+            </div>
+        );
+    }
+}
