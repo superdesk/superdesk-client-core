38f3f05a7bc727c1e299cc328f9b2b19636c1b49
Tomas Kikutis <t.kikutis@gmail.com>
2020-01-29 21:03:16 +0100

    drop status

--- scripts/apps/users/components/UserAvatar.tsx
+++ scripts/apps/users/components/UserAvatar.tsx
@@ -4,33 +4,28 @@ interface IProps {
     displayName: string;
     title?: string;
     pictureUrl?: string;
-    status?: {online: boolean};
 }
 
 export class UserAvatar extends React.PureComponent<IProps> {
     render() {
-        const {displayName, pictureUrl, title, status} = this.props;
+        const {displayName, pictureUrl, title} = this.props;
 
         return (
-            <div className="user-avatar" title={title || displayName} data-test-id="user-avatar">
-                <figure className={[
-                    'avatar',
-                    'avatar--no-margin',
-                    pictureUrl ? 'no-bg' : 'initials',
-                ].join(' ')
-                }>
+            <div
+                className="user-avatar"
+                title={title || displayName}
+                data-test-id="user-avatar"
+            >
+                <figure
+                    className={[
+                        'avatar',
+                        'avatar--no-margin',
+                        pictureUrl ? 'no-bg' : 'initials',
+                    ].join(' ')}
+                >
                     {pictureUrl && <img src={pictureUrl} />}
                     {displayName && <span>{displayName[0].toUpperCase()}</span>}
                 </figure>
-                {
-                    status == null ? null : (
-                        <div className={
-                            status.online
-                                ? 'status-indicator--online'
-                                : 'status-indicator--offline'
-                        } />
-                    )
-                }
             </div>
         );
     }
