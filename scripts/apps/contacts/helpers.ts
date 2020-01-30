14ec35d05d0aa599d0dcba6634d18216dad2e4bc
Tomas Kikutis <t.kikutis@gmail.com>
2020-01-30 21:59:44 +0100

    eslint fix

--- scripts/apps/contacts/helpers.ts
+++ scripts/apps/contacts/helpers.ts
@@ -90,6 +90,7 @@ export function validateMinRequiredField(contact: IContact): boolean {
             return contactHasEmailAddress(contact);
         case 'contact_phone':
         case 'mobile':
+            // eslint-disable-next-line no-empty-pattern
             return _.get(contact, field, []).length > 0 &&
                 !_.some(_.map(contact[field], 'number'), (v) => _.isEmpty(v));
         default:
