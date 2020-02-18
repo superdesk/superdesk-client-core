f84b03d90683899438a60d00772e211b7262d3fa
pablopunk <pablovarela182@gmail.com>
2020-02-18 14:18:01 +0100

    Fix issues with media carousel videos
    SDESK-5040
    
    * Live video has no `source` tag so the OWL component was not loaded
    (cause it was waiting for the tag to appear)
    * Tags like <video> are now rendered in react so they might not be
    loaded after angular finishes rendering

--- scripts/core/helpers/waitForMediaToBeReady.ts
+++ scripts/core/helpers/waitForMediaToBeReady.ts
@@ -4,44 +4,53 @@ import {logger} from 'core/services/logger';
 /**
  * @param elements An array of img, audio and video elements
  */
-export const waitForMediaToLoad = (elements: Array<HTMLImageElement | HTMLAudioElement | HTMLVideoElement>):
-Promise<void> => new Promise((resolve) => {
-    const filteredElements = elements.filter((element) => {
-        if (isImage(element)) {
-            return element.complete === false;
-        } else if (isAudio(element) || isVideo(element)) {
-            return element.readyState < 1;
-        } else {
-            logger.error(new Error('Unexpected element type'));
-            return false;
-        }
-    });
-    let itemsLeftToLoad: number = filteredElements.length;
-
-    if (itemsLeftToLoad === 0) {
-        resolve();
-        return;
-    }
-
-    const eventHandler = (event) => {
-        itemsLeftToLoad--;
+export const waitForMediaToLoad = (
+    elements: Array<HTMLImageElement | HTMLAudioElement | HTMLVideoElement>,
+): Promise<void> =>
+    new Promise((resolve) => {
+        const filteredElements = elements.filter((element) => {
+            if (isImage(element)) {
+                return element.complete === false;
+            } else if (isAudio(element) || isVideo(element)) {
+                return element.readyState < 1;
+            } else {
+                logger.error(new Error('Unexpected element type'));
+                return false;
+            }
+        });
+        let itemsLeftToLoad: number = filteredElements.length;
 
         if (itemsLeftToLoad === 0) {
             resolve();
+            return;
         }
-    };
 
-    filteredElements.forEach((element) => {
-        if (isImage(element)) {
-            // check for error in image src
-            element.addEventListener('error', eventHandler, {once: true});
-            element.addEventListener('load', eventHandler, {once: true});
-        } else if (isAudio(element) || isVideo(element)) {
-            // for audio and video check for the error in source
-            const source = element.getElementsByTagName('source')[0];
+        const eventHandler = (event?) => {
+            itemsLeftToLoad--;
 
-            source.addEventListener('error', eventHandler, {once: true});
-            element.addEventListener('loadedmetadata', eventHandler, {once: true});
-        }
+            if (itemsLeftToLoad === 0) {
+                resolve();
+            }
+        };
+
+        filteredElements.forEach((element) => {
+            if (isImage(element)) {
+                // check for error in image src
+                element.addEventListener('error', eventHandler, {once: true});
+                element.addEventListener('load', eventHandler, {once: true});
+            } else if (isAudio(element) || isVideo(element)) {
+                // for audio and video check for the error in source
+                const source = element.getElementsByTagName('source')[0];
+
+                if (!source) {
+                    // Live videos don't have a <source> tag
+                    return eventHandler();
+                }
+
+                source.addEventListener('error', eventHandler, {once: true});
+                element.addEventListener('loadedmetadata', eventHandler, {
+                    once: true,
+                });
+            }
+        });
     });
-});
