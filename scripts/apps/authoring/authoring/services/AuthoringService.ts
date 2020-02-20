3a92881eae35a473961cccaac7c68d6485d88e06
GyanP <gyanig72@gmail.com>
2020-02-20 17:34:58 +0530

    Do not allow updating of already updated item. [SDBELGA-307]

--- scripts/apps/authoring/authoring/services/AuthoringService.ts
+++ scripts/apps/authoring/authoring/services/AuthoringService.ts
@@ -630,7 +630,9 @@ export function AuthoringService($q, $location, api, lock, autosave, confirm, pr
         let userPrivileges = privileges.privileges;
 
         function canRewrite() {
-            if (currentItem.state === ITEM_STATE.SCHEDULED && appConfig.allow_updating_scheduled_items === true) {
+            if (currentItem.state === ITEM_STATE.SCHEDULED
+                && appConfig.allow_updating_scheduled_items
+                && !currentItem.rewritten_by) {
                 return true;
             }
             return !isReadOnlyState && currentItem.type === 'text'
