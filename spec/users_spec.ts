4443cd8c8c159c2e06541736da5f3e47353a34e0
Tomas <t.kikutis@gmail.com>
2020-03-27 10:23:04 +0100

    Revert "add a test for creating a new user (#3395)"
    This reverts commit 91dfc62140f7caae179c629faa5efdfcf4bdfcf6.

--- spec/users_spec.ts
+++ spec/users_spec.ts
@@ -65,28 +65,6 @@ describe('users', () => {
             nav('/users');
         });
 
-        it('can create a new user', () => {
-            el(['create-user-button']).click();
-
-            el(['user-details-form', 'field--first_name']).sendKeys('John');
-            el(['user-details-form', 'field--last_name']).sendKeys('Doe');
-            el(['user-details-form', 'field--username']).sendKeys('johndoe');
-            el(['user-details-form', 'field--email']).sendKeys('johndoe@example.com');
-
-            el(['user-details-form', 'save']).click();
-
-            browser.wait(
-                ECE.textToBePresentInElement(
-                    el(
-                        ['username'],
-                        null,
-                        els(['users-list-item']).get(0),
-                    ),
-                    'johndoe',
-                ),
-            );
-        });
-
         it('can list users', () => {
             expect(
                 element.all(by.css('[data-test-id="users-list-item"]'))
