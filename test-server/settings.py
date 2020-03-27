88596f839bd959140a4ce5699c24cc298b03a729
Petr Jašek <jasekpetr@gmail.com>
2020-03-27 14:48:22 +0100

    run e2e tests on travis (#3367)
    * run e2e tests on travis
    
    with test server on docker
    
    * test single server worker
    
    * use 2 workers again
    
    * use bionic

--- test-server/settings.py
+++ test-server/settings.py
@@ -6,26 +6,15 @@ SERVER_NAME = 'localhost:5000'
 URL_PROTOCOL = 'http'
 URL_PREFIX = 'api'
 
-CLIENT_URL = 'http://localhost:9090'
-REDIS_URL = 'redis://localhost:6379/2'
+CLIENT_URL = 'http://localhost:9000'
 
 MONGO_DBNAME = 'superdesk_e2e'
-MONGO_URI = 'mongodb://localhost/%s' % MONGO_DBNAME
-
 ARCHIVED_DBNAME = 'superdesk_e2e_archived'
-ARCHIVED_URI = 'mongodb://localhost/%s' % ARCHIVED_DBNAME
-
 LEGAL_ARCHIVE_DBNAME = 'superdesk_e2e_legal_archive'
-LEGAL_ARCHIVE_URI = 'mongodb://localhost/%s' % LEGAL_ARCHIVE_DBNAME
-
 CONTENTAPI_MONGO_DBNAME = 'contentapi_e2e'
-CONTENTAPI_MONGO_URI = 'mongodb://localhost/%s' % CONTENTAPI_MONGO_DBNAME
 
 ELASTICSEARCH_INDEX = MONGO_DBNAME
-ELASTICSEARCH_BACKUPS_PATH = '/tmp/es-backups/'
-
 CONTENTAPI_ELASTICSEARCH_INDEX = CONTENTAPI_MONGO_DBNAME
 
 LEGAL_ARCHIVE = True
-
 CELERY_TASK_ALWAYS_EAGER = True
