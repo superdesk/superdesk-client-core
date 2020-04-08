16930354f8539ceec76f99e37a84d23b958e6034
Pablo P Varela <pablovarela182@gmail.com>
2020-04-08 11:54:00 +0200

    Translate default stages (#3417)
    * Translate default desk stages
    
    * gettext extract
    
    * Translate cards also
    
    * gettext extract
    
    * lint
    
    * lint again
    
    * Fix e2e tests
    
    * . -> -

--- scripts/apps/desks/controllers/DeskSettingsController.ts
+++ scripts/apps/desks/controllers/DeskSettingsController.ts
@@ -1,7 +1,10 @@
+import {getLabelForStage} from 'apps/workspace/content/constants';
+
 DeskSettingsController.$inject = ['$scope', 'desks'];
 export function DeskSettingsController($scope, desks) {
     desks.initialize()
         .then(() => {
             $scope.desks = desks.desks;
+            $scope.labelForStage = getLabelForStage;
         });
 }
