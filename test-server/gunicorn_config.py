4b3eafa4510da2efc6ec86078101aea92d8e024c
Petr Jasek <jasekpetr@gmail.com>
2020-04-15 17:35:33 +0200

    use 2 backend workers

--- test-server/gunicorn_config.py
+++ test-server/gunicorn_config.py
@@ -4,10 +4,10 @@ reload = bool(os.environ.get('WEB_RELOAD'))
 
 bind = '0.0.0.0:%s' % os.environ.get('PORT', '5000')
 
-workers = int(os.environ.get('WEB_WORKERS', 1))
+workers = int(os.environ.get('WEB_WORKERS', 2))
 timeout = int(os.environ.get('WEB_TIMEOUT', 10))
 
-loglevel = os.environ.get('WEB_LOG_LEVEL', 'warning')
+loglevel = os.environ.get('WEB_LOGLEVEL', 'warning')
 
 accesslog = '-'
 access_log_format = '%(m)s %(U)s status=%(s)s time=%(T)ss size=%(B)sb'
