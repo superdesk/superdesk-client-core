7784c178e77439b9e56bc15cf36173efe5182244
Petr <petr.jasek@sourcefabric.org>
2020-04-15 15:02:25 +0200

    save

--- test-server/gunicorn_config.py
+++ test-server/gunicorn_config.py
@@ -1,8 +1,14 @@
 import os
 
+reload = bool(os.environ.get('WEB_RELOAD'))
+
 bind = '0.0.0.0:%s' % os.environ.get('PORT', '5000')
 
-workers = int(os.environ.get('WEB_WORKERS', 2))
+workers = int(os.environ.get('WEB_WORKERS', 1))
 timeout = int(os.environ.get('WEB_TIMEOUT', 10))
 
 loglevel = os.environ.get('WEB_LOG_LEVEL', 'warning')
+
+accesslog = '-'
+access_log_format = '%(m)s %(U)s status=%(s)s time=%(T)ss size=%(B)sb'
+
--- test-server/settings.py
+++ test-server/settings.py
@@ -1,5 +1,7 @@
 
-DEBUG = False
+import os
+
+DEBUG = True
 SUPERDESK_TESTING = True
 
 SERVER_NAME = 'localhost:5000'
@@ -9,9 +11,16 @@ URL_PREFIX = 'api'
 CLIENT_URL = 'http://localhost:9000'
 
 MONGO_DBNAME = 'superdesk_e2e'
-ARCHIVED_DBNAME = 'superdesk_e2e_archived'
+MONGO_URI = os.environ.get('MONGO_URI', 'mongodb://localhost/{}'.format(MONGO_DBNAME))
+
 LEGAL_ARCHIVE_DBNAME = 'superdesk_e2e_legal_archive'
+LEGAL_ARCHIVE_URI = os.environ.get('LEGAL_ARCHIVE_URI', 'mongodb://localhost/{}'.format(LEGAL_ARCHIVE_DBNAME))
+
 CONTENTAPI_MONGO_DBNAME = 'contentapi_e2e'
+CONTENTAPI_MONGO_URI = os.environ.get('CONTENTAPI_MONGO_URI', 'mongodb://localhost/{}'.format(CONTENTAPI_MONGO_DBNAME))
+
+ARCHIVED_DBNAME = 'superdesk_e2e_archived'
+ARCHIVED_URI = os.environ.get('ARCHIVED_URI', 'mongodb://localhost/{}'.format(ARCHIVED_DBNAME))
 
 ELASTICSEARCH_INDEX = MONGO_DBNAME
 CONTENTAPI_ELASTICSEARCH_INDEX = CONTENTAPI_MONGO_DBNAME
