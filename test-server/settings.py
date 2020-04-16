93ea8df7ffa7fd6a8b5ad21954b657c80e0c30fe
Petr Jasek <jasekpetr@gmail.com>
2020-04-16 08:24:13 +0200

    sync mongo uris with dbnames

--- test-server/settings.py
+++ test-server/settings.py
@@ -10,16 +10,16 @@ URL_PREFIX = 'api'
 
 CLIENT_URL = 'http://localhost:9000'
 
-MONGO_DBNAME = 'superdesk_e2e'
+MONGO_DBNAME = os.environ.get('MONGO_DBNAME', 'superdesk_e2e')
 MONGO_URI = os.environ.get('MONGO_URI', 'mongodb://localhost/{}'.format(MONGO_DBNAME))
 
-LEGAL_ARCHIVE_DBNAME = 'superdesk_e2e_legal_archive'
+LEGAL_ARCHIVE_DBNAME = os.environ.get('LEGAL_ARCHIVE_DBNAME', 'superdesk_e2e_legal_archive')
 LEGAL_ARCHIVE_URI = os.environ.get('LEGAL_ARCHIVE_URI', 'mongodb://localhost/{}'.format(LEGAL_ARCHIVE_DBNAME))
 
-CONTENTAPI_MONGO_DBNAME = 'contentapi_e2e'
+CONTENTAPI_MONGO_DBNAME = os.environ.get('CONTENTAPI_MONGO_DBNAME', 'contentapi_e2e')
 CONTENTAPI_MONGO_URI = os.environ.get('CONTENTAPI_MONGO_URI', 'mongodb://localhost/{}'.format(CONTENTAPI_MONGO_DBNAME))
 
-ARCHIVED_DBNAME = 'superdesk_e2e_archived'
+ARCHIVED_DBNAME = os.environ.get('ARCHIVED_DBNAME', 'superdesk_e2e_archived')
 ARCHIVED_URI = os.environ.get('ARCHIVED_URI', 'mongodb://localhost/{}'.format(ARCHIVED_DBNAME))
 
 ELASTICSEARCH_INDEX = MONGO_DBNAME
