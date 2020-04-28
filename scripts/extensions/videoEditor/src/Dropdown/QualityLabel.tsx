d21070efbc149f0fa9548b6816d06a9447d5fada
Loi Tran <loi.tran@idsolutions.com.vn>
2020-04-28 16:24:57 +0700

    Fix hidden quality warning tooltip text

--- scripts/extensions/videoEditor/src/Dropdown/QualityLabel.tsx
+++ scripts/extensions/videoEditor/src/Dropdown/QualityLabel.tsx
@@ -4,10 +4,11 @@ import {IDropdownLabel} from '../interfaces';
 export class QualityLabel extends React.PureComponent<IDropdownLabel> {
     render() {
         const value = this.props.selectedItem?.value ? this.props.selectedItem?.label : 'Same';
+        const {getClass} = this.props;
 
         return (
             <button
-                className="dropdown__toggle dark-ui dropdown-toggle"
+                className={`dropdown__toggle dark-ui dropdown-toggle  ${getClass?.('dropdown__caret')}`}
                 aria-haspopup="true"
                 aria-expanded="false"
                 onClick={this.props.onClick}
