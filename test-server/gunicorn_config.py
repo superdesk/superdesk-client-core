72d343d50961ab268c262571f71b0a78b8704110
Petr Jasek <jasekpetr@gmail.com>
2020-04-29 20:25:16 +0200

    fix e2e tests setup
    SDESK-4968

--- test-server/gunicorn_config.py
+++ test-server/gunicorn_config.py
@@ -6,3 +6,8 @@ workers = int(os.environ.get('WEB_WORKERS', 2))
 timeout = int(os.environ.get('WEB_TIMEOUT', 10))
 
 loglevel = os.environ.get('WEB_LOG_LEVEL', 'warning')
+
+if bool(os.environ.get('SUPERDESK_DEBUG')):
+    accesslog = '-'
+    access_log_format = '%(m)s %(U)s status=%(s)s time=%(T)ss size=%(B)sb'
+
