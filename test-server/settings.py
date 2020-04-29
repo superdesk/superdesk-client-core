664d0dc1526f3ab697c9d75856c70d6ca5fa9df1
Petr Jasek <jasekpetr@gmail.com>
2020-04-29 20:25:16 +0200

    fix test server settings

--- test-server/settings.py
+++ test-server/settings.py
@@ -1,4 +1,6 @@
 
+import os
+
 DEBUG = False
 SUPERDESK_TESTING = True
 
@@ -9,9 +11,15 @@ URL_PREFIX = 'api'
 CLIENT_URL = 'http://localhost:9000'
 
 MONGO_DBNAME = 'superdesk_e2e'
+MONGO_URI = os.environ.get('MONGO_URI', 'mongodb://localhost/%s' % MONGO_DBNAME)
 ARCHIVED_DBNAME = 'superdesk_e2e_archived'
+ARCHIVED_URI = os.environ.get('ARCHIVED_URI', 'mongodb://localhost/%s' % ARCHIVED_DBNAME)
 LEGAL_ARCHIVE_DBNAME = 'superdesk_e2e_legal_archive'
+LEGAL_ARCHIVED_URI = os.environ.get('LEGAL_ARCHIVED_URI', 'mongodb://localhost/%s' % LEGAL_ARCHIVE_DBNAME)
 CONTENTAPI_MONGO_DBNAME = 'contentapi_e2e'
+CONTENTAPI_MONGO_URI = os.environ.get('CONTENTAPI_MONGO_URI', 'mongodb://localhost/%s' % CONTENTAPI_MONGO_DBNAME)
+PUBLICAPI_MONGO_DBNAME = 'publicapi_e2e'
+PUBLICAPI_MONGO_URI = os.environ.get('PUBLICAPI_MONGO_URI', 'mongodb://localhost/%s' % PUBLICAPI_MONGO_DBNAME)
 
 ELASTICSEARCH_INDEX = MONGO_DBNAME
 CONTENTAPI_ELASTICSEARCH_INDEX = CONTENTAPI_MONGO_DBNAME
