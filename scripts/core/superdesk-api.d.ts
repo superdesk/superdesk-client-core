e57a6f907642cb6a1b460f8f89cfb0ca112e875c
GyanP <gyanig72@gmail.com>
2020-06-17 15:17:33 +0530

    Send to Panel changes. [SDANSA-388] (#3484)
    * Send to Panel changes. [SDANSA-388]
    
    * Address comments.
    
    * Address comments.
    
    * Rename 'defaultStage' as 'sendDefaultStage'.
    
    * Checking for working and incoming.

--- scripts/core/superdesk-api.d.ts
+++ scripts/core/superdesk-api.d.ts
@@ -1192,6 +1192,9 @@ declare module 'superdesk-api' {
             publishEmbargo?: any;
             sendAndPublish?: any;
             italicAbstract?: any;
+            sendPublishSchedule?: boolean;
+            sendEmbargo?: boolean;
+            sendDefaultStage?: 'working' | 'incoming';
         };
         list: {
             narrowView: any;
