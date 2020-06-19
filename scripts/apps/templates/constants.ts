66979f1e66e6d2d4feae2005fefd2fe6889ccbc4
GyanP <gyanig72@gmail.com>
2020-06-19 13:24:55 +0530

    Console errors for 'invalid translations'. [SDESK-5319] (#3502)

--- scripts/apps/templates/constants.ts
+++ scripts/apps/templates/constants.ts
@@ -3,8 +3,11 @@ import {gettext} from 'core/utils';
 /*
 	Labels and values for the template filter dropdown
 */
-export const TEMPLATEFILTERS = {All: {label: gettext('All'), value: 'All'},
-    Private: {label: gettext('Private'), value: 'Private'},
-    Personal: {label: gettext('Personal'), value: 'Personal'},
-    NoDesk: {label: gettext('No Desk'), value: 'None'},
-};
+export function getTemplateFilters() {
+    return Object.freeze({
+        All: {label: gettext('All'), value: 'All'},
+        Private: {label: gettext('Private'), value: 'Private'},
+        Personal: {label: gettext('Personal'), value: 'Personal'},
+        NoDesk: {label: gettext('No Desk'), value: 'None'},
+    });
+}
