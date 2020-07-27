85ce97d1b3d24e8f4d34ed8784e6c92433650c6c
Tomas <t.kikutis@gmail.com>
2020-07-27 13:33:25 +0200

    extension point for sticky toolbar

--- scripts/apps/authoring/authoring/index.ts
+++ scripts/apps/authoring/authoring/index.ts
@@ -23,6 +23,7 @@ import {AuthoringTopbarReact} from './authoring-topbar-react';
 import {AuthoringWorkspaceService} from './services';
 import {sdStaticAutocompleteDirective} from './directives/sd-static-autocomplete';
 import {VideoThumbnailEditor} from './components/video-thumbnail-editor';
+import {AuthoringTopbar2React} from './authoring-topbar2-react';
 
 export interface IOnChangeParams {
     item: IArticle;
@@ -99,6 +100,7 @@ angular.module('superdesk.apps.authoring', [
     .directive('sdAuthoring', directive.AuthoringDirective)
     .directive('sdAuthoringTopbar', directive.AuthoringTopbarDirective)
     .component('sdAuthoringTopbarReact', reactToAngular1(AuthoringTopbarReact, ['article', 'action', 'onChange']))
+    .component('sdAuthoringTopbar2React', reactToAngular1(AuthoringTopbar2React, ['article', 'action', 'onChange']))
     .component('sdVideoThumbnailEditor', reactToAngular1(VideoThumbnailEditor, ['item', 'onChange']))
     .directive('sdPreviewFormatted', directive.PreviewFormattedDirective)
     .directive('sdAuthoringContainer', directive.AuthoringContainerDirective)
--- scripts/core/superdesk-api.d.ts
+++ scripts/core/superdesk-api.d.ts
@@ -67,6 +67,7 @@ declare module 'superdesk-api' {
             articleListItemWidgets?: Array<React.ComponentType<{article: IArticle}>>;
             articleGridItemWidgets?: Array<React.ComponentType<{article: IArticle}>>;
             authoringTopbarWidgets?: Array<React.ComponentType<{article: IArticle}>>;
+            authoringTopbar2Widgets?: Array<React.ComponentType<{article: IArticle}>>;
             pages?: Array<IPage>;
             customFieldTypes?: Array<ICustomFieldType>;
             notifications?: {
