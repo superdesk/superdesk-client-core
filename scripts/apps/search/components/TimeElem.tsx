b45c45d7d4dac342fc42cb5ff0710390b0be50f7
Tomas <t.kikutis@gmail.com>
2020-07-29 16:34:26 +0200

    drop key prop for TimeElem

--- scripts/apps/search/components/TimeElem.tsx
+++ scripts/apps/search/components/TimeElem.tsx
@@ -3,7 +3,6 @@ import ng from 'core/services/ng';
 
 interface IProps {
     date: any;
-    key?: string;
 }
 
 export class TimeElem extends React.Component<IProps> {
@@ -15,10 +14,10 @@ export class TimeElem extends React.Component<IProps> {
         this.datetime = ng.get('datetime');
     }
     render() {
-        const {date, key} = this.props;
+        const {date} = this.props;
 
         return (
-            <time key={key} title={this.datetime.longFormat(date)}>{this.datetime.shortFormat(date)}</time>
+            <time title={this.datetime.longFormat(date)}>{this.datetime.shortFormat(date)}</time>
         );
     }
 }
