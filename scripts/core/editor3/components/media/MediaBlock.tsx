286bb54e5867f6cf292030b26a8bf3ad1732b844
Pablo P Varela <pablovarela182@gmail.com>
2020-07-30 10:03:59 +0200

    Make mediablock usageterms consistent with galleries (#3549)
    SDESK-5423
    
    Usageterms were only being shown on a media block for "single selection"
    fields (so only these values: 'Single Usage', 'Time Restricted' and
    'Indefinite Usage'). When the field is a simple input, other places like
    galleries and feature media shows the actual string value, while the
    media block just shows "[No value]".

--- scripts/core/editor3/components/media/MediaBlock.tsx
+++ scripts/core/editor3/components/media/MediaBlock.tsx
@@ -14,8 +14,10 @@ function getTranslationForAssignRights(value) {
         return gettext('Time Restricted');
     } else if (value === 'indefinite-usage') {
         return gettext('Indefinite Usage');
+    } else if (typeof value === 'string') {
+        return value;
     } else {
-        return '';
+        return null;
     }
 }
 
