35b6c3152cfceec168a39310030085a4e973e1c4
Tomas Kikutis <t.kikutis@gmail.com>
2020-08-05 09:49:31 +0200

    fix page retrieval (#3554)

--- scripts/core/extension-page.tsx
+++ scripts/core/extension-page.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import {extensions} from 'appConfig';
 import {logger} from './services/logger';
+import {flatMap} from 'lodash';
 
 export class ExtensionPage extends React.Component {
     render() {
         const currentUrl = window.location.hash.slice(1);
-        const extensionObject = Object.values(extensions).find(
-            ({activationResult}) =>
-                activationResult.contributions != null
-                && activationResult.contributions.pages.find(({url}) => url === currentUrl) != null,
+
+        const pages = flatMap(
+            Object.values(extensions),
+            (extension) => extension.activationResult?.contributions?.pages ?? [],
         );
 
-        if (extensionObject != null) {
-            const Component = extensionObject.activationResult.contributions.pages.find(
-                ({url}) => url === currentUrl,
-            ).component;
+        const currentPage = pages.find(({url}) => url === currentUrl);
+
+        if (currentPage != null) {
+            const Component = currentPage.component;
 
             return <Component />;
         } else {
