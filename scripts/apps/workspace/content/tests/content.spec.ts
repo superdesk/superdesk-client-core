9df958eaee589f945347f255d47d6e4fa5a0a5c5
pablopunk <yo@pablo.pink>
2020-09-01 09:46:11 +0200

    fix test

--- scripts/apps/workspace/content/tests/content.spec.ts
+++ scripts/apps/workspace/content/tests/content.spec.ts
@@ -35,7 +35,7 @@ describe('superdesk.apps.workspace.content', () => {
             $rootScope.$digest();
             expect(api.save).toHaveBeenCalledWith('archive', {headline: '', slugline: '',
                 description_text: '', type: 'composite',
-                groups: [{role: 'grpRole:NEP', refs: [{idRef: 'main'}], id: 'root'},
+                groups: [{role: 'grpRole:NEP', refs: [{idRef: 'main', label: 'main'}], id: 'root'},
                     {refs: [], id: 'main', role: 'grpRole:main'}], version: 0,
                 task: {desk: '1', stage: '2', user: '1'}});
             expect(done).toHaveBeenCalledWith(ITEM);
@@ -58,7 +58,7 @@ describe('superdesk.apps.workspace.content', () => {
                 version: 0,
                 task: {desk: '1', stage: '2', user: '1'},
                 groups: [
-                    {role: 'grpRole:NEP', refs: [{idRef: 'main'}], id: 'root'},
+                    {role: 'grpRole:NEP', refs: [{idRef: 'main', label: 'main'}], id: 'root'},
                     {refs: [{headline: '', residRef: undefined, location: 'archive',
                         slugline: '', renditions: {}, itemClass: '', type: ''}],
                     id: 'main', role: 'grpRole:main'}]});
