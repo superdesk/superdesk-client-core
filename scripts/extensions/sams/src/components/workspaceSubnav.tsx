c077686fcfe71b3533da2941686acd350d0d7c16
Mark Pittaway <mark.pittaway@gmail.com>
2020-09-09 14:32:02 +1000

    New workspaceSubnav component

---
+++ scripts/extensions/sams/src/components/workspaceSubnav.tsx
@@ -0,0 +1,271 @@
+// External Modules
+import * as React from 'react';
+
+// Types
+import {ISuperdesk} from 'superdesk-api';
+import {
+    ASSET_LIST_STYLE,
+    ASSET_SORT_FIELD,
+    IAssetSearchParams,
+    ISetItem,
+    LIST_ACTION,
+    SET_STATE,
+    SORT_ORDER,
+} from '../interfaces';
+
+// UI
+import {
+    Badge,
+    Button,
+    ButtonGroup,
+    Dropdown,
+    NavButton,
+    SubNav,
+    IconButton,
+} from 'superdesk-ui-framework/react';
+import {IMenuGroup, IMenuItem} from 'superdesk-ui-framework/react/components/Dropdown';
+import {ContentBar, SearchBar, SubNavSpacer} from '../ui';
+import {getShowManageSetsModalFunction} from './sets/manageSetsModal';
+import {getShowUploadAssetModalFunction} from './assets/uploadAssetModal';
+import {getAssetTypeFilterButtons} from './assets/assetTypeFilterButtons';
+
+// Utils
+import {getAssetListSortFieldText} from '../utils/ui';
+
+interface IProps {
+    filterPanelOpen: boolean;
+    totalAssets: number;
+    listStyle: ASSET_LIST_STYLE;
+    searchParams: IAssetSearchParams;
+    activeSets: Array<ISetItem>;
+    disabledSets: Array<ISetItem>;
+    currentSet?: ISetItem;
+    toggleFilterPanel(): void;
+    toggleListStyle(): void;
+    updateAssetSearchParamsAndListItems(
+        params: Partial<IAssetSearchParams>,
+        listAction: LIST_ACTION,
+    ): void;
+}
+
+export function getWorkspaceSubnavComponent(superdesk: ISuperdesk) {
+    const {gettext} = superdesk.localization;
+    const {numberToString} = superdesk.helpers;
+
+    const showManageSetsModal = getShowManageSetsModalFunction(superdesk);
+    const showUploadAssetModal = getShowUploadAssetModalFunction(superdesk);
+
+    const AssetTypeFilterButtons = getAssetTypeFilterButtons(superdesk);
+
+    return class WorkspaceSubnav extends React.PureComponent<IProps> {
+        subNavMenuActions: Array<IMenuGroup>;
+        sortFieldOptions: Array<IMenuItem>;
+
+        constructor(props: IProps) {
+            super(props);
+
+            this.subNavMenuActions = this.getSubNavMenuActions();
+            this.sortFieldOptions = this.getSortFieldOptions();
+
+            this.toggleSortOrder = this.toggleSortOrder.bind(this);
+            this.setSearchParamText = this.setSearchParamText.bind(this);
+        }
+
+        getSubNavMenuActions(): Array<IMenuGroup> {
+            const actions: Array<any> = [];
+
+            if (superdesk.privileges.hasPrivilege('sams_manage')) {
+                actions.push({
+                    label: gettext('Manage Sets'),
+                    icon: 'folder-open',
+                    onSelect: showManageSetsModal,
+                });
+            }
+
+            return actions.length === 0 ?
+                [] :
+                [{
+                    type: 'group',
+                    label: gettext('Actions'),
+                    items: [
+                        'divider',
+                        ...actions,
+                    ],
+                }];
+        }
+
+        getSortFieldOptions(): Array<IMenuItem> {
+            return [{
+                label: gettext('Name'),
+                onSelect: () => this.setSortField(ASSET_SORT_FIELD.NAME),
+            }, {
+                label: gettext('Filename'),
+                onSelect: () => this.setSortField(ASSET_SORT_FIELD.FILENAME),
+            }, {
+                label: gettext('Created'),
+                onSelect: () => this.setSortField(ASSET_SORT_FIELD.CREATED),
+            }, {
+                label: gettext('Updated'),
+                onSelect: () => this.setSortField(ASSET_SORT_FIELD.UPDATED),
+            }, {
+                label: gettext('Size'),
+                onSelect: () => this.setSortField(ASSET_SORT_FIELD.SIZE),
+            }];
+        }
+
+        getMenuItems(): Array<IMenuGroup> {
+            const activeSets = this.props.activeSets.map(
+                (set) => ({
+                    label: set.name,
+                    onSelect: () => this.setSearchParamSetId(set._id),
+                }),
+            );
+
+            const disabledSets = this.props.disabledSets.map(
+                (set) => ({
+                    label: set.name + ' ' + gettext('(disabled)'),
+                    icon: 'lock',
+                    onSelect: () => this.setSearchParamSetId(set._id),
+                }),
+            );
+
+            return [{
+                type: 'group',
+                label: gettext('Sets'),
+                items: [
+                    'divider',
+                    {
+                        label: gettext('All Sets'),
+                        onSelect: () => this.setSearchParamSetId(),
+                    },
+                    ...activeSets,
+                    ...disabledSets,
+                ],
+            }];
+        }
+
+        toggleSortOrder() {
+            const sortOrder = this.props.searchParams.sort_order === SORT_ORDER.ASCENDING ?
+                SORT_ORDER.DESCENDING :
+                SORT_ORDER.ASCENDING;
+
+            this.props.updateAssetSearchParamsAndListItems(
+                {sort_order: sortOrder},
+                LIST_ACTION.REPLACE,
+            );
+        }
+
+        setSortField(field: ASSET_SORT_FIELD) {
+            this.props.updateAssetSearchParamsAndListItems(
+                {sort_field: field},
+                LIST_ACTION.REPLACE,
+            );
+        }
+
+        setSearchParamSetId(setId?: string) {
+            this.props.updateAssetSearchParamsAndListItems(
+                {setId: setId},
+                LIST_ACTION.REPLACE,
+            );
+        }
+
+        setSearchParamText(searchText?: string) {
+            this.props.updateAssetSearchParamsAndListItems(
+                {query_string: searchText},
+                LIST_ACTION.REPLACE,
+            );
+        }
+
+        render() {
+            const items = this.getMenuItems();
+            const buttonLabel = this.props.currentSet?.name ?? gettext('All Sets');
+            const buttonIcon = this.props.currentSet?.state === SET_STATE.DISABLED ? 'lock' : undefined;
+            const sortFieldText = getAssetListSortFieldText(
+                superdesk,
+                this.props.searchParams.sort_field,
+            );
+
+            return (
+                <React.Fragment>
+                    <SubNav zIndex={2}>
+                        <ButtonGroup align="inline">
+                            <Dropdown items={items}>
+                                <NavButton
+                                    text={buttonLabel}
+                                    onClick={() => false}
+                                    icon={buttonIcon}
+                                />
+                            </Dropdown>
+                        </ButtonGroup>
+                        <SearchBar
+                            placeholder={gettext('Search Assets')}
+                            type="expanded"
+                            focused={true}
+                            onSubmit={this.setSearchParamText}
+                            initialValue={this.props.searchParams.query_string}
+                        />
+                        <ButtonGroup align="right">
+                            {this.subNavMenuActions.length === 0 ?
+                                null :
+                                (
+                                    <Dropdown items={this.subNavMenuActions}>
+                                        <button className="sd-navbtn">
+                                            <i className="icon-dots-vertical"/>
+                                        </button>
+                                    </Dropdown>
+                                )
+                            }
+                            <Button
+                                type="primary"
+                                icon="upload"
+                                text="plus-large"
+                                shape="round"
+                                iconOnly={true}
+                                onClick={showUploadAssetModal}
+                            />
+                        </ButtonGroup>
+                    </SubNav>
+                    <SubNav zIndex={1}>
+                        <ButtonGroup align="inline">
+                            <NavButton
+                                icon="filter-large"
+                                onClick={this.props.toggleFilterPanel}
+                                type={this.props.filterPanelOpen === true ?
+                                    'primary' :
+                                    'default'
+                                }
+                            />
+                        </ButtonGroup>
+                        <AssetTypeFilterButtons />
+                        <ButtonGroup align="right">
+                            <SubNavSpacer noMargin={true}/>
+                            <ContentBar>
+                                <span className="sd-margin-r--1">
+                                    <span className="sd-margin-r--1">
+                                        {gettext('Total:')}
+                                    </span>
+                                    <Badge text={numberToString(this.props.totalAssets)} />
+                                </span>
+                                <Dropdown items={this.sortFieldOptions}>
+                                    {sortFieldText}
+                                </Dropdown>
+                                <IconButton
+                                    ariaValue={this.props.searchParams.sort_order}
+                                    onClick={this.toggleSortOrder}
+                                    icon={this.props.searchParams.sort_order}
+                                />
+                            </ContentBar>
+                            <NavButton
+                                icon={this.props.listStyle === ASSET_LIST_STYLE.GRID ?
+                                    'list-view' :
+                                    'grid-view'
+                                }
+                                onClick={this.props.toggleListStyle}
+                            />
+                        </ButtonGroup>
+                    </SubNav>
+                </React.Fragment>
+            );
+        }
+    };
+}
