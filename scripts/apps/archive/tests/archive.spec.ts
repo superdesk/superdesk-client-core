1b92b333d600c9998facdf309a18d84029a6bcd5
Tomas <t.kikutis@gmail.com>
2020-09-14 10:40:50 +0200

    missing arguments

--- scripts/apps/archive/tests/archive.spec.ts
+++ scripts/apps/archive/tests/archive.spec.ts
@@ -60,6 +60,7 @@ describe('content', () => {
             authoringWorkspace: AuthoringWorkspaceService,
             config,
             metadata,
+            preferencesService,
         ) => {
             const extensionDelay = 200;
 
@@ -99,6 +100,7 @@ describe('content', () => {
                 authoringWorkspace,
                 config,
                 metadata,
+                preferencesService,
             ).then(() => {
                 activityService.start(superdesk.activities.spike, {data: {item: {_id: '0'}}});
 
