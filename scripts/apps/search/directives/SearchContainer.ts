7ca20a69cc91d2a9aeb8c1ebe99f8adda931e2ba
olegpshenichniy <olegpshenichniy@gmail.com>
2020-10-16 16:25:09 +0200

    SDBELGA-423 Store search panel show/hide state in the user preferences.

--- scripts/apps/search/directives/SearchContainer.ts
+++ scripts/apps/search/directives/SearchContainer.ts
@@ -1,16 +1,24 @@
 import {isEmpty, omit} from 'lodash';
 import {gettext} from 'core/utils';
 
+
 export function SearchContainer() {
+    const FILTERS_PANEL_PREFERENCES_KEY = 'search:filters_panel';
+    var filtersPanelPreferences = {
+        [FILTERS_PANEL_PREFERENCES_KEY]: {
+            'type': 'bool',
+            'default': true
+        }
+    }
+
     return {
-        controller: ['$scope', '$location', 'pageTitle',
-            function SearchContainerController($scope, $location, pageTitle) {
+        controller: ['$scope', '$location', 'pageTitle', 'preferencesService',
+            function SearchContainerController($scope, $location, pageTitle, preferencesService) {
                 const query = omit($location.search(), '_id', 'repo');
 
                 this.flags = $scope.flags || {};
                 this.flags.facets = !isEmpty(query);
                 pageTitle.setUrl(gettext('Search'));
-
                 $scope.$watch('search.activeProvider', (activeProvider) => {
                     if (activeProvider) {
                         if (activeProvider.advanced_search !== undefined) {
@@ -28,6 +36,18 @@ export function SearchContainer() {
                         }
                     }
                 });
+                preferencesService.get().then((result) => {
+                    if (result !== undefined && FILTERS_PANEL_PREFERENCES_KEY in result) {
+                        this.flags.facets = result[FILTERS_PANEL_PREFERENCES_KEY]['enabled'];
+                    } else {
+                        this.flags.facets = filtersPanelPreferences[FILTERS_PANEL_PREFERENCES_KEY]['default'];
+                    }
+                });
+                $scope.toggleFiltersPane = () => {
+                    filtersPanelPreferences[FILTERS_PANEL_PREFERENCES_KEY]['enabled'] = !$scope.flags.facets;
+                    preferencesService.update(filtersPanelPreferences);
+                    $scope.flags.facets = !$scope.flags.facets;
+                };
             }],
     };
 }
