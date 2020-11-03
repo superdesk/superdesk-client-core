853c32fdb389c945780dcfb7a0cb1f2efbc5ada3
Tomas <t.kikutis@gmail.com>
2020-11-03 12:52:22 +0100

    convert to switch

--- scripts/apps/authoring/preview/getAuthoringField.ts
+++ scripts/apps/authoring/preview/getAuthoringField.ts
@@ -10,152 +10,179 @@ export function getAuthoringField(
 ): IAuthoringField {
     const customField = customFieldsVocabularies.find(({_id}) => _id === fieldId);
 
-    if (customField?.field_type === 'text') {
+    switch (customField?.field_type) {
+    case 'text':
         return {
             type: 'html',
             id: fieldId,
             value: item.extra?.[fieldId],
         };
-    } else if (customField?.field_type === 'date') {
+
+    case 'date':
         return {
             type: 'plain-text',
             id: fieldId,
             value: item.extra?.[fieldId] == null ? null : formatDate(new Date(item.extra[fieldId])),
         };
-    } else if (customField?.field_type === 'embed') {
+
+    case 'embed':
         return {
             type: 'embed',
             id: fieldId,
             value: item.extra?.[fieldId],
         };
-    } else if (customField?.field_type === 'urls') {
+
+    case 'urls':
         return {
             type: 'urls',
             id: fieldId,
             value: item.extra?.[fieldId],
         };
-    } else if (customField?.field_type === 'related_content') {
+
+    case 'related_content':
         return {
             type: 'related-articles',
             id: fieldId,
             value: getRelatedArticles(item.associations, customField._id),
         };
-    } else if (customField?.field_type === 'media') {
+
+    case 'media':
         return {
             type: 'media-gallery',
             id: fieldId,
             value: getRelatedMedia(item.associations, customField._id),
         };
-    } else if (customField?.custom_field_type != null) {
-        return {
-            type: 'custom',
-            id: fieldId,
-            value: {item: item, field: customField},
-        };
-    } else if (fieldId === 'feature_media') {
-        return {
-            type: 'media-gallery',
-            id: fieldId,
-            value: item.associations.featuremedia == null ? [] : [item.associations.featuremedia],
-        };
-    } else if (['abstract', 'body_html', 'body_footer'].includes(fieldId)) {
-        return {
-            type: 'html',
-            id: fieldId,
-            value: item[fieldId],
-        };
-    } else if (['anpa_category', 'genre'].includes(fieldId)) {
-        return {
-            type: 'subjects',
-            id: fieldId,
-            value: item[fieldId],
-        };
-    } else if (fieldId === 'place') {
-        return {
-            type: 'subjects',
-            id: fieldId,
-            value: item[fieldId]?.map(({name, code}) => ({name, qcode: code})),
-        };
-    } else if (fieldId === 'authors') {
-        return {
-            type: 'subjects',
-            id: fieldId,
-            value: item[fieldId]?.map(({_id, name}) => ({name, qcode: _id})),
-        };
-    } else if (fieldId === 'keywords') {
-        return {
-            type: 'subjects',
-            id: fieldId,
-            value: item[fieldId]?.map((keyword) => ({name: keyword, qcode: keyword})),
-        };
-    } else if ([
-        'products',
-        'languages',
-        'PhotoCategories',
-        'destination',
-        'company_codes',
-    ].includes(fieldId)) {
-        return {
-            type: 'subjects',
-            id: fieldId,
-            value: item['subject']?.filter(({scheme}) => scheme === fieldId),
-        };
-    } else if (fieldId === 'subject') {
-        return {
-            type: 'subjects',
-            id: fieldId,
-            value: item['subject']?.filter(({scheme}) => scheme == null),
-        };
-    } else if ([
-        'slugline',
-        'anpa_take_key',
-        'byline',
-        'ednote',
-        'headline',
-        'sign_off',
-        'usageterms',
-        'description_text',
-    ].includes(fieldId)) {
-        return {
-            type: 'plain-text',
-            id: fieldId,
-            value: item[fieldId],
-        };
-    } else if (fieldId === 'sms') {
-        return {
-            type: 'plain-text',
-            id: fieldId,
-            value: item.sms_message,
-        };
-    } else if (fieldId === 'dateline') {
-        return {
-            type: 'plain-text',
-            id: fieldId,
-            value: item.dateline?.text,
-        };
-    } else if (fieldId === 'priority' || fieldId === 'urgency') {
-        return {
-            type: 'plain-text',
-            id: fieldId,
-            value: item[fieldId]?.toString(),
-        };
-    } else if (fieldId === 'attachments') {
-        return {
-            type: 'attachments',
-            id: fieldId,
-            value: item[fieldId],
-        };
-    } else if (fieldId === 'language') {
-        return {
-            type: 'vocabulary-values',
-            id: fieldId,
-            value: {vocabularyId: 'languages', qcodes: [item[fieldId]]},
-        };
-    } else if (fieldId === 'footer') {
-        // Does not have it's own separate field. Current implementation can change body_footer
-        // but body_footer has it's own field in content profile.
-        return null;
-    } else {
-        return null;
+
+    default:
+        if (customField?.custom_field_type != null) {
+            return {
+                type: 'custom',
+                id: fieldId,
+                value: {item: item, field: customField},
+            };
+        }
+
+        switch (fieldId) {
+        case 'feature_media':
+            return {
+                type: 'media-gallery',
+                id: fieldId,
+                value: item.associations.featuremedia == null ? [] : [item.associations.featuremedia],
+            };
+
+        case 'abstract':
+        case 'body_html':
+        case 'body_footer':
+            return {
+                type: 'html',
+                id: fieldId,
+                value: item[fieldId],
+            };
+
+        case 'anpa_category':
+        case 'genre':
+            return {
+                type: 'subjects',
+                id: fieldId,
+                value: item[fieldId],
+            };
+
+        case 'place':
+            return {
+                type: 'subjects',
+                id: fieldId,
+                value: item[fieldId]?.map(({name, code}) => ({name, qcode: code})),
+            };
+
+        case 'authors':
+            return {
+                type: 'subjects',
+                id: fieldId,
+                value: item[fieldId]?.map(({_id, name}) => ({name, qcode: _id})),
+            };
+
+        case 'keywords':
+            return {
+                type: 'subjects',
+                id: fieldId,
+                value: item[fieldId]?.map((keyword) => ({name: keyword, qcode: keyword})),
+            };
+
+        case 'products':
+        case 'languages':
+        case 'PhotoCategories':
+        case 'destination':
+        case 'company_codes':
+            return {
+                type: 'subjects',
+                id: fieldId,
+                value: item['subject']?.filter(({scheme}) => scheme === fieldId),
+            };
+
+        case 'subject':
+            return {
+                type: 'subjects',
+                id: fieldId,
+                value: item['subject']?.filter(({scheme}) => scheme == null),
+            };
+
+        case 'slugline':
+        case 'anpa_take_key':
+        case 'byline':
+        case 'ednote':
+        case 'headline':
+        case 'sign_off':
+        case 'usageterms':
+        case 'description_text':
+            return {
+                type: 'plain-text',
+                id: fieldId,
+                value: item[fieldId],
+            };
+
+        case 'sms':
+            return {
+                type: 'plain-text',
+                id: fieldId,
+                value: item.sms_message,
+            };
+
+        case 'dateline':
+            return {
+                type: 'plain-text',
+                id: fieldId,
+                value: item.dateline?.text,
+            };
+
+        case 'priority':
+        case 'urgency':
+            return {
+                type: 'plain-text',
+                id: fieldId,
+                value: item[fieldId]?.toString(),
+            };
+
+        case 'attachments':
+            return {
+                type: 'attachments',
+                id: fieldId,
+                value: item[fieldId],
+            };
+
+        case 'language':
+            return {
+                type: 'vocabulary-values',
+                id: fieldId,
+                value: {vocabularyId: 'languages', qcodes: [item[fieldId]]},
+            };
+
+        case 'footer':
+            // Does not have it's own separate field. Current implementation can change body_footer
+            // but body_footer has it's own field in content profile.
+            return null;
+
+        default:
+            return null;
+        }
     }
 }
