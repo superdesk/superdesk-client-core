e067c0fbe85066875ef857367187754ed9f6ddef
Tomas Kikutis <t.kikutis@gmail.com>
2020-11-16 14:29:58 +0100

    Preview fixes embed,missing fields (#3699)

--- scripts/core/helpers/item.ts
+++ scripts/core/helpers/item.ts
@@ -19,3 +19,7 @@ export const getThumbnailForItem = (item: IArticle) => {
 export function associationIsArticle(a: IArticle | IRelatedArticle): a is IArticle {
     return a['_etag'] != null;
 }
+
+export function isMediaType(item: IArticle) {
+    return ['audio', 'video', 'picture', 'graphic'].includes(item.type);
+}
