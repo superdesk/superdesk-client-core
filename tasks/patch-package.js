666e68c2fd6c550671032ca5c5400871123c7b2f
Tomas <t.kikutis@gmail.com>
2020-11-29 22:07:48 +0100

    copy directory instead of renaming

--- tasks/patch-package.js
+++ tasks/patch-package.js
@@ -2,6 +2,17 @@ const execSync = require('child_process').execSync;
 const fs = require('fs');
 const path = require('path');
 
+function copyFolderSync(from, to) {
+    fs.mkdirSync(to);
+    fs.readdirSync(from).forEach((element) => {
+        if (fs.lstatSync(path.join(from, element)).isFile()) {
+            fs.copyFileSync(path.join(from, element), path.join(to, element));
+        } else {
+            copyFolderSync(path.join(from, element), path.join(to, element));
+        }
+    });
+}
+
 const currentDir = process.cwd();
 const clientCoreRoot = path.join(__dirname, '../');
 const patchesCurrentDir = path.join(clientCoreRoot, 'patches');
@@ -9,7 +20,7 @@ const patchesDestinationDir = path.join(currentDir, 'patches');
 
 if (patchesCurrentDir !== patchesDestinationDir) {
     fs.rmdirSync(patchesDestinationDir, {recursive: true});
-    fs.renameSync(patchesCurrentDir, patchesDestinationDir);
+    copyFolderSync(patchesCurrentDir, patchesDestinationDir);
 }
 
 execSync(
