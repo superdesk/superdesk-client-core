d87c623a0ea36ea8f3516c35619c709ad21374f3
Nemanja Pavlovic <me@pavlovicnemanja.com>
2021-01-19 11:27:32 +0100

    fix(comments): Code improvement based on comments

--- scripts/apps/master-desk/components/AssignmentsComponent.tsx
+++ scripts/apps/master-desk/components/AssignmentsComponent.tsx
@@ -69,10 +69,14 @@ export class AssignmentsComponent extends React.Component<IProps, IState> {
         let total = 0;
 
         if (this.state.deskData && this.state.deskData[desk._id]) {
-            this.state.deskData[desk._id].forEach((item) =>
-                this.assignmentStages.find((stage) =>
-                    stage.code === item.state ? (total += item ? item.count : 0) : false),
-            );
+            this.state.deskData[desk._id].forEach((item) => {
+                let assignment = this.assignmentStages.find((stage) =>
+                    stage.code === item.state);
+
+                if (assignment) {
+                    total += item ? item.count : 0;
+                }
+            });
         }
 
         return total;
