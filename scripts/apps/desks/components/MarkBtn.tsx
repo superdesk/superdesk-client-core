3e6edeffed1988ba1f2bf3a2cc82c9db4f906be8
Tomas Kikutis <t.kikutis@gmail.com>
2021-01-20 14:11:42 +0100

    fix already marked desks not being disabled in the marking dialog (#3770)

--- scripts/apps/desks/components/MarkBtn.tsx
+++ scripts/apps/desks/components/MarkBtn.tsx
@@ -1,52 +1,35 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import ng from 'core/services/ng';
+import {IArticle, IDesk} from 'superdesk-api';
 
-/**
- * @ngdoc React
- * @module superdesk.apps.desks
- * @name MarkBtn
- * @param {Object} item the story to be marked
- * @param {Object} desk desk for the item to be marked
- * @param {Object} desk desksService
- * @description Represents a button for given item to be marked for the given desk
- */
-export class MarkBtn extends React.Component<any, any> {
-    static propTypes: any;
-    static defaultProps: any;
+interface IProps {
+    item: IArticle; // the story to be marked
+    desk: IDesk; // desk for the item to be marked
+}
 
-    item: any;
-    desk: any;
-    desks: any;
-    isMarked: any;
-
-    constructor(props) {
+export class MarkForDeskButton extends React.Component<IProps> {
+    constructor(props: IProps) {
         super(props);
-        this.item = props.item;
-        this.desk = props.desk;
-        this.desks = props.desks;
-        this.isMarked = this.item.marked_desks &&
-            this.item.marked_desks.some((md) => md.desk_id === this.desk._id);
-        this.markDesk = this.markDesk.bind(this);
+
+        this.markForDesk = this.markForDesk.bind(this);
     }
 
-    /** Marks the give item for a given desk */
-    markDesk(event) {
+    markForDesk(event) {
         event.stopPropagation();
-        this.desks.markItem(this.desk._id, this.item);
+
+        const {desk, item} = this.props;
+
+        ng.get('desks').markItem(desk._id, item);
     }
 
     render() {
+        const {desk, item} = this.props;
+        const isMarked = item.marked_desks?.find((_desk) => _desk?.desk_id === desk._id) != null;
+
         return (
-            <button disabled={this.isMarked} onClick={this.markDesk}>
-                <i className="icon-bell" />{this.desk.name}
+            <button disabled={isMarked} onClick={this.markForDesk} data-test-id="mark-for-desk--desk">
+                <i className="icon-bell" />{desk.name}
             </button>
         );
     }
 }
-
-/** Set the types of props for the MrkBtn */
-MarkBtn.propTypes = {
-    desks: PropTypes.object.isRequired,
-    desk: PropTypes.object.isRequired,
-    item: PropTypes.object.isRequired,
-};
