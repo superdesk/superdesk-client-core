24c64f8b71eded7f9dad6eb4696445ec4eb019c7
Tomas Kikutis <t.kikutis@gmail.com>
2021-02-02 09:20:53 +0100

    Drop IDs from extensions. They will have to be specified when loading.

--- scripts/extensions/auto-tagging-widget/src/extension.tsx
+++ scripts/extensions/auto-tagging-widget/src/extension.tsx
@@ -2,7 +2,6 @@ import {ISuperdesk, IExtension, IExtensionActivationResult, IArticle} from 'supe
 import {getAutoTaggingComponent} from './auto-tagging';
 
 const extension: IExtension = {
-    id: 'autoTagging',
     activate: (superdesk: ISuperdesk) => {
         const {gettext} = superdesk.localization;
 
