c59ccf7de556008bf1983912b3be3d864740d3cf
dev-tsoni <62991760+dev-tsoni@users.noreply.github.com>
2021-02-04 11:55:45 +0530

    SDESK-5679 ADD: Error message while adding duplicate set name (#3778)
    * SDESK-5679 ADD: Error message while adding duplicate set name
    
    * SDESK-5679 UPDATE: Added else case for name if nothing matches the key..
    
    * SDESK-5679 ADD: Moved error to API_ERRORS.
    
    * SDESK-5679 UPDATE: Kept error name dynamic.

--- scripts/extensions/sams/src/utils/api.ts
+++ scripts/extensions/sams/src/utils/api.ts
@@ -23,16 +23,32 @@ export function isSamsApiError(error: any): boolean {
 export const API_ERRORS: Dictionary<string, (error: IAPIError) => string> = {
     '04002': (_error) =>
         superdeskApi.localization.gettext('Error[04002]: Invalid search query'),
+    '04001': (_error) => {
+        for (let error of Object.keys(_error.errors!)) {
+            for (let key of _error.errors![error]) {
+                if (key === 'unique') {
+                    return superdeskApi.localization.gettext('Error[{{number}}]: {{error}} not unique', {
+                        number: _error.error,
+                        error: error.toUpperCase(),
+                    });
+                } else if (key === 'required' || key === 'empty values not allowed') {
+                    return superdeskApi.localization.gettext('Error[{{number}}]: {{error}} requried', {
+                        number: _error.error,
+                        error: error.toUpperCase(),
+                    });
+                }
+            }
+        }
+        return superdeskApi.localization.gettext('Error[{{number}}]: {{description}}', {
+            number: _error.error,
+            description: _error.description,
+        });
+    },
 };
 
 export function getApiErrorMessage(error: IAPIError): string {
     if (API_ERRORS[error.error] != null) {
         return API_ERRORS[error.error](error);
-    }
-    if (error.errors?.name != null) {
-        return superdeskApi.localization.gettext('Error[{{number}}]: Name requried', {
-            number: error.error,
-        });
     }
     return superdeskApi.localization.gettext('Error[{{number}}]: {{description}}', {
         number: error.error,
