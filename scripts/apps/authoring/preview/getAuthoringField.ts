ddf8980ef462970e9c47192a8d6d421740261fbf
Tomas Kikutis <t.kikutis@gmail.com>
2021-02-04 11:46:47 +0100

    _id is an array here

--- scripts/apps/authoring/preview/getAuthoringField.ts
+++ scripts/apps/authoring/preview/getAuthoringField.ts
@@ -98,7 +98,7 @@ export function getAuthoringField(
             return {
                 type: 'subjects',
                 id: fieldId,
-                value: item[fieldId]?.map(({_id, name}) => ({name, qcode: _id})),
+                value: item[fieldId]?.map(({_id, name}) => ({name, qcode: _id[1]})),
             };
 
         case 'keywords':
