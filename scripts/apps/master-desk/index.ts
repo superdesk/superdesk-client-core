88bb0fe5209a1650f3139ba9aa483ce217d47b69
Petr Jasek <jasekpetr@gmail.com>
2021-02-15 10:58:54 +0100

    fix master desk url

--- scripts/apps/master-desk/index.ts
+++ scripts/apps/master-desk/index.ts
@@ -8,7 +8,7 @@ angular.module('superdesk.apps.master-desk', [])
     .component('sdMasterDesk', reactToAngular1(MasterDesk, [], [], styles))
     .config(['superdeskProvider', 'workspaceMenuProvider', (superdesk, workspaceMenuProvider) => {
         superdesk
-            .activity('/master-desk/', {
+            .activity('/master-desk', {
                 label: gettext('Master Desk'),
                 priority: 100,
                 template: require('./views/master-desk.html'),
