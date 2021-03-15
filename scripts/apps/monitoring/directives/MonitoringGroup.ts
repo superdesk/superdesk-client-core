56723d714c7172c3932e1c587c1758693dcada68
Tomas Kikutis <t.kikutis@gmail.com>
2021-03-15 14:34:24 +0100

    Fix timing issue in monitoring groups (#3836)

--- scripts/apps/monitoring/directives/MonitoringGroup.ts
+++ scripts/apps/monitoring/directives/MonitoringGroup.ts
@@ -1,5 +1,5 @@
 /* eslint-disable complexity */
-import _ from 'lodash';
+import _, {throttle} from 'lodash';
 import getCustomSortForGroup, {GroupSortOptions} from '../helpers/CustomSortOfGroups';
 import {GET_LABEL_MAP, getLabelForStage} from '../../workspace/content/constants';
 import {isPublished} from 'apps/archive/utils';
@@ -141,6 +141,15 @@ export function MonitoringGroup(
                 );
             }
 
+            function scheduleQueryFn(event, data) {
+                queryItems(event, data, {auto: (data && data.force) ? 0 : 1})
+                    .finally(() => {
+                        scope.$applyAsync();
+                    });
+            }
+
+            const scheduleQuery = throttle(scheduleQueryFn, 1000);
+
             var monitoring = ctrls[0];
             var projections = search.getProjectedFields();
 
@@ -198,6 +207,7 @@ export function MonitoringGroup(
             scope.$on('item:duplicate', scheduleQuery);
             scope.$on('item:translate', scheduleQuery);
             scope.$on('item:move', scheduleQuery);
+            scope.$on('item:unlock', scheduleQuery);
             scope.$on('broadcast:created', (event, args) => {
                 scope.previewingBroadcast = true;
                 queryItems();
@@ -403,25 +413,6 @@ export function MonitoringGroup(
                     }
 
                     scope.style.maxHeight = groupItems * ITEM_HEIGHT - scrollOffset;
-                }
-            }
-
-            var queryTimeout;
-
-            /**
-             * Schedule content reload after some delay
-             */
-            function scheduleQuery(event, data) {
-                if (!queryTimeout) {
-                    queryTimeout = $timeout(() => {
-                        queryItems(event, data, {auto: (data && data.force) ? 0 : 1})
-                            .finally(() => {
-                                scope.$applyAsync(() => {
-                                    // ignore any updates requested in current $digest
-                                    queryTimeout = null;
-                                });
-                            });
-                    }, 1000, false);
                 }
             }
 
