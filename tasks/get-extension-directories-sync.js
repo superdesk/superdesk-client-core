187ce4fff83a5b6beba634543ae3877aee348ee7
Tomas Kikutis <t.kikutis@gmail.com>
2021-04-21 13:52:02 +0200

    fix issues with extensions setup

--- tasks/get-extension-directories-sync.js
+++ tasks/get-extension-directories-sync.js
@@ -2,15 +2,26 @@
 
 var fs = require('fs');
 var path = require('path');
-var getConfig = require('./options/get-config').getConfig;
+
+function getClientDir() {
+    const clientCoreRoot = path.join(__dirname, '../');
+    const maybeParentModulePath = path.join(clientCoreRoot, '../../');
+
+    // If node_modules exists in `maybeParentModulePath` set it as mainDirectory
+    const mainDirectory = fs.existsSync(path.join(maybeParentModulePath, 'node_modules'))
+        ? maybeParentModulePath
+        : clientCoreRoot;
+
+    return mainDirectory;
+}
 
 function getPaths(distRelative) {
     const from = 0;
     const to = distRelative.lastIndexOf('/dist/');
 
-    const extensionRootPath = path.join(process.cwd(), 'node_modules', distRelative.slice(from, to));
+    const extensionRootPath = path.join(getClientDir(), 'node_modules', distRelative.slice(from, to));
     const extensionSrcPath = path.join(extensionRootPath, 'src');
-    const extensionDistPath = path.join(process.cwd(), 'node_modules', distRelative);
+    const extensionDistPath = path.join(getClientDir(), 'node_modules', distRelative);
     const extensionCssFilePath = path.join(extensionRootPath, 'src/index.css');
 
     return {
@@ -21,11 +32,30 @@ function getPaths(distRelative) {
     };
 }
 
-function getIndexFilePath(indexFilePathRelative) {
-    const base = path.join(process.cwd(), 'dist', indexFilePathRelative);
+function getFileExtension(path) {
+    const lastDotAt = path.lastIndexOf('.');
 
-    for (const extension of ['.js', '.jsx', '.ts', '.tsx']) {
-        const filePath = base + extension;
+    if (lastDotAt < 0) {
+        return null;
+    } else {
+        return path.slice(lastDotAt);
+    }
+}
+
+function getIndexFilePath(indexFilePathRelative) {
+    const fileExtensions = ['.js', '.jsx', '.ts', '.tsx'];
+
+    // paths in `importApps` are relative client/dist
+    const base = path.join(getClientDir(), 'dist', indexFilePathRelative);
+
+    const maybeExtension = getFileExtension(base);
+
+    if (maybeExtension != null && fileExtensions.includes(maybeExtension)) {
+        return base;
+    }
+
+    for (const ext of fileExtensions) {
+        const filePath = path.join(base, `index${ext}`);
 
         if (fs.existsSync(filePath)) {
             return filePath;
@@ -36,7 +66,8 @@ function getIndexFilePath(indexFilePathRelative) {
 }
 
 function getExtensionDirectoriesSync() {
-    const config = getConfig();
+    const config = require(path.join(getClientDir(), 'superdesk.config.js'))();
+
     const paths = config.importApps || config.apps || [];
 
     // paths in `importApps` are relative client/dist
