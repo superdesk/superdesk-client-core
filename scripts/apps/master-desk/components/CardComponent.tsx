6bee7a44506b103215d8369c5e4f2e537e1ef7cb
Nemanja Pavlovic <me@pavlovicnemanja.com>
2021-06-23 11:55:57 +0200

    fix lint

--- scripts/apps/master-desk/components/CardComponent.tsx
+++ scripts/apps/master-desk/components/CardComponent.tsx
@@ -19,15 +19,16 @@ export class CardComponent extends React.Component<IProps, {}> {
     };
 
     selectDesk(desk: IDesk) {
-        this.props.onDeskSelect ?
-            this.props.onDeskSelect(desk) : null;
+        if (this.props.onDeskSelect) {
+            this.props.onDeskSelect(desk);
+        }
     }
 
     render() {
         return (
             <div className="sd-board" >
                 <div
-                    className={"sd-board__header" + (this.props.onDeskSelect ? " sd-board__header--clickable" : "")}
+                    className={'sd-board__header' + (this.props.onDeskSelect ? ' sd-board__header--clickable' : '')}
                     onClick={() => this.selectDesk(this.props.desk)}
                 >
                     <h3 className="sd-board__header-title">{this.props.desk.name}</h3>
