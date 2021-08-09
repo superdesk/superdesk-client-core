cc85428327dcb0241751d4595c95946a6d34f7f2
Tomas Kikutis <t.kikutis@gmail.com>
2021-08-09 15:05:04 +0200

    add local storage flag for network queue logs

--- scripts/core/constants.ts
+++ scripts/core/constants.ts
@@ -4,3 +4,16 @@ export const ARTICLE_RELATED_RESOURCE_NAMES = [
     'archive_spike',
     'archive_unspike',
 ];
+
+export enum IDevTools {
+    reduxLogger = 'reduxLogger',
+    networkQueueLogger = 'networkQueueLogger',
+}
+
+const devtoolsString = localStorage.getItem('devtools');
+const devToolsValues = devtoolsString == null ? [] : JSON.parse(devtoolsString);
+
+export const DEV_TOOLS = {
+    reduxLoggerEnabled: devToolsValues.includes('redux-logger'),
+    networkQueueLoggerEnabled: devToolsValues.includes('network-queue-logger'),
+};
