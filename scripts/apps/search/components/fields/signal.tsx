226fe88da1b61d8501bdd4d5d19461135047721c
Petr Jasek <jasekpetr@gmail.com>
2021-10-18 16:23:19 +0200

    fix signal handling in item list
    SDESK-6198

--- scripts/apps/search/components/fields/signal.tsx
+++ scripts/apps/search/components/fields/signal.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import {IPropsItemListInfo} from '../ListItemInfo';
 
-class SignalComponent extends React.Component<IPropsItemListInfo> {
+class SignalComponent extends React.PureComponent<IPropsItemListInfo> {
     render() {
         const props = this.props;
 
-        if (props.item.signal) {
-            return React.createElement('span', {className: 'signal', key: 'signal'}, props.item.signal);
-        } else {
+        if (props.item.signal == null) {
             return null;
         }
+
+        return (
+            <React.Fragment>
+                {props.item.signal.map((_signal) => (
+                    <span className="signal" key={_signal.qcode}>{_signal.name || _signal.qcode}</span>
+                ))}
+            </React.Fragment>
+        );
     }
 }
 
