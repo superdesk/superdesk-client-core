9c6c9fb907a9d32ba9defd0fcbe45d59afc5cd23
Tomas Kikutis <t.kikutis@gmail.com>
2021-11-02 12:13:40 +0100

    ensure that extension components render with the latest data (#4021)

--- scripts/apps/authoring/authoring/header-component-field.tsx
+++ scripts/apps/authoring/authoring/header-component-field.tsx
@@ -11,8 +11,15 @@ export class HeaderComponentField extends React.PureComponent<IProps> {
     render() {
         const Component = this.props.component;
 
+        /**
+         * Item properties are mutated from angular based authoring code.
+         * Always create a new object to ensure that components from extensions
+         * display the latest data, even if using React.PureComponent
+         */
+        const article = {...this.props.item};
+
         return (
-            <Component article={this.props.item} />
+            <Component article={article} />
         );
     }
 }
