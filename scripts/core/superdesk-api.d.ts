68b254fef80f4b931a634d46e8d33aecbd7b6ac0
Tomas Kikutis <t.kikutis@gmail.com>
2021-11-19 12:16:37 +0100

    fix manual reloading of articles (#4029)

--- scripts/core/superdesk-api.d.ts
+++ scripts/core/superdesk-api.d.ts
@@ -1931,7 +1931,14 @@ declare module 'superdesk-api' {
             confirmMediaOnUpdate?: any;
             noMissingLink?: any;
             hideRoutedDesks?: any;
-            autorefreshContent?: any;
+
+            /**
+             * If set to false, a refresh icon will be displayed
+             * instead of reloading the list automatically
+             * when items are created/deleted
+             */
+            autorefreshContent?: boolean;
+
             elasticHighlight?: any;
             onlyEditor3?: any;
             nestedItemsInOutputStage?: boolean;
