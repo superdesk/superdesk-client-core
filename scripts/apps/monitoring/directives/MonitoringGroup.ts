f534e24c91b7672bf83d5c4cf6a9ce495aabd0d5
Tomas Kikutis <t.kikutis@gmail.com>
2021-12-14 16:30:28 +0100

    manual refresh fix v4 - reset page size to default (#4039)

--- scripts/apps/monitoring/directives/MonitoringGroup.ts
+++ scripts/apps/monitoring/directives/MonitoringGroup.ts
@@ -649,6 +649,9 @@ export function MonitoringGroup(
                             // update scope items only with the matching fetched items
                             scope.items = search.updateItems(items, scope.items);
                         }
+                    }).finally(() => {
+                        // reset page size to default
+                        criteria.source.size = PAGE_SIZE;
                     });
             }
 
@@ -693,7 +696,7 @@ export function MonitoringGroup(
                 })()
                     .then((items) => {
                         scope.$applyAsync(() => {
-                            if (scope.total !== items._meta.total) {
+                            if (!scope.showRefresh && scope.total !== items._meta.total) {
                                 scope.total = items._meta.total;
                             }
                             let onlyHighlighted = scope.group.type === 'highlights'
