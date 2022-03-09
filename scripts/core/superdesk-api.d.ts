d6727a0ed2b400949a6fd110476cad37d46b6287
MarkLark86 <mark.pittaway@gmail.com>
2022-03-09 14:30:58 +1100

    [TGA-16] Ability to use template_url from transmitter config (#4075)

--- scripts/core/superdesk-api.d.ts
+++ scripts/core/superdesk-api.d.ts
@@ -2092,6 +2092,7 @@ declare module 'superdesk-api' {
             type: string;
             name: string;
             config: any;
+            template_url?: string;
         }>;
 
         userOnlineMinutes: number;
