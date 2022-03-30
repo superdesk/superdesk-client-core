4cd44ccd88608b2cb2ce8b3b6cb5049ef13434fe
Tomas Kikutis <t.kikutis@gmail.com>
2022-03-30 11:01:17 +0200

    enable adding more entities to store easily

--- scripts/core/data/index.ts
+++ scripts/core/data/index.ts
@@ -1,35 +1,146 @@
-import {combineReducers, createStore} from 'redux';
-import {IBaseRestApiResponse, IResourceUpdateEvent, IWebsocketMessage} from 'superdesk-api';
+import {createStore} from 'redux';
+import {IBaseRestApiResponse, IResourceUpdateEvent, IUser, IWebsocketMessage} from 'superdesk-api';
 import {addWebsocketEventListener} from 'core/notification/notification';
-import {users, IUserState, IUserAction} from './users';
 import {getMiddlewares} from 'core/redux-utils';
 import {debounceAsync, IDebounced} from 'core/helpers/debounce-async';
 import {requestQueue} from 'core/helpers/request-queue';
+import {keyBy} from 'lodash';
 
-export type IEntityAction = {
-    type: 'UPDATE_ENTITY',
-    payload: {
-        resource: string;
-        _id: string;
-        data: IBaseRestApiResponse;
-    },
-};
-
-export interface IStoreState {
-    users: IUserState;
-}
-
-type IAction = IUserAction;
-
-export const store = createStore<IStoreState, IAction, {}, {}>(combineReducers({
-    users,
-}), getMiddlewares());
-
-const updating: {[key: string]: IDebounced} = {};
+/**
+ * To add a new entity, add it to
+ * resourcesInStore, IInterfacesByResourceType
+ * and call `initEntity`
+ */
 
 const resourcesInStore = [
     'users',
-];
+] as const;
+
+interface IInterfacesByResourceType {
+    users: IUser;
+}
+
+type IStoreResource = typeof resourcesInStore[number];
+
+export type IStoreState = {
+    [Properties in keyof IInterfacesByResourceType]: {
+        entities: {
+            [key: string]: IInterfacesByResourceType[Properties];
+        };
+    };
+};
+
+type IUpdateEntityAction = {
+    type: 'UPDATE_ENTITY',
+    payload: {
+        resource: IStoreResource;
+        _id: string;
+        data: any;
+    },
+};
+
+interface IInitEntityAction {
+    type: 'INIT_ENTITY';
+    payload: {
+        resource: string;
+        items: Array<any>;
+    };
+}
+
+interface IUpdateLastUserActiveAction {
+    type: 'UPDATE_USER_LAST_ACTIVE';
+    payload: IUser['_id'];
+}
+
+type IUserAction = IUpdateLastUserActiveAction;
+
+type IAction = IUserAction | IUpdateEntityAction | IInitEntityAction;
+
+const initialState: IStoreState = {
+    users: {
+        entities: {},
+    },
+};
+
+function isStoreResource(resource: any): resource is IStoreResource {
+    return resourcesInStore.includes(resource);
+}
+
+function reducer(state: IStoreState = initialState, action: IAction): IStoreState {
+    switch (action.type) {
+    /**
+     * USER ACTIONS
+     */
+    case 'UPDATE_USER_LAST_ACTIVE':
+        return (() => {
+            const userId = action.payload;
+
+            return {
+                ...state,
+                users: {
+                    entities: {
+                        ...state.users.entities,
+                        [userId]: {
+                            ...state.users.entities[userId],
+                            last_activity_at: (new Date()).toISOString(),
+                        },
+                    },
+                },
+            };
+        })();
+
+    /**
+     * GENERIC ACTIONS THAT APPLY TO ALL ENTITIES
+     */
+    case 'INIT_ENTITY':
+        return (() => {
+            const {resource, items} = action.payload;
+
+            return {
+                ...state,
+                [resource]: {
+                    entities: keyBy(items, (entity) => entity._id),
+                },
+            };
+        })();
+
+    case 'UPDATE_ENTITY':
+        return (() => {
+            const resource = action.payload.resource;
+            const entity = action.payload.data;
+
+            return {
+                ...state,
+                [resource]: {
+                    entities: {
+                        ...state[resource].entities,
+                        [entity._id]: entity,
+                    },
+                },
+            };
+        })();
+
+    default:
+        return state;
+    }
+}
+
+export const store = createStore<IStoreState, IAction, {}, {}>(reducer, getMiddlewares());
+
+export function initEntity<T extends IStoreResource>(
+    resource: T,
+    items: Array<IInterfacesByResourceType[T]>,
+) {
+    store.dispatch({
+        type: 'INIT_ENTITY',
+        payload: {
+            resource,
+            items,
+        },
+    });
+}
+
+const updating: {[key: string]: IDebounced} = {};
 
 addWebsocketEventListener(
     'resource:updated',
@@ -37,7 +148,7 @@ addWebsocketEventListener(
         const {resource, _id, fields} = event.extra;
         const fieldKeys = Object.keys(fields);
 
-        if (!resourcesInStore.includes(resource)) {
+        if (!isStoreResource(resource)) {
             return;
         } else if (resource === 'users' &&
             fieldKeys.length === 1 &&
