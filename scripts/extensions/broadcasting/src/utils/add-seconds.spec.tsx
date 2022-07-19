71701c306dd25bf0a47193d928a70f9d4155dbef
Tomas Kikutis <t.kikutis@gmail.com>
2022-07-19 14:12:01 +0200

    nicer formatting of assertions

--- scripts/extensions/broadcasting/src/utils/add-seconds.spec.tsx
+++ scripts/extensions/broadcasting/src/utils/add-seconds.spec.tsx
@@ -1,9 +1,12 @@
 import {describe, it} from 'mocha';
-import {equal} from 'assert';
+import * as assert from 'assert';
 import {addSeconds} from './add-seconds';
 
 describe('utils.addSeconds', () => {
     it('adds one hour', () => {
-        equal(addSeconds('12:31', 3600), '13:31:00.000');
+        assert.equal(
+            addSeconds('12:31', 3600),
+            '13:31:00.000',
+        );
     });
 });
