684f3a84673552c5fd6ba1d92a8d4ed02e3b6260
Tomas Kikutis <t.kikutis@gmail.com>
2022-07-29 12:02:13 +0200

    use avatar in authors dropdown

--- scripts/apps/authoring-react/field-adapters/authors.tsx
+++ scripts/apps/authoring-react/field-adapters/authors.tsx
@@ -1,13 +1,35 @@
+/* eslint-disable react/no-multi-comp */
 import * as React from 'react';
 import ng from 'core/services/ng';
 import {IArticle, IAuthor, IAuthoringFieldV2, IFieldAdapter, IUser, IDropdownTreeConfig} from 'superdesk-api';
 import {gettext} from 'core/utils';
 import {arrayToTree} from 'core/helpers/tree';
+import {UserAvatar} from 'apps/users/components/UserAvatar';
+import {Spacer} from 'core/ui/components/Spacer';
+
+function isAuthorRole(x: IUserOption | IAuthorRole): x is IAuthorRole {
+    return Array.isArray(x._id);
+}
 
 function valueTemplate({item}) {
     return (
         <span>{item.name}: {item.sub_label}</span>
     );
+}
+
+function optionTemplate({item}: {item: IUserOption | IAuthorRole}) {
+    if (isAuthorRole(item)) {
+        return (
+            <span>{item.name}</span>
+        );
+    } else {
+        return (
+            <Spacer h gap="8" justifyContent="start" noGrow>
+                <UserAvatar user={item.user} size="small" displayAdministratorIndicator />
+                {item.name}
+            </Spacer>
+        );
+    }
 }
 
 const getId = (item) => typeof item._id === 'string' ? item._id : JSON.stringify(item._id);
@@ -20,10 +42,6 @@ interface IUserOption {
 
 interface IAuthorRole extends IAuthor {
     parent: IUser['_id'];
-}
-
-function isAuthorRole(x: IUserOption | IAuthorRole): x is IAuthorRole {
-    return Array.isArray(x._id);
 }
 
 export const authors: IFieldAdapter<IArticle> = {
@@ -52,6 +70,7 @@ export const authors: IFieldAdapter<IArticle> = {
             canSelectBranchWithChildren: () => false,
             getLabel: (item: IUserOption | IAuthorRole) => item.name,
             valueTemplate: valueTemplate,
+            optionTemplate: optionTemplate,
             getId: (item) => getId(item),
             source: 'dropdown-tree',
             multiple: true,
