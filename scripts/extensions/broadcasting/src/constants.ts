10d52d2d1ecb4a3a969c64a87c00a7dcc90aeefc
Tomas Kikutis <t.kikutis@gmail.com>
2022-09-13 12:16:16 +0200

    update vocabulary name

--- scripts/extensions/broadcasting/src/constants.ts
+++ scripts/extensions/broadcasting/src/constants.ts
@@ -1,4 +1,4 @@
 export const LANGUAGE = 'en';
 export const SHOW_PART_VOCABULARY_ID = 'show_part';
 export const RUNDOWN_SUBITEM_TYPES = 'rundown_subitem_types';
-export const RUNDOWN_ITEM_TYPES_VOCABULARY_ID = 'rundown-item-types';
+export const RUNDOWN_ITEM_TYPES_VOCABULARY_ID = 'rundown_item_types';
