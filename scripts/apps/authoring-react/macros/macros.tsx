d0e928318c56781efb48a6a8991593fbaf25448d
Konstantin Markov <maga.ot.oz@protonmail.com>
2022-10-14 17:00:43 +0300

    Code review improvements

--- scripts/apps/authoring-react/macros/macros.tsx
+++ scripts/apps/authoring-react/macros/macros.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import {
     IArticle,
     IArticleSideWidget,
+    IContentProfileV2,
     IExtensionActivationResult,
-    IFieldsV2,
     IRestApiResponse,
 } from 'superdesk-api';
 import {gettext} from 'core/utils';
@@ -21,6 +21,7 @@ import {Switch} from 'superdesk-ui-framework/react/components/Switch';
 import {omitFields} from '../data-layer';
 import {Spacer} from 'core/ui/components/Spacer';
 import {nameof} from 'core/helpers/typescript-helpers';
+import {EDITOR_3_FIELD_TYPE} from '../fields/editor3';
 
 type IProps = React.ComponentProps<
     IExtensionActivationResult['contributions']['authoringSideWidgets'][0]['component']
@@ -30,24 +31,40 @@ const getLabel = () => gettext('Macros widget');
 
 interface IState {
     macros: Array<IMacro> | null;
-    article: {
-        saved: IArticle | null,
-        autosaved: IArticle | null,
-    };
-    displayGrouped: boolean | null;
+
+    // not-supported when there are no groups
+    displayGrouped: boolean | 'not-supported';
 }
 
-const EDITOR_3_FIELD_TYPE = 'editor3';
+interface IMacroGroup {
+    initiallyOpen: boolean;
+    macros: Array<IMacro>;
+    groupName: string;
+}
+
+function getAllMacros(): Promise<IRestApiResponse<IMacro>> {
+    return httpRequestJsonLocal<IRestApiResponse<IMacro>>({
+        method: 'GET',
+        path: '/macros',
+        urlParams: {
+            backend: false,
+            desk: sdApi.desks.getCurrentDeskId(),
+            max_results: 200,
+            page: 1,
+        },
+    });
+}
 
 function overwriteArticle(
     currentArticle: IArticle,
     patch: Partial<IArticle>,
-    contentProfileFields: IFieldsV2,
+    profile: IContentProfileV2,
 ): Promise<void> {
     const patchCopy = omitFields({...patch, fields_meta: {}});
+    const allFields = profile.header.merge(profile.content);
 
     Object.keys(patchCopy).forEach((fieldKey) => {
-        const currentField = contentProfileFields.get(fieldKey);
+        const currentField = allFields.get(fieldKey);
 
         if (currentField != null && currentField.fieldType === EDITOR_3_FIELD_TYPE) {
             patchCopy.fields_meta[currentField.name.toLowerCase()] = {};
@@ -69,39 +86,18 @@ class MacrosWidget extends React.PureComponent<IProps, IState> {
 
         this.state = {
             macros: null,
-            article: {
-                saved: null,
-                autosaved: null,
-            },
             displayGrouped: false,
         };
     }
 
     componentDidMount(): void {
-        Promise.all([
-            this.getAllMacros(),
-            this.props.authoringStorage.getEntity(this.props.article._id),
-        ]).then(([macros, article]) => {
+        getAllMacros().then((macros) => {
             const groupedMacros = groupBy(macros._items.filter((x) => x.group != null), nameof<IMacro>('group'));
 
             this.setState({
                 macros: macros._items,
-                article: article,
                 displayGrouped: Object.keys(groupedMacros).length > 0 ? true : null,
             });
-        });
-    }
-
-    getAllMacros(page = 1): Promise<IRestApiResponse<IMacro>> {
-        return httpRequestJsonLocal<IRestApiResponse<IMacro>>({
-            method: 'GET',
-            path: '/macros',
-            urlParams: {
-                backend: false,
-                desk: sdApi.desks.getCurrentDeskId(),
-                max_results: 200,
-                page: page,
-            },
         });
     }
 
@@ -111,14 +107,14 @@ class MacrosWidget extends React.PureComponent<IProps, IState> {
             path: '/macros',
             payload: {
                 macro: macro.name,
-                item: this.state.article.saved,
+                item: this.props.article,
             },
         }).then((res: IMacro) => {
             const patch = generatePatch(this.props.article, res.item);
             const isEmpty = Object.keys(patch).length < 1;
 
             if (!isEmpty) {
-                overwriteArticle(this.props.article, patch, this.props.contentProfile.content);
+                overwriteArticle(this.props.article, patch, this.props.contentProfile);
             }
         });
     }
@@ -128,21 +124,35 @@ class MacrosWidget extends React.PureComponent<IProps, IState> {
             return null;
         }
 
-        const groupedOrdered: Dictionary<string, Array<IMacro>> = {};
+        const groupedOrdered: Array<IMacroGroup> = [];
 
-        if (this.state.displayGrouped != null) {
-            groupedOrdered['Quick List'] = this.state.macros.filter((m) => m.order != null);
+        if (this.state.displayGrouped !== 'not-supported') {
+            groupedOrdered.push({
+                groupName: gettext('Quick List'),
+                initiallyOpen: true,
+                macros: this.state.macros.filter((m) => m.order != null),
+            });
             const groupedMacros = groupBy(this.state.macros.filter((m) => m.group != null), nameof<IMacro>('group'));
 
-            Object.keys(groupedMacros).sort().forEach((key) => {
-                groupedOrdered[key] = groupedMacros[key].sort((a, b) => a.label.localeCompare(b.label));
+            Object.entries(groupedMacros).forEach(([groupName, macros]) => {
+                groupedOrdered.push({
+                    groupName: groupName,
+                    initiallyOpen: false,
+                    macros: groupedMacros[groupName].sort((a, b) => a.label.localeCompare(b.label)),
+                });
             });
-            groupedOrdered['Miscallaneous'] = this.state.macros.filter((x) => x.group == null);
+            groupedOrdered.push({
+                groupName: gettext('Miscellaneous'),
+                initiallyOpen: false,
+                macros: this.state.macros
+                    .filter((m) => m.group == null)
+                    .sort((a, b) => a.label.localeCompare(b.label)),
+            });
         }
 
-        const RunMacroButton = (macro: IMacro) => {
+        const RunMacroButton: React.ComponentType<{macro: IMacro}> = ({macro}) => {
             return (
-                <div key={macro.name} style={{paddingTop: 4}}>
+                <div style={{paddingTop: 4}}>
                     <Button
                         expand
                         style="hollow"
@@ -163,35 +173,32 @@ class MacrosWidget extends React.PureComponent<IProps, IState> {
                 )}
                 body={(
                     <Spacer v gap="8">
-                        {this.state.displayGrouped != null && (
+                        {this.state.displayGrouped !== 'not-supported' && (
                             <Switch
                                 label={{text: gettext('Group Macros')}}
-                                value={this.state.displayGrouped != null ? this.state.displayGrouped : null}
+                                value={this.state.displayGrouped}
                                 onChange={() => this.setState({displayGrouped: !this.state.displayGrouped})}
                             />
                         )}
                         {
                             this.state.displayGrouped ? (
-                                <React.Fragment>
-                                    {
-                                        Object.entries(groupedOrdered).map((entry, i) => {
-                                            return (
-                                                <ToggleBox
-                                                    key={i}
-                                                    initiallyOpen={entry[0] === 'Quick List' && true}
-                                                    title={entry[0]}
-                                                >
-                                                    {entry[1].map((macro) => RunMacroButton(macro))}
-                                                </ToggleBox>
-                                            );
-                                        })
-                                    }
-                                </React.Fragment>
-                            ) : (
-                                <React.Fragment>
-                                    {this.state.macros.map((macro) => RunMacroButton(macro))}
-                                </React.Fragment>
-                            )
+                                groupedOrdered.map((group, i) => {
+                                    return (
+                                        <ToggleBox
+                                            key={i}
+                                            initiallyOpen={group.initiallyOpen}
+                                            title={group.groupName}
+                                        >
+                                            {group.macros.map((macro) => (
+                                                <RunMacroButton
+                                                    key={macro.name}
+                                                    macro={macro}
+                                                />
+                                            ))}
+                                        </ToggleBox>
+                                    );
+                                })
+                            ) : this.state.macros.map((macro) => (<RunMacroButton key={macro.name} macro={macro} />))
                         }
                     </Spacer>
                 )}
