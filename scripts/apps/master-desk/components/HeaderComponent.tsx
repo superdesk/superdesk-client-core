51f57a09a3f8053d6cd19c80a99035de9fe9aec0
pavlovicnemanja <me@pavlovicnemanja.com>
2022-10-18 16:27:55 +0200

    fix(radio): Update components from new version of ui framework

--- scripts/apps/master-desk/components/HeaderComponent.tsx
+++ scripts/apps/master-desk/components/HeaderComponent.tsx
@@ -3,7 +3,7 @@ import ng from 'core/services/ng';
 import {IDesk} from 'superdesk-api';
 import {
     CheckButtonGroup,
-    RadioButton,
+    RadioGroup,
     Switch,
     Dropdown,
     NavButton,
@@ -193,9 +193,9 @@ export class HeaderComponent extends React.Component<IProps, IState> {
                         </button>
                     ) : null}
 
-                    <ButtonGroup align="left" padded={true}>
+                    <ButtonGroup align="start" padded={true}>
                         <CheckButtonGroup>
-                            <RadioButton
+                            <RadioGroup
                                 value={this.props.activeTab}
                                 options={tabs}
                                 onChange={(value: IMasterDeskTab) => this.changeTab(value)}
@@ -205,7 +205,7 @@ export class HeaderComponent extends React.Component<IProps, IState> {
 
                     {this.props.selectedDesk != null &&
                         this.isDeskMember(this.props.selectedDesk) ? (
-                            <ButtonGroup align="right">
+                            <ButtonGroup align="end">
                                 <Button
                                     text={gettext('Go To Desk')}
                                     style="hollow"
@@ -228,6 +228,7 @@ export class HeaderComponent extends React.Component<IProps, IState> {
                                 <label>{gettext('Show all')}</label>
                                 <div className="pull-right">
                                     <Switch
+                                        label={{text: gettext('Show all'), hidden: true}}
                                         value={this.state.showAllDesks}
                                         onChange={() => this.toggleShowAll()}
                                     />
@@ -239,6 +240,7 @@ export class HeaderComponent extends React.Component<IProps, IState> {
                                     <label>{item.name}</label>
                                     <div className="pull-right">
                                         <Switch
+                                            label={{text: item.name, hidden: true}}
                                             value={this.isDeskActive(item)}
                                             onChange={() => this.toggleDesk(item)}
                                         />
