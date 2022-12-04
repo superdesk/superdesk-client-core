eda6b211fffb28348e23b9a4c53698ff28d63206
devketanpro <devketanpro11@gmail.com>
2022-12-04 12:23:32 +0530

    Add types in the function

--- scripts/core/editor3/helpers/selection/blockKeys.ts
+++ scripts/core/editor3/helpers/selection/blockKeys.ts
@@ -1,8 +1,12 @@
-export const getBlockKeys = (contentState, start, end) => [
-    contentState
+export interface IContentState {
+    getBlockMap: () => { keySeq: () => { skipUntil: (k: any) => { takeUntil: (k: any) => void } } };
+}
+
+export const getBlockKeys = (contentState: IContentState, start: string, end: string) =>
+    [contentState
         .getBlockMap()
         .keySeq()
         .skipUntil((k) => k === start)
         .takeUntil((k) => k === end),
     end,
-];
+    ];
