1f5eaf3f544f98485708ffd1ed88c3d8b508b286
Konstantin Markov <maga.ot.oz@protonmail.com>
2022-12-07 13:30:54 +0200

    Revert "Toolbar export action modal"
    This reverts commit eb3914d9d5dfb0437c95d4847b98408cc1352d6f.

--- scripts/apps/authoring-react/authoring-integration-wrapper.tsx
+++ scripts/apps/authoring-react/authoring-integration-wrapper.tsx
@@ -38,8 +38,6 @@ import {ITEM_STATE} from 'apps/archive/constants';
 import {dispatchInternalEvent} from 'core/internal-events';
 import {IArticleActionInteractive} from 'core/interactive-article-actions-panel/interfaces';
 import {ARTICLE_RELATED_RESOURCE_NAMES} from 'core/constants';
-import {showModal} from '@superdesk/common';
-import ExportModal from './toolbar/export-modal';
 
 function getAuthoringActionsFromExtensions(
     item: IArticle,
@@ -142,30 +140,6 @@ export class AuthoringIntegrationWrapper extends React.PureComponent<IProps> {
                 (Component) => (props: {item: IArticle}) => <Component article={props.item} />,
             );
 
-        const getExportModal = (
-            item: IArticle,
-            handleUnsavedChanges: () => Promise<IArticle>,
-            hasUnsavedChanges: boolean,
-        ): IAuthoringAction => ({
-            label: gettext('Export'),
-            onTrigger: () => {
-                const openModal = (article: IArticle) => showModal(({closeModal}) => {
-                    return (
-                        <ExportModal
-                            closeModal={closeModal}
-                            article={article}
-                        />
-                    );
-                });
-
-                if (hasUnsavedChanges) {
-                    handleUnsavedChanges().then((article) => openModal(article));
-                } else {
-                    openModal(item);
-                }
-            },
-        });
-
         return (
             <WithInteractiveArticleActionsPanel location="authoring">
                 {(panelState, panelActions) => {
@@ -199,14 +173,7 @@ export class AuthoringIntegrationWrapper extends React.PureComponent<IProps> {
                                 onEditingEnd={(article) => {
                                     dispatchCustomEvent('articleEditEnd', article);
                                 }}
-                                getActions={({
-                                    item,
-                                    contentProfile,
-                                    fieldsData,
-                                    getLatestItem,
-                                    handleUnsavedChanges,
-                                    hasUnsavedChanges,
-                                }) => {
+                                getActions={({item, contentProfile, fieldsData}) => {
                                     return Promise.all([
                                         getAuthoringActionsFromExtensions(item, contentProfile, fieldsData),
                                         getArticleActionsFromExtensions(item),
@@ -214,7 +181,6 @@ export class AuthoringIntegrationWrapper extends React.PureComponent<IProps> {
                                         const [authoringActionsFromExtensions, articleActionsFromExtensions] = res;
 
                                         return [
-                                            getExportModal(getLatestItem(), handleUnsavedChanges, hasUnsavedChanges()),
                                             ...authoringActionsFromExtensions,
                                             ...articleActionsFromExtensions,
                                         ];
