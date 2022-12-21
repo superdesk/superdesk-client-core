50e9d834b5d11488c5f0568799e22d252466e0f2
Konstantin Markov <maga.ot.oz@protonmail.com>
2022-12-21 11:16:37 +0200

    Resolve conflicts, remove duplicate field type

--- scripts/apps/authoring-react/fields/editor3/index.tsx
+++ scripts/apps/authoring-react/fields/editor3/index.tsx
@@ -48,12 +48,12 @@ export function editor3ToOperationalFormat(
     return result;
 }
 
-export const EDITOR_3_FIELD_ID = 'editor3';
+export const EDITOR_3_FIELD_TYPE = 'editor3';
 
 export function getEditor3Field()
 : ICustomFieldType<IEditor3ValueOperational, IEditor3ValueStorage, IEditor3Config, IUserPreferences> {
     const field: ICustomFieldType<IEditor3ValueOperational, IEditor3ValueStorage, IEditor3Config, IUserPreferences> = {
-        id: EDITOR_3_FIELD_ID,
+        id: EDITOR_3_FIELD_TYPE,
         label: gettext('Editor3 (authoring-react)'),
         editorComponent: Editor,
         previewComponent: Preview,
