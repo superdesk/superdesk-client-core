9cbbef410e4ef22b2ffe85fabf5fadf3618ad41e
MarkLark86 <mark.pittaway@gmail.com>
2023-03-09 19:13:56 +1100

    [SDESK-6847] fix: Update moment and use newer timezone file for webpack alias (#4220)
    * [SDESK-6847] fix: Update moment and use newer timezone file for webpack alias
    
    * fix types after Moment changes

--- scripts/core/datetime/datetime.ts
+++ scripts/core/datetime/datetime.ts
@@ -19,7 +19,7 @@ const SERVER_FORMAT = 'YYYY-MM-DDTHH:mm:ssZZ';
 *
 * @param {String} d iso format datetime
 */
-export function longFormat(d: string): string {
+export function longFormat(d: string | moment.Moment): string {
     return moment(d).format(LONG_FORMAT);
 }
 
