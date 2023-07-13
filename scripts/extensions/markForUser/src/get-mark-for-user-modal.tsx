c2a5858a3618e42f8c77509913abe78607086f15
dzonidoo <nikola71092@gmail.com>
2023-07-13 14:01:03 +0200

    fix lint

--- scripts/extensions/markForUser/src/get-mark-for-user-modal.tsx
+++ scripts/extensions/markForUser/src/get-mark-for-user-modal.tsx
@@ -50,7 +50,7 @@ export function getMarkForUserModal(options: {
                 selectedUserId: markedForUserInitial,
             };
         }
-        
+
         render() {
             return (
                 <Modal
