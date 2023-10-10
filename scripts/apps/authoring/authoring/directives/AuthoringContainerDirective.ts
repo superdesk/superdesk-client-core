fcd54af6af6c5e522dd1b7d836fb71b6c93aaee1
Konstantin Markov <maga.ot.oz@protonmail.com>
2023-10-10 13:05:27 +0300

    Fix lint

--- scripts/apps/authoring/authoring/directives/AuthoringContainerDirective.ts
+++ scripts/apps/authoring/authoring/directives/AuthoringContainerDirective.ts
@@ -35,7 +35,7 @@ export function AuthoringContainerDirective(authoringWorkspace: AuthoringWorkspa
             // Needed only for authoring Angular. In authoring react we have a generic
             // event ('resource:updated') which listens to all item changes.
             scope.$on('author_approval:updated', (event) => {
-                if (event.item_id == scope.item._id) {
+                if (event.item_id === scope.item._id) {
                     scope.item.extra.publish_sign_off = event.sign_off_new_data;
                 }
             });
