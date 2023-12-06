67e3344845a4aacd81dd71ad05571c20f3392fe3
Tomas Kikutis <t.kikutis@gmail.com>
2023-12-06 13:08:04 +0100

    remove superdesk-common (moved to a separate repo)

--- superdesk-common/mocha-setup.ts
+++
@@ -1,7 +0,0 @@
-import 'jsdom-global/register';
-import * as Enzyme from 'enzyme';
-import * as Adapter from 'enzyme-adapter-react-16';
-
-export function mochaGlobalSetup() {
-    Enzyme.configure({adapter: new Adapter()});
-}
--- superdesk-common/src/time/add-seconds.ts
+++
@@ -1,9 +0,0 @@
-import * as dateFns from 'date-fns';
-import {getTimeStringIso, ITimeISO} from './get-time-string-iso';
-
-export function addSeconds(time: ITimeISO, seconds: number): ITimeISO {
-    return getTimeStringIso(dateFns.addSeconds(
-        new Date(`1970-01-01T${time}`),
-        seconds,
-    ));
-}
