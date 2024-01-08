5e7ecb05bb42d5471a5323235cd219da09fa535d
cmuldur <clm@cp.org>
2024-01-08 09:37:11 -0500

    Add forceRenderKey to AutoTaggingComponent

--- scripts/extensions/auto-tagger/src/auto-tagging.tsx
+++ scripts/extensions/auto-tagger/src/auto-tagging.tsx
@@ -172,6 +172,7 @@ export function getAutoTaggingComponent(superdesk: ISuperdesk, label: string) {
                 runAutomaticallyPreference: 'loading',
                 vocabularyLabels: null,
                 tentativeTagName: '',
+                forceRenderKey: Math.random(),
             };
 
             this._mounted = false;
