69dc07a4b30357a8d5e95c2cc1b557221c06d86e
Tomas Kikutis <t.kikutis@gmail.com>
2024-01-10 12:42:19 +0100

    update unit tests

--- scripts/apps/authoring/authoring/authoring.spec.ts
+++ scripts/apps/authoring/authoring/authoring.spec.ts
@@ -1,8 +1,9 @@
 /* eslint-disable max-len */
 /* tslint:disable:max-line-length */
 import {CustomEditor3Entity} from 'core/editor3/constants';
-import {onChange} from 'core/editor3/store/index';
-import {convertFromRaw, RawDraftContentState} from 'draft-js';
+import {generateAnnotations, syncAssociations} from 'core/editor3/store/index';
+import {RawDraftContentState} from 'draft-js';
+import {IArticle} from 'superdesk-api';
 import {AuthoringWorkspaceService} from './services/AuthoringWorkspaceService';
 
 describe('authoring', () => {
@@ -69,38 +70,30 @@ describe('authoring', () => {
             expect(scope.item.anpa_category[0].scheme).toBeUndefined();
         }));
 
-    it('generates annotations field from the editor state', inject((authoring) => {
-        var contentStateRaw = {blocks: [{key: 'ak77o', text: 'test', type: 'unstyled', depth: 0, inlineStyleRanges: [{offset: 1, length: 2, style: 'ANNOTATION-1'}], entityRanges: [], data: {MULTIPLE_HIGHLIGHTS: {highlightsData: {'ANNOTATION-1': {data: {msg: '{"blocks":[{"key":"7f13c","text":"123","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}],"entityMap":{}}', email: 'a@a.com', annotationType: 'regular:001', authorId: '59c222237300870c9a7863b2', date: '2018-05-21T04:14:45.074Z', author: 'Andrew Powers', avatar: 'https://sd-master.test.superdesk.org/api/upload-raw/2018022710028/e5bc36e933881f6ad79962a085844b8616aecea0a87908db20033ac129d88cf2.jpg'}, type: 'ANNOTATION'}}, lastHighlightIds: {SPLIT_PARAGRAPH_SUGGESTION: 0, DELETE_SUGGESTION: 0, TOGGLE_BOLD_SUGGESTION: 0, MERGE_PARAGRAPHS_SUGGESTION: 0, ADD_SUGGESTION: 0, ADD_LINK_SUGGESTION: 0, ANNOTATION: 1, COMMENT: 0, BLOCK_STYLE_SUGGESTION: 0, TOGGLE_SUPERSCRIPT_SUGGESTION: 0, TOGGLE_SUBSCRIPT_SUGGESTION: 0, TOGGLE_UNDERLINE_SUGGESTION: 0, TOGGLE_STRIKETHROUGH_SUGGESTION: 0, CHANGE_LINK_SUGGESTION: 0, TOGGLE_ITALIC_SUGGESTION: 0, REMOVE_LINK_SUGGESTION: 0}, highlightsStyleMap: {'ANNOTATION-1': {borderBottom: '4px solid rgba(100, 205, 0, 0.6)'}}}, __PUBLIC_API__comments: []}}], entityMap: {}};
-        var contentState = convertFromRaw(contentStateRaw as RawDraftContentState);
+    it('generates annotations field from the editor state', () => {
+        var contentStateRaw: RawDraftContentState = {
+            blocks: [{key: 'ak77o', text: 'test', type: 'unstyled', depth: 0, inlineStyleRanges: [{offset: 1, length: 2, style: 'ANNOTATION-1'}], entityRanges: [], data: {MULTIPLE_HIGHLIGHTS: {highlightsData: {'ANNOTATION-1': {data: {msg: '{"blocks":[{"key":"7f13c","text":"123","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}],"entityMap":{}}', email: 'a@a.com', annotationType: 'regular:001', authorId: '59c222237300870c9a7863b2', date: '2018-05-21T04:14:45.074Z', author: 'Andrew Powers', avatar: 'https://sd-master.test.superdesk.org/api/upload-raw/2018022710028/e5bc36e933881f6ad79962a085844b8616aecea0a87908db20033ac129d88cf2.jpg'}, type: 'ANNOTATION'}}, lastHighlightIds: {SPLIT_PARAGRAPH_SUGGESTION: 0, DELETE_SUGGESTION: 0, TOGGLE_BOLD_SUGGESTION: 0, MERGE_PARAGRAPHS_SUGGESTION: 0, ADD_SUGGESTION: 0, ADD_LINK_SUGGESTION: 0, ANNOTATION: 1, COMMENT: 0, BLOCK_STYLE_SUGGESTION: 0, TOGGLE_SUPERSCRIPT_SUGGESTION: 0, TOGGLE_SUBSCRIPT_SUGGESTION: 0, TOGGLE_UNDERLINE_SUGGESTION: 0, TOGGLE_STRIKETHROUGH_SUGGESTION: 0, CHANGE_LINK_SUGGESTION: 0, TOGGLE_ITALIC_SUGGESTION: 0, REMOVE_LINK_SUGGESTION: 0}, highlightsStyleMap: {'ANNOTATION-1': {borderBottom: '4px solid rgba(100, 205, 0, 0.6)'}}}, __PUBLIC_API__comments: []}}], entityMap: {},
+        } as unknown as RawDraftContentState; // without casting it doesn't compile due to our custom style (ANNOTATION-1)
 
-        var context: any = {
-            pathToValue: 'body_html',
-            item: {
-                body_html: '<p>t<span annotation-id="1">es</span>t</p>',
-                fields_meta: {
-                    body_html: {
-                        draftjsState: [contentStateRaw],
-                    },
-                },
-            },
-            $rootScope: {
-                $applyAsync: () => {
-                    expect(JSON.stringify(context.item.annotations))
-                        .toEqual('[{"id":1,"type":"regular:001","body":"<p>123</p>"}]');
+        const item: Partial<IArticle> = {
+            body_html: '<p>t<span annotation-id="1">es</span>t</p>',
+            fields_meta: {
+                body_html: {
+                    draftjsState: [contentStateRaw],
                 },
             },
         };
 
-        spyOn(context.$rootScope, '$applyAsync').and.callThrough();
-        onChange.call(context, contentState);
-        expect(context.$rootScope.$applyAsync).toHaveBeenCalled();
-    }));
+        generateAnnotations(item);
+
+        expect(JSON.stringify(item.annotations)).toEqual('[{"id":1,"type":"regular:001","body":"<p>123</p>"}]');
+    });
 
     it('synchronizes associations field from editor state', inject(() => {
         const entity1 = {guid: 'entity1', renditions: {original: {href: 'foo'}}};
         const entity2 = {guid: 'entity2', renditions: {original: {href: 'bar'}}};
 
-        const contentStateRaw = {
+        const contentStateRaw: RawDraftContentState = {
             blocks: [
                 {
                     key: 'aaaa1', text: 'test', type: 'unstyled', depth: 0, inlineStyleRanges: [], entityRanges: [],
@@ -137,33 +130,31 @@ describe('authoring', () => {
                 },
             },
         };
-        const contentState = convertFromRaw(contentStateRaw as RawDraftContentState);
 
-        const context = {
-            pathToValue: 'body_html',
-            item: {
-                body_html: '<p>foo</p>',
-                associations: {
-                    foo: {foo: 1},
-                    editor_1: {guid: 'existing assoc'},
-                    editor_2: {guid: 'some old association'},
-                },
-                fields_meta: {
-                    body_html: {
-                        draftjsState: [contentStateRaw],
-                    },
+        const association1: IArticle = {guid: 'foo'} as IArticle;
+        const association2: IArticle = {guid: 'existing assoc'} as IArticle;
+        const association3: IArticle = {guid: 'some old association'} as IArticle;
+
+        const item: IArticle = {
+            body_html: '<p>foo</p>',
+            associations: {
+                foo: association1,
+                editor_1: association2,
+                editor_2: association3,
+            },
+            fields_meta: {
+                body_html: {
+                    draftjsState: [contentStateRaw],
                 },
             },
-            $rootScope: {
-                $applyAsync: () => null,
-            },
-        };
+        } as unknown as IArticle;
 
-        onChange.call(context, contentState);
-        expect(context.item.associations.foo).toEqual({foo: 1});
-        expect(context.item.associations['editor_0']).toEqual(entity1);
-        expect(context.item.associations.editor_1).toEqual(entity2);
-        expect(context.item.associations.editor_2).toBe(null);
+        syncAssociations(item, contentStateRaw);
+
+        expect(item.associations.foo).toEqual(association1);
+        expect(item.associations['editor_0']).toEqual(entity1);
+        expect(item.associations['editor_1']).toEqual(entity2);
+        expect(item.associations['editor_2']).toEqual(null);
     }));
 
     describe('authoring workspace', () => {
