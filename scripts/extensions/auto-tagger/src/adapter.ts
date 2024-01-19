b98998772c5a5cffdb6df7035f8c00d230e52846
cmuldur <clm@cp.org>
2024-01-19 10:02:13 -0500

    Update parent property in ISubjectTag interface

--- scripts/extensions/auto-tagger/src/adapter.ts
+++ scripts/extensions/auto-tagger/src/adapter.ts
@@ -15,7 +15,7 @@ export interface ITagBase {
 }
 
 export interface ISubjectTag extends ITagBase {
-    parent?: string;
+    parent?: string | null;
 }
 
 export interface IServerResponse {
@@ -158,7 +158,7 @@ export function toServerFormat(items: OrderedMap<string, ITagUi>, superdesk: ISu
                 qcode,
                 source,
                 altids,
-                parent,
+                parent: parent ?? undefined,
                 scheme: item.group.value,
                 aliases,
                 original_source,
