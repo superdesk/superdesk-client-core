5df3c742024c2bc7f9e62949540b35ea5c685638
Tomas Kikutis <t.kikutis@gmail.com>
2024-02-05 13:36:21 +0100

    add waiting limit

--- e2e/client/specs/helpers/authoring.ts
+++ e2e/client/specs/helpers/authoring.ts
@@ -452,6 +452,7 @@ class Authoring {
             this.save_button.click();
             return browser.wait(
                 ECE.stalenessOf(this.save_button.element(by.css('[data-test-id="loading-indicator"]'))),
+                5000,
             );
         };
 
