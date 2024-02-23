ddcd8193703bd9b8a6279090592dd1bfaaf87532
cmuldur <clm@cp.org>
2024-02-23 13:20:45 -0500

    Add styles

--- scripts/extensions/translate-widget/src/translate-widget.tsx
+++ scripts/extensions/translate-widget/src/translate-widget.tsx
@@ -1,13 +1,20 @@
 import * as React from "react";
 import { ISuperdesk, IArticle } from "superdesk-api";
 import { Input, Button, RadioButtonGroup } from "superdesk-ui-framework/react";
+import "./TranslateWidget.css";
 
-export const getTranslateWidgetComponent = (superdesk: ISuperdesk, label: string) => {
+export const getTranslateWidgetComponent = (
+  superdesk: ISuperdesk,
+  label: string
+) => {
   console.log("Translate widget activated with label:", label);
   interface TranslatingWidgetProps {
     article: IArticle; // Ensure this matches the IArticle interface
   }
-  return class TranslatingWidget extends React.PureComponent<TranslatingWidgetProps, any> {
+  return class TranslatingWidget extends React.PureComponent<
+    TranslatingWidgetProps,
+    any
+  > {
     constructor(props: TranslatingWidgetProps) {
       console.log("superdesk:", superdesk);
       super(props);
@@ -19,84 +26,86 @@ export const getTranslateWidgetComponent = (superdesk: ISuperdesk, label: string
           <header className="header">
             <h1>Translate Widget</h1>
           </header>
-          <div className="input-column">
-            <section className="language-selection">
-              <RadioButtonGroup
-                group={{ groupLabel: "Input Languages" }}
-                options={[
-                  { value: "en", label: "English" },
-                  { value: "fr", label: "French" },
-                  { value: "sp", label: "Spanish" },
-                ]}
-                value="en"
-                onChange={() => false}
+          <div className="columns">
+            <div className="input-column">
+              <section className="language-selection">
+                <RadioButtonGroup
+                  group={{ groupLabel: "Input Languages" }}
+                  options={[
+                    { value: "en", label: "English" },
+                    { value: "fr", label: "French" },
+                    { value: "sp", label: "Spanish" },
+                  ]}
+                  value="en"
+                  onChange={() => false}
+                />
+              </section>
+              <section className="input-section">
+                <Input
+                  label="Headline"
+                  value={""}
+                  type="text"
+                  disabled={true}
+                  onChange={() => false}
+                />
+                <Input
+                  label="Extended Headline"
+                  value={""}
+                  type="text"
+                  disabled={true}
+                  onChange={() => false}
+                />
+                <Input
+                  label="Body HTML"
+                  value={""}
+                  type="text"
+                  disabled={true}
+                  onChange={() => false}
+                />
+              </section>
+              <Button
+                text="Translate Article"
+                type="primary"
+                onClick={() => false}
               />
-            </section>
-            <section className="input-section">
-              <Input
-                label="Headline"
-                value={""}
-                type="text"
-                disabled={true}
-                onChange={() => false}
-              />
-              <Input
-                label="Extended Headline"
-                value={""}
-                type="text"
-                disabled={true}
-                onChange={() => false}
-              />
-              <Input
-                label="Body HTML"
-                value={""}
-                type="text"
-                disabled={true}
-                onChange={() => false}
-              />
-            </section>
-            <Button
-              text="Translate Article"
-              type="primary"
-              onClick={() => false}
-            />
-          </div>
-          <div className="output-column">
-            <section className="language-selection">
-              <RadioButtonGroup
-                group={{ groupLabel: "Output Languages" }}
-                options={[
-                  { value: "en", label: "English" },
-                  { value: "fr", label: "French" },
-                  { value: "sp", label: "Spanish" },
-                ]}
-                value="fr"
-                onChange={() => false}
-              />
-            </section>
-            <section className="output-section">
-              <Input
-                label=" Translated Headline"
-                value={""}
-                type="text"
-                disabled={true}
-                onChange={() => false}
-              />
-              <Input
-                label="Translated Extended Headline"
-                value={""}
-                type="text"
-                disabled={true}
-                onChange={() => false}
-              />
-              <Input
-                label="Translated Body HTML"
-                value={""}
-                type="text"
-                disabled={true}
-                onChange={() => false}
-              />
-            </section>
+            </div>
+            <div className="output-column">
+              <section className="language-selection">
+                <RadioButtonGroup
+                  group={{ groupLabel: "Output Languages" }}
+                  options={[
+                    { value: "en", label: "English" },
+                    { value: "fr", label: "French" },
+                    { value: "sp", label: "Spanish" },
+                  ]}
+                  value="fr"
+                  onChange={() => false}
+                />
+              </section>
+              <section className="output-section">
+                <Input
+                  label=" Translated Headline"
+                  value={""}
+                  type="text"
+                  disabled={true}
+                  onChange={() => false}
+                />
+                <Input
+                  label="Translated Extended Headline"
+                  value={""}
+                  type="text"
+                  disabled={true}
+                  onChange={() => false}
+                />
+                <Input
+                  label="Translated Body HTML"
+                  value={""}
+                  type="text"
+                  disabled={true}
+                  onChange={() => false}
+                />
+              </section>
+            </div>
           </div>
         </div>
       );
