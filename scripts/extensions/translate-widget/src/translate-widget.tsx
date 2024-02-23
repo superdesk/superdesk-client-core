6309df40a112e96497fb4f72308e2122f249618e
cmuldur <clm@cp.org>
2024-02-23 15:58:27 -0500

    Refactor translate-widget component layout

--- scripts/extensions/translate-widget/src/translate-widget.tsx
+++ scripts/extensions/translate-widget/src/translate-widget.tsx
@@ -21,12 +21,12 @@ export const getTranslateWidgetComponent = (
 
     render() {
       return (
-        <div className="translate-widget" style={{width: '50vw'}}>
+        <div className="translate-widget" style={{width: '90%', margin: '0 auto', height: '90vh'}}>
           <header className="header" style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', margin: '24px'}}>
             <h1 style={{margin: '0', fontSize: '24px', fontWeight: 600, color: '#333'}}>Translate Widget</h1>
           </header>
-          <div className="columns" style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginBottom: '32px'}}>
-            <div className="input-column" style={{flex: 1, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', padding: '10px', marginRight: '10px'}}>
+          <div className="columns" style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'flex-start', marginBottom: '32px', height: '90%'}}>
+            <div className="input-column" style={{flex: 1, display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'center', padding: '10px', gap: '32px', marginRight: '10px', height: '100%', borderRight: '1px solid #ddd'}}>
               <section className="language-selection">
                 <RadioButtonGroup
                   group={{ groupLabel: "Input Languages" }}
@@ -39,7 +39,7 @@ export const getTranslateWidgetComponent = (
                   onChange={() => false}
                 />
               </section>
-              <section className="input-section">
+              <section className="input-section" style={{display: 'flex', flexDirection: 'column', gap: '24px', width: '100%', height: '75vh', paddingLeft: '24px', paddingRight: '8px'}}>
                 <Input
                   label="Headline"
                   value={""}
@@ -68,7 +68,7 @@ export const getTranslateWidgetComponent = (
                 onClick={() => false}
               />
             </div>
-            <div className="output-column" style={{flex: 1, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', padding: '10px', marginLeft: '10px'}}>
+            <div className="output-column" style={{flex: 1, display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'center', padding: '10px', gap: '32px', marginLeft: '10px', height: '100%'}}>
               <section className="language-selection">
                 <RadioButtonGroup
                   group={{ groupLabel: "Output Languages" }}
@@ -81,7 +81,7 @@ export const getTranslateWidgetComponent = (
                   onChange={() => false}
                 />
               </section>
-              <section className="output-section">
+              <section className="output-section" style={{display: 'flex', flexDirection: 'column', gap: '24px', width: '100%', paddingLeft: '24px', paddingRight: '8px'}}>
                 <Input
                   label=" Translated Headline"
                   value={""}
