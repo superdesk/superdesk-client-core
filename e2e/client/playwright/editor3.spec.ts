71d582b58644f7cd3904a1968d79b717944aa289
Konstantin Markov <maga.ot.oz@protonmail.com>
2024-03-05 15:56:55 +0200

    Further improvements

--- e2e/client/playwright/editor3.spec.ts
+++ e2e/client/playwright/editor3.spec.ts
@@ -1,12 +1,31 @@
 import {test, expect} from '@playwright/test';
 import {Monitoring} from './page-object-models/monitoring';
 import {restoreDatabaseSnapshot, s} from './utils';
+import {getEmbedObject} from 'core/editor3/components/embeds/EmbedInput';
+import {appConfig} from 'scripts/appConfig';
 
 test('can add embeds', async ({page}) => {
     await restoreDatabaseSnapshot();
 
     const monitoring = new Monitoring(page);
 
+    const requestRoute = 'https://sourcefabric.org';
+
+    await page.route(
+        `https://iframe.ly/api/oembed?callback=?&url=
+        ${requestRoute}
+        &api_key=${appConfig.iframely.key}
+        &omit_script=true&iframe=true`,
+        (route) => {
+            route.fulfill({
+                body: JSON.stringify([{
+                    title: 'Open Source Software for Journalism',
+                    // eslint-disable-next-line max-len
+                    description: 'Sourcefabric is Europe\'s largest developer of open source tools for news media, powering news and media organisations around the world.',
+                }]),
+            });
+        },
+    );
     await page.goto('/#/workspace/monitoring');
 
     await monitoring.selectDesk('Sports');
@@ -24,5 +43,7 @@ test('can add embeds', async ({page}) => {
     await page.getByPlaceholder('Enter URL or code to embed').type('https://sourcefabric.org');
 
     await page.locator(s('embed-controls', 'submit')).click();
-    await expect(page.getByText('https://sourcefabric.org')).toBeDefined();
+    await expect(
+        page.locator(s('authoring', 'authoring-field=body_html')).getByText('https://sourcefabric.org'),
+    ).toBeDefined();
 });
