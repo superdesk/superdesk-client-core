280d796cb95dd0801998e325fda1200a9f9f8a67
Tomas Kikutis <t.kikutis@gmail.com>
2024-04-01 14:00:43 +0200

    make editor3 generate html before saving in multi edit mode

--- scripts/apps/authoring/multiedit/multiedit.ts
+++ scripts/apps/authoring/multiedit/multiedit.ts
@@ -182,8 +182,7 @@ function MultieditArticleDirective(authoring, content, multiEdit, lock, $timeout
         templateUrl: 'scripts/apps/authoring/multiedit/views/sd-multiedit-article.html',
         scope: {article: '=', focus: '='},
         link: function(scope, elem) {
-            const MEDIA_TYPES = ['video', 'picture', 'audio'];
-            var mediaFields = {};
+            scope.requestEditor3DirectivesToGenerateHtml = [];
 
             scope.$watch('article', (newVal, oldVal) => {
                 if (newVal && newVal !== oldVal) {
@@ -233,6 +232,10 @@ function MultieditArticleDirective(authoring, content, multiEdit, lock, $timeout
             }, true);
 
             scope.save = function() {
+                for (const fn of scope.requestEditor3DirectivesToGenerateHtml) {
+                    fn();
+                }
+
                 return authoring.save(scope.origItem, cloneDeep(scope.item)).then((res) => {
                     scope.dirty = false;
                     InitializeMedia.initMedia(scope);
