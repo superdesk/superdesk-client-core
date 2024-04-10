f51bd53e69eb1ad02c93f4c49b7680f1deb7f1bb
Tomas Kikutis <t.kikutis@gmail.com>
2024-04-10 13:24:35 +0200

    improve article embedding (#4471)
    Co-authored-by: Petr Jasek <jasekpetr@gmail.com>

--- scripts/core/editor3/components/article-embed/article-embed.tsx
+++ scripts/core/editor3/components/article-embed/article-embed.tsx
@@ -4,7 +4,7 @@ import {ContentBlock, ContentState, EditorState} from 'draft-js';
 import {IEditorDragDropArticleEmbed} from 'core/editor3/reducers/editor3';
 import {openArticle} from 'core/get-superdesk-api-implementation';
 import {Card} from 'core/ui/components/Card';
-import {gettext} from 'core/utils';
+import {getArticleLabel, gettext} from 'core/utils';
 
 interface IProps {
     block: ContentBlock;
@@ -18,14 +18,14 @@ export class ArticleEmbed extends React.Component<IProps> {
         const {block, contentState} = this.props;
         const entityKey = block.getEntityAt(0);
         const entity = contentState.getEntity(entityKey);
-        const {id, name, html} = entity.getData() as IEditorDragDropArticleEmbed['data'];
+        const {item} = entity.getData() as IEditorDragDropArticleEmbed['data'];
 
         const heading = (
             <Spacer h gap="32" justifyContent="space-between" noWrap>
                 <div>
                     {gettext('Embedding from:')}
                     &nbsp;
-                    <strong>{name}</strong>
+                    <strong>{getArticleLabel(item)}</strong>
                 </div>
 
                 <div style={{flexShrink: 0}}>
@@ -34,7 +34,7 @@ export class ArticleEmbed extends React.Component<IProps> {
                         ariaValue={gettext('open in a new window')}
                         size="small"
                         onClick={() => {
-                            openArticle(id, 'edit-new-window');
+                            openArticle(item._id, 'edit-new-window');
                         }}
                     />
                 </div>
@@ -44,7 +44,7 @@ export class ArticleEmbed extends React.Component<IProps> {
         return (
             <Card heading={heading} width="100%">
                 <div className="article-embed">
-                    <div dangerouslySetInnerHTML={{__html: html}} />
+                    <div dangerouslySetInnerHTML={{__html: item.body_html}} />
                 </div>
             </Card>
 
