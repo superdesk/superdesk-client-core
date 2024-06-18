f2f7c80d5954a6a1cd676475fd9a0fd6d394b38f
Konstantin Markov <maga.ot.oz@protonmail.com>
2024-06-18 13:47:35 +0300

    Fix lint

--- scripts/apps/dashboard/widget-react.tsx
+++ scripts/apps/dashboard/widget-react.tsx
@@ -17,7 +17,7 @@ interface IState {
 
 export class WidgetReact extends React.PureComponent<IProps, IState> {
     render() {
-        const Component = this.props.widget.active?.component ?? this.props.widget?.pinnedWidget?.component,;
+        const Component = this.props.widget.active?.component ?? this.props.widget?.pinnedWidget?.component;
 
         // Ensure that widget component re-mounts if the item is locked/unlocked.
         // Avoid null key in case item is unlocked - use a random string to force it to re-mount.
