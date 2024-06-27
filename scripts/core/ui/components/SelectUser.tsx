89d683b5687f00d08c2f3275cd5320fd8b6ae1eb
Konstantin Markov <maga.ot.oz@protonmail.com>
2024-06-27 13:48:57 +0300

    Update user select (#4562)

--- scripts/core/ui/components/SelectUser.tsx
+++ scripts/core/ui/components/SelectUser.tsx
@@ -113,6 +113,7 @@ export class SelectUser extends SuperdeskReactComponent<IPropsSelectUser, IState
 
         return (
             <SelectWithTemplate
+                key={this.props.deskId}
                 label={gettext('Select a user')}
                 inlineLabel={true}
                 labelHidden={true}
