08a24ca475d9828c1f1824b619528b4dc50f2c85
Konstantin Markov <maga.ot.oz@protonmail.com>
2024-07-22 09:48:13 +0300

    Fix lint

--- scripts/apps/users/directives/UserPreferencesDirective.ts
+++ scripts/apps/users/directives/UserPreferencesDirective.ts
@@ -67,7 +67,7 @@ export function UserPreferencesDirective(
                         }
                     }
                 }
-            }
+            };
 
             scope.buildNotificationsFromExtensions();
 
