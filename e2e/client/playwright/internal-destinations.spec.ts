fa178f9e67adca4b1a61c9d6f5b134d7e759658f
Konstantin Markov <maga.ot.oz@protonmail.com>
2024-09-04 16:44:54 +0300

    Fix removal

--- e2e/client/playwright/internal-destinations.spec.ts
+++ e2e/client/playwright/internal-destinations.spec.ts
@@ -1,24 +1,26 @@
 import {test, expect} from '@playwright/test';
 import {restoreDatabaseSnapshot, s} from './utils';
-import {login} from 'end-to-end-testing-helpers';
+import {Monitoring} from './page-object-models/monitoring';
 
 test.describe('internal destinations', async () => {
     test('display and removal of active filters', async ({page}) => {
-        await login('admin', 'admin');
+        new Monitoring(page);
+        await restoreDatabaseSnapshot();
         await restoreDatabaseSnapshot();
         await page.goto('/#/settings/internal-destinations');
 
         await expect(page.locator(s('list-page--filters-active', 'tag-label'))).toHaveCount(0);
-        await page.locator(s('toggle-filters')).click();
 
+        await page.locator(s('toggle-filters')).click();
         await page.locator(s('list-page--filters-form', 'gform-input--desk')).click();
+
+        // FIXME: Scroll so element doesn't collide with form button. scrollIntoViewIfNeeded doesn't work.
+        // setting zindex 99999 to select2 component doesn't work as well.
         await page.locator(s('list-page--filters-form', 'gform-input--desk')).getByRole('button', {name: 'Sports'}).click();
         await page.locator(s('list-page--filters-form', 'filters-submit')).click();
         await expect(page.locator(s('list-page--filters-active', 'tag-label'))).toHaveCount(1);
 
-        const activeFilter = page.locator(s('list-page--filters-active', 'tag-label')).first();
-
-        await page.getByText(await activeFilter.textContent()).click();
+        await page.locator(s('list-page--filters-active', 'tag-label', 'tag-label--remove')).click();
         await expect(page.locator(s('list-page--filters-active', 'tag-label'))).toHaveCount(0);
     });
 })
