1b4dcf57ad6d58f767d2b7e3de9d7199cffe3547
Konstantin Markov <maga.ot.oz@protonmail.com>
2024-09-05 10:35:51 +0300

    Code refactoring

--- e2e/client/playwright/internal-destinations.spec.ts
+++ e2e/client/playwright/internal-destinations.spec.ts
@@ -6,7 +6,6 @@ test.describe('internal destinations', async () => {
     test('display and removal of active filters', async ({page}) => {
         new Monitoring(page);
         await restoreDatabaseSnapshot();
-        await restoreDatabaseSnapshot();
         await page.goto('/#/settings/internal-destinations');
 
         await expect(page.locator(s('list-page--filters-active', 'tag-label'))).toHaveCount(0);
@@ -16,7 +15,8 @@ test.describe('internal destinations', async () => {
 
         // FIXME: Scroll so element doesn't collide with form button. scrollIntoViewIfNeeded doesn't work.
         // setting zindex 99999 to select2 component doesn't work as well.
-        await page.locator(s('list-page--filters-form', 'gform-input--desk')).getByRole('button', {name: 'Sports'}).click();
+        await page.locator(s('list-page--filters-form', 'gform-input--desk'))
+            .getByRole('button', {name: 'Sports'}).click();
         await page.locator(s('list-page--filters-form', 'filters-submit')).click();
         await expect(page.locator(s('list-page--filters-active', 'tag-label'))).toHaveCount(1);
 
