07d0ed27b65f228fe3d25029a9dfec646d34c9ac
Konstantin Markov <maga.ot.oz@protonmail.com>
2024-09-05 12:27:27 +0300

    Fix test

--- e2e/client/playwright/internal-destinations.spec.ts
+++ e2e/client/playwright/internal-destinations.spec.ts
@@ -1,11 +1,9 @@
 import {test, expect} from '@playwright/test';
-import {restoreDatabaseSnapshot, s} from './utils';
-import {Monitoring} from './page-object-models/monitoring';
+import {login, s} from './utils';
 
 test.describe('internal destinations', async () => {
     test('display and removal of active filters', async ({page}) => {
-        new Monitoring(page);
-        await restoreDatabaseSnapshot();
+        await login(page);
         await page.goto('/#/settings/internal-destinations');
 
         await expect(page.locator(s('list-page--filters-active', 'tag-label'))).toHaveCount(0);
@@ -13,10 +11,10 @@ test.describe('internal destinations', async () => {
         await page.locator(s('toggle-filters')).click();
         await page.locator(s('list-page--filters-form', 'gform-input--desk')).click();
 
-        // FIXME: Scroll so element doesn't collide with form button. scrollIntoViewIfNeeded doesn't work.
-        // setting zindex 99999 to select2 component doesn't work as well.
         await page.locator(s('list-page--filters-form', 'gform-input--desk'))
-            .getByRole('button', {name: 'Sports'}).click();
+            .getByRole('combobox').fill('desk1');
+        await page.locator(s('list-page--filters-form', 'gform-input--desk'))
+            .getByRole('button', {name: 'desk1'}).click();
         await page.locator(s('list-page--filters-form', 'filters-submit')).click();
         await expect(page.locator(s('list-page--filters-active', 'tag-label'))).toHaveCount(1);
 
