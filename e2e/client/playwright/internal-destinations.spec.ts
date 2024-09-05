792124447e880e5cf3d54a3a39995455633d6e59
Konstantin Markov <maga.ot.oz@protonmail.com>
2024-09-05 13:32:06 +0300

    Fix test

--- e2e/client/playwright/internal-destinations.spec.ts
+++ e2e/client/playwright/internal-destinations.spec.ts
@@ -9,12 +9,13 @@ test.describe('internal destinations', async () => {
         await expect(page.locator(s('list-page--filters-active', 'tag-label'))).toHaveCount(0);
 
         await page.locator(s('toggle-filters')).click();
-        await page.locator(s('list-page--filters-form', 'gform-input--desk')).click();
 
         await page.locator(s('list-page--filters-form', 'gform-input--desk'))
-            .getByRole('combobox').fill('desk1');
+            .getByRole('combobox').click();
         await page.locator(s('list-page--filters-form', 'gform-input--desk'))
-            .getByRole('button', {name: 'desk1'}).click();
+            .getByRole('combobox').fill('Sports');
+        await page.locator(s('list-page--filters-form', 'gform-input--desk'))
+            .getByRole('button', {name: 'Sports'}).click();
         await page.locator(s('list-page--filters-form', 'filters-submit')).click();
         await expect(page.locator(s('list-page--filters-active', 'tag-label'))).toHaveCount(1);
 
