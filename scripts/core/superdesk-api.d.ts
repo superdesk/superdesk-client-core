681357f676e5039a87adf1ef7a96c0f5d6aa4b40
Tomas Kikutis <t.kikutis@gmail.com>
2024-09-12 14:00:57 +0200

    use config to provide additional spellcheckers

--- scripts/core/superdesk-api.d.ts
+++ scripts/core/superdesk-api.d.ts
@@ -3430,6 +3430,11 @@ declare module 'superdesk-api' {
 
         userOnlineMinutes: number;
 
+        // e.g. {nl: 'leuven_dutch'}
+        spellcheckers?: {
+            [languageCode: string]: string;
+        };
+
         iMatricsFields: {
             entities: {
                 [key: string]: {
