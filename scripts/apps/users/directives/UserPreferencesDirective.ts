ee5a2510180177f13f532ced1cadada5cdd6751a
Nikola Stojanovic <68916411+dzonidoo@users.noreply.github.com>
2024-12-10 22:15:52 +0100

    Setting "Appearance" preferences in the user profile is not kept upon re-login (#4696)

--- scripts/apps/users/directives/UserPreferencesDirective.ts
+++ scripts/apps/users/directives/UserPreferencesDirective.ts
@@ -48,7 +48,7 @@ export function UserPreferencesDirective(
             const NOTIFICATIONS_KEY = 'notifications';
 
             scope.activeNavigation = null;
-            scope.activeTheme = localStorage.getItem('theme');
+            scope.activeTheme = '';
             const registeredNotifications: IExtensionActivationResult['contributions']['notifications'] = (() => {
                 const result = {};
 
@@ -79,6 +79,14 @@ export function UserPreferencesDirective(
 
                 scope.userPrefs.$setDirty();
                 scope.$applyAsync();
+            };
+
+            scope.toggleUiTheme = function(theme) {
+                scope.activeTheme = theme;
+                if (orig['application:theme'] == null) {
+                    orig['application:theme'] = {};
+                }
+                orig['application:theme']['theme'] = theme;
             };
 
             scope.toggleEmailNotification = function(notificationId: string) {
@@ -149,7 +157,6 @@ export function UserPreferencesDirective(
                             preferencesService.desktopNotification.requestPermission();
                         }
 
-                        localStorage.setItem('theme', scope.activeTheme);
                         body.attr('data-theme', scope.activeTheme);
                         notify.success(gettext('User preferences saved'));
                         scope.cancel();
@@ -281,6 +288,7 @@ export function UserPreferencesDirective(
                     }
                 });
 
+                scope.activeTheme = data['application:theme']?.['theme'] ?? 'light-ui';
                 // metadata service initialization is needed if its
                 // values object is undefined or any of the needed
                 // data buckets are missing in it
