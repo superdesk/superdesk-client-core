64811610f56a769e0e1132964c6d8b3752a2414e
Tomas Kikutis <t.kikutis@gmail.com>
2024-12-19 19:47:36 +0100

    it can now extract translations from filters

--- grep-lint.py
+++ grep-lint.py
@@ -14,11 +14,6 @@ def get_command(branch=None):
 
 rules_to_check = [
     {
-        'name': 'Do not use `translate` filter anymore. Translation strings can\'t be extracted when the filter is used.',
-        'perl_regex': '\|\s*?translate',
-        'tolerance': True
-    },
-    {
         'name': 'Do not use angularjs for views anymore. Use React components and use `reactToAngular1` if you need to use React components inside existing angular templates.',
 
         # must match:
