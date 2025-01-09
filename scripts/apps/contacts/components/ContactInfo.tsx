1ddf3dc8423b7dea62625ef870f3a1639d58b63b
Konstantin Markov <maga.ot.oz@protonmail.com>
2025-01-09 19:53:03 +0200

    Rename props

--- scripts/apps/contacts/components/ContactInfo.tsx
+++ scripts/apps/contacts/components/ContactInfo.tsx
@@ -8,14 +8,18 @@ import {ContentDivider, Icon, Spacer} from 'superdesk-ui-framework/react';
 
 interface IProps {
     item: IContact;
-    labelInactive?: string;
-    isActive?: boolean;
+    showIfContactIsInactive?: string;
+    hideHeader?: boolean;
 }
 
 /**
  * Media Contact Info - renders contact's information
  */
-export const ContactInfo: React.FunctionComponent<IProps> = ({item, labelInactive, isActive}) => {
+export const ContactInfo: React.FunctionComponent<IProps> = ({
+    item,
+    showIfContactIsInactive,
+    hideHeader,
+}) => {
     const meta = [];
     const info = [];
 
@@ -23,7 +27,7 @@ export const ContactInfo: React.FunctionComponent<IProps> = ({item, labelInactiv
     const contactOrg = item.first_name && item.organisation ?
         <span>{item.organisation}</span> : null;
 
-    if (isActive != true) {
+    if (hideHeader != true) {
         info.push(
             <Spacer style={{margin: '8px 0'}} h gap="8" justifyContent="start" alignItems="center" noWrap>
                 <div
@@ -45,7 +49,7 @@ export const ContactInfo: React.FunctionComponent<IProps> = ({item, labelInactiv
                 </h3>
                 <div key="contact-org">
                     <span className="item-info">{contactOrg}</span>
-                    {!item.is_active && labelInactive && (
+                    {!item.is_active && showIfContactIsInactive && (
                         <span
                             title="inactive"
                             className="label label--draft label--hollow pull-right"
