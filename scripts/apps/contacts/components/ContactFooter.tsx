e19ecaf9b4e1822704b5a6176d4be72c74bb5417
Konstantin Markov <maga.ot.oz@protonmail.com>
2025-01-09 11:53:07 +0200

    Fix social footer

--- scripts/apps/contacts/components/ContactFooter.tsx
+++ scripts/apps/contacts/components/ContactFooter.tsx
@@ -13,7 +13,7 @@ interface IProps {
  */
 export const ContactFooter: React.FunctionComponent<IProps> = ({item}) => {
     return (
-        <Spacer style={{padding: 8}} h gap="4" justifyContent="start" alignItems="center">
+        <Spacer style={{padding: 8}} h gap="4" justifyContent="start" alignItems="center" noGrow>
             <Spacer h gap="4" justifyContent="start" alignItems="center" noWrap>
                 <span className="sd-grid-item__text-label">{gettext('Updated:')}</span>
                 <DateTime dateTime={item._updated} />
