b39b48ba920efa63248861a6277cf42376f431c3
dzonidoo <nikola71092@gmail.com>
2025-01-28 13:07:04 +0100

    fix lint

--- scripts/apps/authoring-react/toolbar/multi-edit-toolbar-action.tsx
+++ scripts/apps/authoring-react/toolbar/multi-edit-toolbar-action.tsx
@@ -48,10 +48,9 @@ export class MultiEditToolbarAction extends React.Component<IProps, IState> {
                         getId={(item) => getArticleLabel(item)}
                         getLabel={(item) => getArticleLabel(item)}
                         getOptions={() => sdApi.article.getWorkQueueItems().filter((article) =>
-                                this.state.selectedArticles.map(({_id}) => _id)
+                            this.state.selectedArticles.map(({_id}) => _id)
                                 .includes(article._id) === false,
-                            ).map((item) => ({value: item})
-                        )}
+                        ).map((item) => ({value: item}))}
                     />
                     <Spacer h gap="8" justifyContent="end" noWrap>
                         <Button
