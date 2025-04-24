edfd82a9006b62c44852b9623a0326df03a76948
Tomas Kikutis <t.kikutis@gmail.com>
2025-04-24 09:43:08 +0200

    day preview test

--- e2e/client/playwright/availability-manager/settings.spec.ts
+++ e2e/client/playwright/availability-manager/settings.spec.ts
@@ -1,16 +1,34 @@
-import {test, expect} from '@playwright/test';
+import {test, expect, Page} from '@playwright/test';
 import {restoreDatabaseSnapshot, s} from '../utils';
 import {TreeSelectDriver} from '../utils/tree-select-driver';
 
-test.describe('availability manager settings', async () => {
-    test('setting availability to "available" for a single day', async ({page}) => {
-        await page.clock.setFixedTime(new Date('1970-02-15'));
+async function openAvailabilitySettings(page: Page) {
+    await page.clock.setFixedTime(new Date('1970-02-15'));
 
-        await restoreDatabaseSnapshot({snapshotName: 'availability-manager'});
-        await page.goto('/#/profile');
-        await page.locator(s('page-sections')).getByRole('button', {name: 'Availability'}).click();
+    await restoreDatabaseSnapshot({snapshotName: 'availability-manager'});
+    await page.goto('/#/profile');
+    await page.locator(s('page-sections')).getByRole('button', {name: 'Availability'}).click();
+}
+
+test.describe('availability manager settings', async () => {
+    test('main view', async ({page}) => {
+        await openAvailabilitySettings(page);
 
         await expect(page.locator(s('availability-settings'))).toHaveScreenshot();
+    });
+
+    test('day preview', async ({page}) => {
+        await openAvailabilitySettings(page);
+
+        page.locator(s('month=mar')).getByRole('button', {name: '10'}).click();
+
+        await expect(page.locator(s('working-day-view', 'tags', 'tag'))).toHaveText(['Austria']);
+        await expect(page.locator(s('working-day-view'))).toContainText('Tuesday, March 10, 1970');
+        await expect(page.locator(s('working-day-view'))).toHaveScreenshot();
+    });
+
+    test('setting availability to "available" for a single day', async ({page}) => {
+        await openAvailabilitySettings(page);
 
         const feb15 = page.locator(s('month=feb')).getByRole('button', {name: '15'});
 
