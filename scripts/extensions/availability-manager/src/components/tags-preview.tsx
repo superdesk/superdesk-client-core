2ae57538cc8de18bde5e414127469170761fac0d
Tomas Kikutis <t.kikutis@gmail.com>
2025-04-29 12:19:09 +0200

    add justifyContent prop to tags preview

--- scripts/extensions/availability-manager/src/components/tags-preview.tsx
+++ scripts/extensions/availability-manager/src/components/tags-preview.tsx
@@ -3,11 +3,16 @@ import {keyBy} from 'lodash';
 import {TAGS_VOCABULARY_ID} from '../constants';
 import {superdesk} from '../superdesk';
 import {Label} from 'superdesk-ui-framework/react';
-import {Spacer} from '@sourcefabric/common';
+import {IPropsSpacer, Spacer} from '@sourcefabric/common';
 
 const {getVocabularyItemNameTranslated} = superdesk.entities.vocabulary;
 
-export function TagsPreview(props: {tags?: Array<{code: string}>}) {
+interface IProps {
+    tags?: Array<{code: string}>;
+    justifyContent?: IPropsSpacer['justifyContent']; // defaults to start
+}
+
+export function TagsPreview(props: IProps) {
     const {tags} = props;
     const tagsById = keyBy(
         superdesk.entities.vocabulary.getAll().get(TAGS_VOCABULARY_ID).items,
@@ -23,7 +28,7 @@ export function TagsPreview(props: {tags?: Array<{code: string}>}) {
             h
             gap="4"
             noWrap
-            justifyContent="start"
+            justifyContent={props.justifyContent ?? 'start'}
             style={{
                 maxWidth: 300,
                 flexWrap: 'wrap',
