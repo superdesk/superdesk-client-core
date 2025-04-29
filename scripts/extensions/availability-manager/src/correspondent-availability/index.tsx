b20a576f45f36bc82e7a45246d8bb8484a6d2550
Tomas Kikutis <t.kikutis@gmail.com>
2025-04-29 12:19:31 +0200

    ListView component

--- scripts/extensions/availability-manager/src/correspondent-availability/index.tsx
+++ scripts/extensions/availability-manager/src/correspondent-availability/index.tsx
@@ -1,19 +1,16 @@
 import * as React from 'react';
 import {nameof} from '@sourcefabric/common';
-import {IPage, ISuperdeskQuery, IUser} from 'superdesk-api';
+import {IPage, ISuperdeskQuery} from 'superdesk-api';
 import {IAvailabilityRecord} from '../interfaces';
 import {superdesk} from '../superdesk';
-import {sortAvailabilityRecords} from './sort-availability-records';
+import {ListView} from './list-view';
 
-const {gettext} = superdesk.localization;
-const {UserAvatar} = superdesk.components;
 const WithAvailabilityRecordsQuery = superdesk.components.getLiveQueryHOC<IAvailabilityRecord>();
 
 type IProps = React.ComponentProps<IPage['component']>;
 
 export class CorrespondentAvailability extends React.PureComponent<IProps> {
     render() {
-        const users: {[key: string]: IUser} = superdesk.entities.users.getAllUsers();
         const date = '2025-04-24';
 
         const query: ISuperdeskQuery = {
@@ -29,25 +26,11 @@ export class CorrespondentAvailability extends React.PureComponent<IProps> {
         }
 
         return (
-            <div>
-                <div>
-                    {gettext('Correspondent availability')}
-                </div>
-
-                <WithAvailabilityRecordsQuery resource="user_availability" query={query}>
-                    {(res) => sortAvailabilityRecords(res._items).map((item, i) => {
-                        const user = users[item.user];
-
-                        return (
-                            <div key={i}>
-                                <UserAvatar userId={item.user} />
-                                {user.display_name}
-                                {user.sign_off}
-                            </div>
-                        );
-                    })}
-                </WithAvailabilityRecordsQuery>
-            </div>
+            <WithAvailabilityRecordsQuery resource="user_availability" query={query}>
+                {(res) => (
+                    <ListView items={res._items} />
+                )}
+            </WithAvailabilityRecordsQuery>
         );
     }
 }
