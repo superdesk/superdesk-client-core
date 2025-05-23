db005baaddaea11cacae519e3c25f024e0776fa2
Tomas Kikutis <t.kikutis@gmail.com>
2025-05-23 11:48:35 +0200

    test not set records being shown when there are no other records

--- e2e/client/playwright/extensions/availability-manager/daily-view.spec.ts
+++ e2e/client/playwright/extensions/availability-manager/daily-view.spec.ts
@@ -41,3 +41,19 @@ test('filtering in daily view', async ({page}) => {
 
     await expect(page.locator(s('day-view'))).toHaveScreenshot();
 });
+
+test('not set records being shown when there are no other records in a day', async ({page}) => {
+    await restoreDatabaseSnapshot({snapshotName: 'availability-management'});
+    await page.clock.setFixedTime(new Date('1970-02-08'));
+
+    await page.goto('/#/availability-management');
+
+    await expect(page.locator(s('day-view'))).toHaveScreenshot();
+
+    await new TreeSelectDriver(
+        page,
+        page.locator(s('filters', 'status')),
+    ).setValues('Not set');
+
+    await expect(page.locator(s('day-view'))).toHaveScreenshot();
+});
--- e2e/client/playwright/extensions/availability-manager/weekly-view.spec.ts
+++ e2e/client/playwright/extensions/availability-manager/weekly-view.spec.ts
@@ -46,3 +46,21 @@ test('filtering in weekly view', async ({page}) => {
 
     await expect(page.locator(s('week-view'))).toHaveScreenshot();
 });
+
+test('not set records being shown when there are no other records in a week', async ({page}) => {
+    await restoreDatabaseSnapshot({snapshotName: 'availability-management'});
+    await page.clock.setFixedTime(new Date('1970-02-08'));
+
+    await page.goto('/#/availability-management');
+
+    await page.locator(s('filter-type', 'item=Week')).click();
+
+    await expect(page.locator(s('week-view'))).toHaveScreenshot();
+
+    await new TreeSelectDriver(
+        page,
+        page.locator(s('filters', 'status')),
+    ).setValues('Not set');
+
+    await expect(page.locator(s('week-view'))).toHaveScreenshot();
+});
