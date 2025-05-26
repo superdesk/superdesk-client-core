bd2d0a1f25605a0444fe3f22ce96dc4c616d9ce6
Tomas Kikutis <t.kikutis@gmail.com>
2025-05-26 13:31:57 +0200

    unused import

--- e2e/client/playwright/correct-item.spec.ts
+++ e2e/client/playwright/correct-item.spec.ts
@@ -1,6 +1,6 @@
 import {test, expect} from '@playwright/test';
 import {Monitoring} from './page-object-models/monitoring';
-import {restoreDatabaseSnapshot, s, sleep} from './utils';
+import {restoreDatabaseSnapshot, s} from './utils';
 
 test('can correct published item', async ({page}) => {
     const monitoring = new Monitoring(page);
