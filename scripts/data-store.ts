c2e5ce9a50b5ac354291f2db51afd92f54046391
Tomas Kikutis <t.kikutis@gmail.com>
2025-06-02 13:55:30 +0200

    Availability patch 1 (#4828)

--- scripts/data-store.ts
+++ scripts/data-store.ts
@@ -9,7 +9,7 @@ import {prepareSuperdeskQuery} from 'core/helpers/universal-query';
  */
 class DataStore {
     public contentProfiles: OrderedMap<IContentProfile['_id'], IContentProfile>;
-    public users: OrderedMap<IContentProfile['_id'], IContentProfile>;
+    public users: OrderedMap<IUser['_id'], IUser>;
 
     constructor() {
         this.contentProfiles = OrderedMap();
@@ -62,7 +62,7 @@ class DataStore {
                 sort: [{[sortKey]: 'asc'}],
             };
 
-            new DataProvider<IContentProfile>(
+            new DataProvider<IUser>(
                 () => {
                     const {path, urlParams} = prepareSuperdeskQuery('users', query);
 
