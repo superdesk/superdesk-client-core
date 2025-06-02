43e7f67a31bdbd996d653e03a4665f571c16f279
Tomas Kikutis <t.kikutis@gmail.com>
2025-06-02 11:57:46 +0200

    show a loading indicator for availability settings

--- scripts/extensions/availability-manager/src/settings/availability-settings.tsx
+++ scripts/extensions/availability-manager/src/settings/availability-settings.tsx
@@ -5,7 +5,7 @@ import * as ReactDOM from 'react-dom';
 import {addMonths, format, startOfMonth, endOfMonth} from 'date-fns';
 import {keyBy, range} from 'lodash';
 import {MonthCalendar, nameof, showModal, Spacer, SpacerBlock} from '@sourcefabric/common';
-import {Button, Card, Checkbox, getTextColor, IconButton, PopupPositioner} from 'superdesk-ui-framework/react';
+import {Button, Card, Checkbox, getTextColor, IconButton, Loader, PopupPositioner} from 'superdesk-ui-framework/react';
 import {ISuperdeskQuery, IUser} from 'superdesk-api';
 import {superdesk} from '../superdesk';
 import {IAvailabilityRecord, IAvailabilityRecordTemplate, IDefaultAvailability} from '../interfaces';
@@ -128,7 +128,7 @@ export class AvailabilitySettings extends React.PureComponent<IProps, IState> {
             <WithAvailabilityRecordsQuery resource="user_availability" query={query}>
                 {(res) => {
                     if (res.loading) {
-                        return null;
+                        return <Loader />;
                     }
 
                     const grouped = keyBy(res.data._items, (item) => item.date);
