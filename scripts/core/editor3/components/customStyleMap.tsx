45be92a08bb6b8683096da8db7727b51a0423b81
thecalcc <maga.ot.oz@protonmail.com>
2025-07-14 14:11:19 +0300

    Fix optional chaining

--- scripts/core/editor3/components/customStyleMap.tsx
+++ scripts/core/editor3/components/customStyleMap.tsx
@@ -16,7 +16,7 @@ const getUiFrameworkColor = (borderColor: typeof appConfig.authoring.customEdito
 
 export const customStyleMap = {
     ...Object.fromEntries(
-        (appConfig.authoring.customEditorTags ?? []).map(({id, borderColor}) => [
+        (appConfig.authoring?.customEditorTags ?? []).map(({id, borderColor}) => [
             id, {display: 'inline-block', borderBlockEnd: `2px double ${getUiFrameworkColor(borderColor)}`},
         ]),
     ),
