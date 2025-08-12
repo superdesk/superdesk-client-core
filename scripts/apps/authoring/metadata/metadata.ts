734939d7f717b30a7a974e71af8709a292c52afa
dzonidoo <nikola71092@gmail.com>
2025-08-12 11:27:07 +0200

    implement sort by qcode

--- scripts/apps/authoring/metadata/metadata.ts
+++ scripts/apps/authoring/metadata/metadata.ts
@@ -426,6 +426,8 @@ function MetaDropdownDirective($filter, metadata) {
                         scope.places = _.groupBy(scope.list, 'group');
                     } else if (scope.field === 'genre') {
                         scope.list = $filter('sortByName')(scope.list);
+                    } else if (scope.field === 'priority' || scope.field === 'urgency') {
+                        scope.list.sort((a, b) => a.qcode - b.qcode);
                     }
                 }
             });
