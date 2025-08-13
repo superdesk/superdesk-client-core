ba772903b7e05375dde14fb28c8b0b9e5d9489b8
dzonidoo <nikola71092@gmail.com>
2025-08-13 10:45:41 +0200

    cahnges after review part I

--- scripts/apps/authoring/metadata/metadata.ts
+++ scripts/apps/authoring/metadata/metadata.ts
@@ -427,7 +427,12 @@ function MetaDropdownDirective($filter, metadata) {
                     } else if (scope.field === 'genre') {
                         scope.list = $filter('sortByName')(scope.list);
                     } else if (scope.field === 'priority' || scope.field === 'urgency') {
-                        scope.list.sort((a, b) => a.qcode - b.qcode);
+                        scope.list.sort((a, b) => {
+                            const valA = a?.qcode ?? '';
+                            const valB = b?.qcode ?? '';
+
+                            return String(valA).localeCompare(String(valB));
+                        });
                     }
                 }
             });
