2deb7025ecf0db33c0bd76f2974bacdaa953edca
Konstantin Markov <maga.ot.oz@protonmail.com>
2025-09-02 15:08:06 +0300

    use ts-loader for @sourcefabric/common (#4933)

--- webpack.config.js
+++ webpack.config.js
@@ -232,6 +232,7 @@ module.exports = function makeConfig(grunt) {
 
                             // @sourcefabric/date-fns-tz uses logical OR assignment operator ||=
                             || absolutePath.includes('/@sourcefabric/date-fns-tz/')
+                            || absolutePath.includes('/@sourcefabric/common/')
                         ) {
                             return false;
                         }
