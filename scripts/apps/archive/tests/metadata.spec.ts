a1852b6f7a815aee4caf45eb098ad75d6efae9bf
Tomas Kikutis <t.kikutis@gmail.com>
2025-10-01 19:04:52 +0200

    drop timeout overwrites

--- scripts/apps/archive/tests/metadata.spec.ts
+++ scripts/apps/archive/tests/metadata.spec.ts
@@ -1,76 +1,66 @@
-import * as metadataFns from 'apps/archive/parse-metadata';
+import {getMetadata} from 'apps/archive/parse-metadata';
+
+const fetchFile = (filename: string): Promise<File> => {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+
+        xhr.open('GET', `/base/fixtures/${filename}`);
+        xhr.responseType = 'blob';
+        xhr.onload = () => {
+            if (xhr.status >= 200 && xhr.status < 300) {
+                const file = new File([xhr.response], filename, {
+                    type: xhr.getResponseHeader('Content-Type'),
+                });
+
+                resolve(file);
+            } else {
+                reject(new Error(`Failed to load file: ${xhr.status}`));
+            }
+        };
+        xhr.onerror = () => reject(new Error('Network error'));
+        xhr.send();
+    });
+};
+
+const exiftoolFetchPolyfill = (url: string): Promise<Response> => {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+
+        xhr.open('GET', url);
+        xhr.responseType = 'arraybuffer';
+
+        xhr.onload = () => {
+            if (xhr.status >= 200 && xhr.status < 300) {
+                const response = new Response(xhr.response, {
+                    status: xhr.status,
+                    statusText: xhr.statusText,
+                    headers: {
+                        'Content-Type': 'application/wasm',
+                    },
+                });
+
+                resolve(response);
+            } else {
+                reject(
+                    new Error(`Failed to fetch ${url}: ${xhr.status} ${xhr.statusText}`),
+                );
+            }
+        };
+
+        xhr.onerror = () =>
+            reject(new Error(`Network error while fetching ${url}`));
+        xhr.send();
+    });
+};
 
 describe('process item metadata', () => {
-    // first test needs a longer timeout to set up exiftool otherwise first test always fails
-    let originalTimeout;
-
-    beforeEach(() => {
-        originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
-        jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
-    });
-
-    afterEach(() => {
-        jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
-    });
-
-    const fetchFile = (filename: string): Promise<File> =>
-        new Promise((resolve, reject) => {
-            const xhr = new XMLHttpRequest();
-
-            xhr.open('GET', `/base/fixtures/${filename}`);
-            xhr.responseType = 'blob';
-            xhr.onload = () => {
-                if (xhr.status >= 200 && xhr.status < 300) {
-                    const file = new File([xhr.response], filename, {
-                        type: xhr.getResponseHeader('Content-Type'),
-                    });
-
-                    resolve(file);
-                } else {
-                    reject(new Error(`Failed to load file: ${xhr.status}`));
-                }
-            };
-            xhr.onerror = () => reject(new Error('Network error'));
-            xhr.send();
-        });
-
-    const exiftoolFetchPolyfill = (url: string): Promise<Response> =>
-        new Promise((resolve, reject) => {
-            const xhr = new XMLHttpRequest();
-
-            xhr.open('GET', url);
-            xhr.responseType = 'arraybuffer';
-
-            xhr.onload = () => {
-                if (xhr.status >= 200 && xhr.status < 300) {
-                    const response = new Response(xhr.response, {
-                        status: xhr.status,
-                        statusText: xhr.statusText,
-                        headers: {
-                            'Content-Type': 'application/wasm',
-                        },
-                    });
-
-                    resolve(response);
-                } else {
-                    reject(
-                        new Error(`Failed to fetch ${url}: ${xhr.status} ${xhr.statusText}`),
-                    );
-                }
-            };
-
-            xhr.onerror = () =>
-                reject(new Error(`Network error while fetching ${url}`));
-            xhr.send();
-        });
-
     it('image metadata', async () => {
         const expected = {
             Keywords: ['Keyword1ref2014', 'Keyword2ref2014', 'Keyword3ref2014'],
             'By-line': 'Creator1 (ref2014)',
         };
         const file = await fetchFile('metadata.jpg');
-        const result = await metadataFns.getMetadata(file, {
+        const result = await getMetadata(file, {
             fetch: exiftoolFetchPolyfill,
         });
 
@@ -83,7 +73,7 @@ describe('process item metadata', () => {
             'By-line': ['Your Creator Name 6', 'Your Creator Name 6'],
         };
         const file = await fetchFile('metadata.mov');
-        const result = await metadataFns.getMetadata(file, {
+        const result = await getMetadata(file, {
             fetch: exiftoolFetchPolyfill,
         });
 
@@ -93,7 +83,7 @@ describe('process item metadata', () => {
     it('{} for item with no metadata', async () => {
         for (const filename of ['empty_metadata.jpg', 'empty_metadata.mov']) {
             const file = await fetchFile(filename);
-            const result = await metadataFns.getMetadata(file, {
+            const result = await getMetadata(file, {
                 fetch: exiftoolFetchPolyfill,
             });
 
