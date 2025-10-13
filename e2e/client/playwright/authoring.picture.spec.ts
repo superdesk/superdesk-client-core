5afdb193297c63181dd2ee594dbe614c5160d535
Petr Jasek <jasekpetr@gmail.com>
2025-10-13 09:27:29 +0200

    use helper

--- e2e/client/playwright/authoring.picture.spec.ts
+++ e2e/client/playwright/authoring.picture.spec.ts
@@ -4,6 +4,7 @@ import {restoreDatabaseSnapshot} from './utils';
 import {MediaEditor} from './page-object-models/media-editor';
 import {PictureAuthoring} from './page-object-models/authoring';
 import {MediaUpload} from './page-object-models/upload';
+import {setEditor3FieldValue} from './utils/editor3';
 
 /**
  * upload a picture
@@ -27,10 +28,7 @@ test('media metadata editor', async ({page}) => {
 
     await expect(mediaEditor.field('field--headline')).toContainText('The Headline');
 
-    // workaround for flaky fill with plain text editor, it sometimes keeps previous text
-    await mediaEditor.field('field--headline').fill('');
-    await mediaEditor.field('field--headline').fill('');
-    await mediaEditor.field('field--headline').fill('picture');
+    await setEditor3FieldValue(mediaEditor.field('field--headline'), 'picture');
 
     await expect(mediaEditor.field('field--headline')).toHaveText('picture');
 
@@ -40,12 +38,13 @@ test('media metadata editor', async ({page}) => {
 
     await pictureAuthoring.openMetadataEditor();
 
-    await mediaEditor.field('field--description_text').fill('test description');
+    await setEditor3FieldValue(mediaEditor.field('field--description_text'), 'test description');
+
     await mediaEditor.saveMetadata();
 
     await expect(pictureAuthoring.field('field--description_text')).toContainText('test description');
 
-    await pictureAuthoring.field('field--description_text').fill('new description');
+    await setEditor3FieldValue(pictureAuthoring.field('field--description_text'), 'new description');
 
     await pictureAuthoring.openMetadataEditor();
 
