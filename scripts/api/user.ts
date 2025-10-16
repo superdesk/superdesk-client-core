0d6c39a771b28dc0997a5c1cd4811bc636930056
MarkLark86 <mark.pittaway@gmail.com>
2025-10-17 10:04:55 +1100

    [SDESK-7783] Add getUniqueClientId to SuperdeskApi (#4980)

--- scripts/api/user.ts
+++ scripts/api/user.ts
@@ -1,6 +1,7 @@
 import ng from 'core/services/ng';
 import {IUser} from 'superdesk-api';
 import {OrderedMap} from 'immutable';
+import {uuid} from 'core/helpers/uuid';
 
 function hasPrivilege(privilege: string): boolean {
     const privileges = ng.get('privileges');
@@ -24,9 +25,21 @@ function getAll(): OrderedMap<IUser['_id'], IUser> {
     return OrderedMap(ng.get('desks').users._items.map((user) => [user._id, user]));
 }
 
+function getUniqueClientId(): string {
+    let clientId: string | null = sessionStorage.getItem('clientId');
+
+    if (clientId == null) {
+        clientId = uuid();
+        sessionStorage.setItem('clientId', clientId);
+    }
+
+    return clientId;
+}
+
 export const user = {
     hasPrivilege,
     isLoggedIn,
     getCurrentUserId,
     getAll,
+    getUniqueClientId,
 };
