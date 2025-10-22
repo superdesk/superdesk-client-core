4556f165f3412e0a989b085409a3b3edd79983f2
Petr Jašek <jasekpetr@gmail.com>
2025-10-22 16:11:52 +0200

    drop raven (#4990)
    we're not using it anyhow

--- scripts/core/services/logger.ts
+++ scripts/core/services/logger.ts
@@ -1,58 +1,16 @@
-import Raven, {RavenOptions} from 'raven-js';
-
 class Logger {
-    constructor() {
-        /* globals __SUPERDESK_CONFIG__: true */
-        const appConfig = __SUPERDESK_CONFIG__;
-
-        if (appConfig.raven && appConfig.raven.dsn.length > 0) {
-            Raven.config(appConfig.raven.dsn, {
-                logger: 'javascript-client',
-                release: appConfig.version,
-            }).install();
-        }
-    }
-
     error(
         e: Error,
         additionalData?: any, // has to be serializable to JSON string
     ) {
-        console.error(e);
-
-        const options: RavenOptions = {};
-
-        if (additionalData != null) {
-            try {
-                options.extra = {additionalData: JSON.stringify(additionalData)};
-            } catch (e) {
-                options.extra = {additionalData: 'Failed to serialize'};
-            }
-        }
-
-        Raven.captureException(e, options);
+        console.error(e, additionalData || '');
     }
 
     warn(
         message: string,
         additionalData?, // has to be serializable to JSON string
     ) {
-        const options: RavenOptions = {};
-
-        options['level'] = 'warning';
-
-        if (additionalData != null) {
-            try {
-                options.extra = {additionalData: JSON.stringify(additionalData)};
-            } catch (e) {
-                options.extra = {additionalData: 'Failed to serialize'};
-            }
-        }
-
-        console.warn(message, options);
-
-        if (Raven.isSetup()) {
-            Raven.captureMessage(message, options);
-        }
+        console.warn(message, additionalData || '');
     }
 }
 
