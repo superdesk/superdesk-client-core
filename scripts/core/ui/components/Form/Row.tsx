c333cf5f3c939ed10859ad787e70144b9bdad3b0
Mark Pittaway <mark.pittaway@gmail.com>
2025-11-27 11:35:17 +1100

    [SDESK-7522] Add test-ids to Contact form

--- scripts/core/ui/components/Form/Row.tsx
+++ scripts/core/ui/components/Form/Row.tsx
@@ -1,23 +1,40 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
 import './style.scss';
+
+interface IProps {
+    flex?: boolean;
+    noPadding?: boolean;
+    className?: string | {[classname: string]: boolean};
+    halfWidth?: boolean;
+    enabled?: boolean;
+    'data-test-id'?: string;
+}
 
 /**
  * @ngdoc react
  * @name Row
  * @description Component to encapsulate a component in Form row style
  */
-export const Row: React.StatelessComponent<any> = ({children, flex, noPadding, halfWidth, className, enabled}) => (
-    !enabled ? null : (
+export const Row: React.FC<IProps> = ({
+    children,
+    flex,
+    noPadding,
+    halfWidth,
+    className,
+    enabled,
+    'data-test-id': testId,
+}) => (
+    enabled === false ? null : (
         <div
+            data-test-id={testId}
             className={classNames(
                 'form__row',
                 {
-                    'form__row--flex': flex,
-                    'no-padding': noPadding,
-                    'form__row--half-width': halfWidth,
+                    'form__row--flex': flex === true,
+                    'no-padding': noPadding === true,
+                    'form__row--half-width': halfWidth === true,
                 },
                 className,
             )}
@@ -26,22 +43,3 @@ export const Row: React.StatelessComponent<any> = ({children, flex, noPadding, h
         </div>
     )
 );
-
-Row.propTypes = {
-    children: PropTypes.node,
-    flex: PropTypes.bool,
-    noPadding: PropTypes.bool,
-    className: PropTypes.oneOfType([
-        PropTypes.string,
-        PropTypes.object,
-    ]),
-    halfWidth: PropTypes.bool,
-    enabled: PropTypes.bool,
-};
-
-Row.defaultProps = {
-    flex: false,
-    noPadding: false,
-    halfWidth: false,
-    enabled: true,
-};
