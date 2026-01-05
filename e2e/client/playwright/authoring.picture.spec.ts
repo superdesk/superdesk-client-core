33057f97baaa11c01d31e95ac02f3f9ee42a7010
thecalcc <maga.ot.oz@protonmail.com>
2026-01-05 14:50:54 +0200

    E2E fix media metadata

--- e2e/client/playwright/authoring.picture.spec.ts
+++ e2e/client/playwright/authoring.picture.spec.ts
@@ -26,11 +26,13 @@ test('media metadata editor', async ({page}) => {
 
     await upload.selectFile('iptc-photo.jpg');
 
-    await expect(mediaEditor.field('field--headline')).toContainText('The Headline');
-
     await setEditor3FieldValue(mediaEditor.field('field--headline'), 'picture');
 
     await expect(mediaEditor.field('field--headline')).toHaveText('picture');
+
+    await setEditor3FieldValue(mediaEditor.field('field--description_text'), 'a nice picture');
+
+    await expect(mediaEditor.field('field--description_text')).toHaveText('a nice picture');
 
     await upload.startUpload();
 
