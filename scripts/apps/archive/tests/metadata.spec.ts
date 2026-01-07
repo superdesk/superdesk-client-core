e185f40f06cd3e9dd5fc5c4988848c37f1a9ac64
thecalcc <maga.ot.oz@protonmail.com>
2026-01-07 11:43:21 +0200

    Dont skip metadata unit test

--- scripts/apps/archive/tests/metadata.spec.ts
+++ scripts/apps/archive/tests/metadata.spec.ts
@@ -55,7 +55,7 @@ const exiftoolFetchPolyfill = (url: string): Promise<Response> => {
 
 const timeout = 10000; // 10s
 
-xdescribe('process item metadata', () => {
+describe('process item metadata', () => {
     it('image metadata', async () => {
         const expected = {
             Keywords: ['Keyword1ref2014', 'Keyword2ref2014', 'Keyword3ref2014'],
