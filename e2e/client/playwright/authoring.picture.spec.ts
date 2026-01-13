94afae26e53474b267dab1f73f700df5137c5cc3
Petr Jasek <jasekpetr@gmail.com>
2026-01-13 15:23:07 +0100

    re-enable metadata e2e test

--- e2e/client/playwright/authoring.picture.spec.ts
+++ e2e/client/playwright/authoring.picture.spec.ts
@@ -11,7 +11,7 @@ import {setEditor3FieldValue} from './utils/editor3';
  * edit metadata
  * test metadata changes from modal are visible in the editor
  */
-test.skip('media metadata editor', async ({page}) => {
+test('media metadata editor', async ({page}) => {
     await restoreDatabaseSnapshot();
 
     const upload = new MediaUpload(page);
