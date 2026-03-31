8bd86f90e91e103799ca41697a3715c71148f8f7
Ivan Jelić <99324896+IvanJelicSF@users.noreply.github.com>
2026-03-31 20:08:55 +0200

    Fix linting errors

--- scripts/apps/authoring/preview/fullPreview.tsx
+++ scripts/apps/authoring/preview/fullPreview.tsx
@@ -112,9 +112,11 @@ export class FullPreview extends React.Component<IProps, IState> {
                         item.type === 'picture' && hideMedia !== true && item.renditions?.baseImage?.href != null
                             ? (
                                 <div className="d-flex flex-col justify-start">
-                                    <button className="btn btn--hollow btn--tertiary btn--small mb-1 ms-auto"
+                                    <button
+                                        className="btn btn--hollow btn--tertiary btn--small mb-1 ms-auto"
                                         onClick={() => {
                                             const a = document.createElement('a');
+
                                             a.href = item.renditions.original.href;
                                             a.download = item.renditions.original.href.split('/').pop();
                                             a.click();
