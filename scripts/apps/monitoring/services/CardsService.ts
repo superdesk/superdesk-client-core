b215b829a75de8ac6998d09631732e8c619de4c9
Brian Mwangi <mwangikabiru21@gmail.com>
2026-05-28 19:55:38 +0300

    SDESK-7942: hide personal items from sent desk output

--- scripts/apps/monitoring/services/CardsService.ts
+++ scripts/apps/monitoring/services/CardsService.ts
@@ -140,6 +140,7 @@ export function CardsService(search, session, desks, $location) {
                 deskId = card._id.substring(0, card._id.indexOf(':'));
                 query.filter({bool: {
                     filter: {term: {'task.desk_history': deskId}},
+                    must: {exists: {field: 'task.desk'}},
                     must_not: {term: {'task.desk': deskId}},
                 }});
                 break;
--- scripts/apps/monitoring/tests/monitoring.spec.ts
+++ scripts/apps/monitoring/tests/monitoring.spec.ts
@@ -181,6 +181,17 @@ describe('monitoring', () => {
             });
         }));
 
+        it('can get criteria for sent desk output', inject((cards, session) => {
+            var card: any = {_id: '1:sent', type: 'sentDeskOutput'};
+            var criteria = cards.criteria(card);
+
+            expect(criteria.source.query.filtered.filter.and).toContain({bool: {
+                filter: {term: {'task.desk_history': '1'}},
+                must: {exists: {field: 'task.desk'}},
+                must_not: {term: {'task.desk': '1'}},
+            }});
+        }));
+
         it('can get criteria for highlight', inject((cards, session) => {
             var card = {type: 'highlights'};
             var queryParam = {highlight: '123'};
