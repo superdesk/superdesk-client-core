efbbe3dde346aabd12394a44f9c0738ea95cdc0c
Petr Jasek <jasekpetr@gmail.com>
2026-09-09 11:36:15 +0200

    hide reading time via app config
    SDANSA-577 SDESK-8017

---
+++ scripts/apps/authoring/authoring/components/text-statistics.spec.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import {appConfig} from 'appConfig';
+import {TextStatistics} from './text-statistics';
+
+describe('TextStatistics', () => {
+    afterEach(() => {
+        delete appConfig.authoring;
+    });
+
+    it('shows reading time by default', () => {
+        const wrapper = shallow(<TextStatistics text="one two three" />);
+
+        expect(wrapper.text()).toContain('less than one minute read');
+    });
+
+    it('hides reading time when disabled via app config', () => {
+        appConfig.authoring = {
+            timeToRead: false,
+        };
+
+        const wrapper = shallow(<TextStatistics text="one two three" />);
+
+        expect(wrapper.text()).not.toContain('less than one minute read');
+    });
+});
--- scripts/apps/authoring/authoring/components/text-statistics.tsx
+++ scripts/apps/authoring/authoring/components/text-statistics.tsx
@@ -19,6 +19,11 @@ export class TextStatistics extends React.PureComponent<IProps> {
     render() {
         const wordCount = countWords(this.props.text);
         const showLineCount = appConfig?.authoring?.lineLength != null && !this.props.singleLine;
+        const showReadingTime = this.props.hideReadingTime !== true && (
+            appConfig.authoring == null || appConfig.authoring.timeToRead == null
+                ? true
+                : appConfig.authoring.timeToRead
+        );
 
         return (
             <div className="char-count__wrapper">
@@ -34,7 +39,7 @@ export class TextStatistics extends React.PureComponent<IProps> {
                     item={this.props.text}
                 />
 
-                {this.props.hideReadingTime !== true && (
+                {showReadingTime && (
                     <span className="char-count__base">
                         {getReadingTimeText(this.props.text, this.props.language ?? '')}
                     </span>
