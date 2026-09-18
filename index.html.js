6950cb8ad747cee66890e83188d655e3e5b7cf76
Helmy Giacoman <helmygb@gmail.com>
2026-09-18 18:27:28 +0200

    Ship the favicon as a bitmap ICO
    The icon was written with PNG-compressed entries, which Safari does not draw as a favicon. The href gets a version query so browsers that cached the old file fetch the new one.

--- index.html.js
+++ index.html.js
@@ -9,7 +9,7 @@ module.exports = function buildIndex(o) {
             <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
             <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
             ${o.qumu ? '<script src="https://video.fidelity.tv/widgets/1/application.js"></script>' : ''}
-            <link rel="icon" type="image/x-icon" href="images/favicon.ico" />
+            <link rel="icon" type="image/x-icon" href="images/favicon.ico?v=briefdesk2" />
             <link rel="stylesheet" type="text/css" href="app.bundle.css">
           </head>
           <body ng-class="config.bodyClass">
